b436d92c3e629dd350a4d55597b7bac2d4fd86bf
egli <stephan.egli@psi.ch>
2018-01-04 15:09:11 +0100

    Also added proposal collection for correct UTC time handling

--- common/models/proposal.js
+++ common/models/proposal.js
@@ -1,7 +1,27 @@
 'use strict';
 var config = require('../../server/config.local');
+var utils = require('./utils');
 
 module.exports = function(Proposal) {
+
+    // put
+    Proposal.beforeRemote('replaceOrCreate', function(ctx, instance, next) {
+        // handle embedded datafile documents
+        utils.updateAllTimesToUTC(["start","end"], ctx.args.data.MeasurementPeriodList)
+        next();
+    });
+
+    //patch
+    Proposal.beforeRemote('patchOrCreate', function(ctx, instance, next) {
+        utils.updateAllTimesToUTC(["start","end"], ctx.args.data.MeasurementPeriodList)
+        next();
+    });
+
+    //post
+    Proposal.beforeRemote('create', function(ctx, unused, next) {
+        utils.updateAllTimesToUTC(["start","end"], ctx.args.data.MeasurementPeriodList)
+        next();
+    });
 
     Proposal.findByInstrumentAndDate = function(instrument, measureTime, options, cb) {
         Proposal.findOne({
