f2f4a2f59e27ddff5ec487a3d728928d2b75cc30
encima <encima@gmail.com>
2018-01-12 13:48:20 +0100

    removed the bloody middleware file because it replaces all settings and does not combine (as in helm

--- CI/PSI/local_jenkins.sh
+++ CI/PSI/local_jenkins.sh
@@ -7,7 +7,7 @@ cp catamel-psisecrets/server/pass-db-qa/datasources_new.json datasources.json
 cp catamel-psisecrets/server/providers.json providers.json
 cp catamel-psiconfig/server/config.local.js config.local.js
 cp catamel-psiconfig/server/kubernetes/helm/dacat-api-server/envfiles-qa/component-config.json component-config.json
-cp catamel-psiconfig/server/kubernetes/helm/dacat-api-server/envfiles-qa/middleware.json middleware.json
+# cp catamel-psiconfig/server/kubernetes/helm/dacat-api-server/envfiles-qa/middleware.json middleware.json
 cd ../../..
 docker build -f CI/PSI/Dockerfile.test . -t catamel_test
 docker run  --privileged -t catamel_test npm run test
