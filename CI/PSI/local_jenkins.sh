caabb938dd846c29cc1d0d09cbb8e10ac40458a7
encima <encima@gmail.com>
2018-01-12 16:08:00 +0100

    issues with chaining commands

--- CI/PSI/local_jenkins.sh
+++ CI/PSI/local_jenkins.sh
@@ -10,4 +10,4 @@ cp catamel-psiconfig/server/kubernetes/helm/dacat-api-server/envfiles-qa/compone
 # cp catamel-psiconfig/server/kubernetes/helm/dacat-api-server/envfiles-qa/middleware.json middleware.json
 cd ../../..
 docker build -f CI/PSI/Dockerfile.test . -t catamel_test
-docker run  --privileged -t catamel_test
+docker run  --privileged -t catamel_test service rabbitmq-server start && NODE_ENV=qa ./node_modules/mocha/bin/mocha --timeout=5000 --reporter nyan --exit
