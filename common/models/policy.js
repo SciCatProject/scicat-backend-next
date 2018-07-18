5cd3dc1327c66f29ab421052c30af86834534719
Luke Gorman <luke.gorman@psi.ch>
2018-07-18 15:14:26 +0200

    Issue #41 work in progress

--- common/models/policy.js
+++ common/models/policy.js
@@ -1,40 +1,45 @@
 'use strict';
 
-module.exports = function(Policy) {
-
-};
-
 //model extension file
 
-/*var config = require('../../server/config.local');
+var config = require('../../server/config.local');
 var p = require('../../package.json');
 var utils = require('./utils');
-var dsl = require('./dataset-lifecycle.json');
-var ds = require('./dataset.json');
-var dsr = require('./raw-dataset.json');
-var dsd = require('./derived-dataset.json');
 var own = require('./ownable.json');
 
 module.exports = function(Policy) {
-        var app = require('../../server/server');
-        console.log("Log: policy.js 1!");
+    var app = require('../../server/server');
 
-        // check logged in user email is a member of policy.manager
-        Policy.observe('before save', (ctx, next) => {
-          console.log("I am her!!!!!!!!");
-            if (ctx.instance) {
-                var email
-                var login
-                if (user && user.profile) {
-                    login = user.profile.login
-                    email = user.profile.email
-                } else {
-                    login = Object.keys(ctx.options.authorizedRoles)[0]
-                    email = login
+    // for policy interactions
+    // check logged in user email is a member of policy.manager
+    Policy.observe('before save', (ctx, next) => {
+
+        if (ctx.currentInstance) {
+            //is a partial update currentInstance rather than instance
+            var UserIdentity = app.models.UserIdentity;
+            var userId = ctx.options.accessToken.userId;
+
+            //PersistedModel Static Method call
+            UserIdentity.findOne({
+                //json filter
+                where: {
+                    userId: userId
                 }
-
-            }
+            }, function(err, instance) {
+                var email = instance.profile.email;
+                //console.log("email:", email);
+                //console.log("manager: ", ctx.currentInstance.manager);
+                if (!ctx.currentInstance.manager.includes(email)) {
+                    var e = new Error('Access Not Allowed - policy manager action');
+                    e.statusCode = 401;
+                    next(e);
+                }
+                else next();
+            });
+        } else {
+            //is an full update/insert/delete
+            //should only be proposalingestor
             next();
-        });
-      };
-*/
+        }
+    });
+};
