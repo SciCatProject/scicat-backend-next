79998fb238d44883a85db0b4098d6f693fea0373
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-05 13:14:59 +0200

    add remoteMethod for get_doi

--- common/models/published-data.js
+++ common/models/published-data.js
@@ -3,6 +3,15 @@
 
 module.exports = function(PublishedData) {
 
+
+    PublishedData.get_doi = function(msg, cb) {
+        cb(null, '10.111.79/TES/DOI ' + msg);
+    }
+
+    PublishedData.remoteMethod('get_doi', {
+        accepts: {arg: 'msg', type: 'string'},
+        returns: {arg: 'doi', type: 'string'}
+    });
      // TODO add logic that give authors privileges to modify data
 
     // // put
