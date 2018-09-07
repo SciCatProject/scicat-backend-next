2299352aa327d22f8b1c94e9d0dd21adbdb20319
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-07 14:40:47 +0200

    add get published_data

--- common/models/published-data.js
+++ common/models/published-data.js
@@ -4,20 +4,21 @@ const datacite_authentication = require("/tmp/generic_config.json");
 const rp = require("request-promise");
 
 module.exports = function(PublishedData) {
-    PublishedData.register = function(id, msg, first_name, last_name, cb) {
-        //const first_name = "Gareth";
-        //const last_name = "Murphy";
-        const affiliation = "ESS";
-        const publisher = "ESS";
-        const publication_year = "2018";
-        const title = "Sample Data from NMX";
-        const technical_info = "Sample Data from NMX";
-        const abstract = "Sample Data experiment for ESS";
-        const doi = "10.17199/BRIGHTNESS/NMX0001";
-        const resource_type = "NeXus HDF5 Files";
-        const url = "https://scicat.esss.se/10.1178/BRIGHTNESS";
+    PublishedData.register = function(id, cb) {
+       PublishedData.findOne({ id: id }, function(err, pub) {
+            const affiliation = pub["affiliation"];
+            const first_name = "Gareth";
+            const last_name = "Murphy";
+            const publisher = pub["publisher"];
+            const publication_year = pub["publicationYear"];
+            const title = pub["title"];
+            const technical_info = pub["title"];
+            const abstract = pub["abstract"];
+            const doi = pub["doi"];
+            const resource_type = pub["resourceType"];
+            const url = pub["url"];
 
-        const xml = `<?xml version="1.0" encoding="UTF-8"?> \
+            const xml = `<?xml version="1.0" encoding="UTF-8"?> \
 <resource xmlns="http://datacite.org/schema/kernel-4" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://datacite.org/schema/kernel-4 http://schema.datacite.org/meta/kernel-4/metadata.xsd">  \
   <identifier identifierType=\"DOI\">${doi}</identifier>  \
   <creators> \
@@ -40,37 +41,38 @@ module.exports = function(PublishedData) {
   <resourceType resourceTypeGeneral="Text">${resource_type}</resourceType> \
 </resource>`;
 
-        const register_plain_text = `#Content-Type:text/plain;charset=UTF-8
+            const register_plain_text = `#Content-Type:text/plain;charset=UTF-8
 doi= ${doi}
 url= ${url}`;
 
-        console.log(xml);
+            //console.log(xml);
 
-        const datacite_register_metadata =
-            "https://mds.datacite.org/metadata" + "/" + doi;
-        const datacite_register_doi =
-            "https://mds.datacite.org/doi" + "/" + doi;
+            const datacite_register_metadata =
+                "https://mds.datacite.org/metadata" + "/" + doi;
+            const datacite_register_doi =
+                "https://mds.datacite.org/doi" + "/" + doi;
 
-        const options_put = {
-            method: "PUT",
-            body: xml,
-            uri: datacite_register_metadata,
-            headers: {
-                "content-type": "application/xml;charset=UTF-8"
-            },
-            auth: datacite_authentication
-        };
+            const options_put = {
+                method: "PUT",
+                body: xml,
+                uri: datacite_register_metadata,
+                headers: {
+                    "content-type": "application/xml;charset=UTF-8"
+                },
+                auth: datacite_authentication
+            };
 
-        const options_register_put = {
-            method: "PUT",
-            body: register_plain_text,
-            uri: datacite_register_doi,
-            headers: {
-                "content-type": "text/plain;charset=UTF-8"
-            },
-            auth: datacite_authentication
-        };
+            const options_register_put = {
+                method: "PUT",
+                body: register_plain_text,
+                uri: datacite_register_doi,
+                headers: {
+                    "content-type": "text/plain;charset=UTF-8"
+                },
+                auth: datacite_authentication
+            };
 
+            /*
         rp(options_put)
             .then(function(parsedBody) {
                 console.log("register metadata worked");
@@ -95,15 +97,20 @@ url= ${url}`;
                 // POST failed...
             });
 
-        cb(null, "doi " + doi);
+*/
+            return doi;
+        });
+        cb(null, "doi: "+doi);
+        return doi;
     };
 
     PublishedData.remoteMethod("register", {
         accepts: [
-            { arg: "id", number: "string", required: true },
-            { arg: "msg", type: "string" },
-            { arg: "first_name", type: "string" },
-            { arg: "last_name", type: "string" }
+            {
+                arg: "id",
+                type: "string",
+                required: true
+            }
         ],
         http: { path: "/:id/register", verb: "post" },
         returns: { arg: "doi", type: "string" }
