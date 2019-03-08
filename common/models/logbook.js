9f554cc38f3e32fd89ca44f2fed16f747512edbb
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-08 09:48:32 +0100

    Updates to Logbook model

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -1,35 +1,5 @@
-"use strict";
-
-var utils = require("./utils");
-var lb = require("./logbook.json");
+'use strict';
 
 module.exports = function(Logbook) {
-    // PUT
-    Logbook.beforeRemote("replaceOrCreate", function(ctx, instance, next) {
-        next();
-    });
 
-    // PATCH
-    Logbook.beforeRemote("patchOrCreate", function(ctx, instance, next) {
-        next();
-    });
-
-    // POST
-    Logbook.beforeRemote("create", function(ctx, unused, next) {
-        next();
-    });
-
-    Logbook.createRoom = function(
-        visibility,
-        room_alias_name,
-        name,
-        topic,
-        creation_content
-    ) {};
-
-    Logbook.findAllRooms = function() {};
-
-    Logbook.login = function(type, identifier, password) {};
-
-    Logbook.sync = function() {};
 };
