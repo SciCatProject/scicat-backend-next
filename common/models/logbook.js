dece03babcf00ca8900a9403bc5b4a7a868e1762
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-13 13:37:52 +0100

    Added requested properties to Logbook model

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -1,5 +1,9 @@
-'use strict';
+"use strict";
 
 module.exports = function(Logbook) {
-
+    Logbook.getUserMessages = function(name, cb) {
+        Logbook.find({ where: { name: name } }, function(err, userMessages) {
+            cb(null, userMessages);
+        });
+    };
 };
