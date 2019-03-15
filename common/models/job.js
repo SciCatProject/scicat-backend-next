54f9dbfb179bbfc8b309c6f7e9a7367c2aa13a95
egli <stephan.egli@psi.ch>
2019-03-15 13:04:49 +0100

    Also Job updates can lead to publish messages now

--- common/models/job.js
+++ common/models/job.js
@@ -13,7 +13,7 @@ function sendMail(to, subjectText, mailText, next) {
         transporter.verify(function (error, success) {
             if (error) {
                 console.log(error);
-                next();
+                return next();
             } else {
                 console.log('      Server is ready to send message to ', to);
                 var message = Object.assign({}, config.smtpMessage);
@@ -26,148 +26,29 @@ function sendMail(to, subjectText, mailText, next) {
                     } else {
                         console.log('      Email sent');
                     }
-                    next();
+                    return next();
                 });
             }
         });
     } else {
-        next()
+        return next()
     }
 }
+
 
 function publishJob(job, ctx, next) {
     if ('queue' in config && config.queue === 'rabbitmq') {
         job.publishJob(ctx.instance, "jobqueue")
         console.log('      Saved Job %s#%s and published to message broker', ctx.Model.modelName, ctx.instance.id);
     }
+    return next()
+}
 
+function sendStartJobEmail(job, ctx, next) {
     let subjectText = ' ' + ctx.instance.type + ' job submitted successfully';
     let mailText = 'Hello, \n\n You created a job to ' + ctx.instance.type + ' datasets. Your job was received and will be completed as soon as possible. \n\n Many Thanks.\n\n' + JSON.stringify(ctx.instance, null, 4);
     let to = ctx.instance.emailJobInitiator
     sendMail(to, subjectText, mailText, next)
-}
-
-function MarkDatasetsAsScheduled(job, ctx, idList, next) {
-
-    let Dataset = app.models.Dataset;
-    Dataset.updateAll({
-        pid: {
-            inq: idList
-        }
-    }, {
-        datasetlifecycle: {
-            archivable: false,
-            retrievable: false,
-            archiveStatusMessage: "scheduledForArchiving"
-        }
-    }, ctx.options, function (err, p) {
-        if (err) {
-            var e = new Error();
-            e.statusCode = 404;
-            e.message = 'Can not find all needed Dataset entries - no archive job sent:\n' + JSON.stringify(err)
-            next(e);
-        } else {
-            publishJob(job, ctx, next)
-        }
-    });
-}
-// for archive jobs all datasets must be in state archivable
-function TestArchiveJobs(job, ctx, idList, next) {
-    let Dataset = app.models.Dataset;
-    Dataset.find({
-        fields: {"pid":true},
-        where: {
-            'datasetlifecycle.archivable': false,
-            pid: {
-                inq: idList
-            }
-        }
-    }, ctx.options, function (err, p) {
-        if (p.length > 0) {
-            var e = new Error();
-            e.statusCode = 409;
-            e.message = 'The following datasets are not in archivable state - no archive job sent:\n' + JSON.stringify(p)
-            next(e);
-        } else {
-            // mark all Datasets as in state scheduledForArchiving, archivable=false
-            MarkDatasetsAsScheduled(job, ctx, idList, next)           
-        }
-    });
-}
-
-// for retrieve jobs all datasets must be in state retrievable 
-// ownerGroup is tested implicitly via Ownable
-
-function TestRetrieveJobs(job, ctx, idList, next) {
-    let Dataset = app.models.Dataset;
-    Dataset.find({
-        fields: {"pid":true},
-        where: {
-            'datasetlifecycle.retrievable': true,
-            pid: {
-                inq: idList
-            }
-        }
-    }, ctx.options, function (err, p) {
-        if (err) {
-            return next(err)
-        } else if (p.length != idList.length) {
-            Dataset.find({
-                fields: {"pid":true},
-                where: {
-                    'datasetlifecycle.retrievable': false,
-                    pid: {
-                        inq: idList
-                    }
-                }
-            }, ctx.options, function (err2, pmiss) {
-                if (err2) {
-                    return next(err2)
-                } else {
-                    var e = new Error();
-                    e.statusCode = 409;
-                    e.message = 'The following datasets are not in retrievable state - no retrieve job sent:\n' + JSON.stringify(pmiss)
-                    return next(e);
-                }
-            })
-        } else {
-            publishJob(job, ctx, next);
-        }
-    });
-}
-
-function TestAllDatasets(job, ctx, idList, next) {
-    
-    let Dataset = app.models.Dataset;
-    Dataset.find({
-        fields:{"pid":true},
-        where: {
-            pid: {
-                inq: idList
-            }
-        }
-    }, ctx.options, function (err, p) {
-        if (err || (p.length != idList.length)) {
-            var e = new Error();
-            e.statusCode = 404;
-            e.message = 'At least one of the datasets could not be found - no Job sent';
-            // TODO should I send an email here ? Only if triggered by autoarchive option ?
-            // subjectText =
-            // mailText=
-            // let to = ctx.instance.emailJobInitiator
-            // sendMail(to, subjectText, mailText, next)
-            return next(e)
-        } else {
-            //test if all datasets are in archivable state
-            if (ctx.instance.type == "archive") {
-                TestArchiveJobs(job, ctx, idList, next)
-            } else if (ctx.instance.type == "retrieve") {
-                TestRetrieveJobs(job, ctx, idList, next)
-            } else {
-                publishJob(job, ctx, next)
-            }
-        }
-    });
 }
 
 function SendFinishJobEmail(Job, ctx, idList, next) {
@@ -182,7 +63,12 @@ function SendFinishJobEmail(Job, ctx, idList, next) {
     //test if all datasets are in retrievable state
     if (ctx.instance.type == "archive" || ctx.instance.type == "retrieve") {
         Dataset.find({
-            fields: {"pid":true,"sourceFolder":true,"size":true,"datasetlifecycle":true},
+            fields: {
+                "pid": true,
+                "sourceFolder": true,
+                "size": true,
+                "datasetlifecycle": true
+            },
             where: {
                 'datasetlifecycle.retrievable': false,
                 pid: {
@@ -209,6 +95,144 @@ function SendFinishJobEmail(Job, ctx, idList, next) {
     }
 }
 
+function MarkDatasetsAsScheduled(job, ctx, idList, next) {
+
+    let Dataset = app.models.Dataset;
+    Dataset.updateAll({
+        pid: {
+            inq: idList
+        }
+    }, {
+        datasetlifecycle: {
+            archivable: false,
+            retrievable: false,
+            archiveStatusMessage: "scheduledForArchiving"
+        }
+    }, ctx.options, function (err, p) {
+        if (err) {
+            var e = new Error();
+            e.statusCode = 404;
+            e.message = 'Can not find all needed Dataset entries - no archive job sent:\n' + JSON.stringify(err)
+            next(e);
+        } else {
+            sendStartJobEmail(job, ctx, function () {
+                publishJob(job, ctx, next)
+            })
+        }
+    });
+}
+// for archive jobs all datasets must be in state archivable
+function TestArchiveJobs(job, ctx, idList, next) {
+    let Dataset = app.models.Dataset;
+    Dataset.find({
+        fields: {
+            "pid": true
+        },
+        where: {
+            'datasetlifecycle.archivable': false,
+            pid: {
+                inq: idList
+            }
+        }
+    }, ctx.options, function (err, p) {
+        if (p.length > 0) {
+            var e = new Error();
+            e.statusCode = 409;
+            e.message = 'The following datasets are not in archivable state - no archive job sent:\n' + JSON.stringify(p)
+            next(e);
+        } else {
+            // mark all Datasets as in state scheduledForArchiving, archivable=false
+            MarkDatasetsAsScheduled(job, ctx, idList, next)
+        }
+    });
+}
+
+// for retrieve jobs all datasets must be in state retrievable 
+// ownerGroup is tested implicitly via Ownable
+
+function TestRetrieveJobs(job, ctx, idList, next) {
+    let Dataset = app.models.Dataset;
+    Dataset.find({
+        fields: {
+            "pid": true
+        },
+        where: {
+            'datasetlifecycle.retrievable': true,
+            pid: {
+                inq: idList
+            }
+        }
+    }, ctx.options, function (err, p) {
+        if (err) {
+            return next(err)
+        } else if (p.length != idList.length) {
+            Dataset.find({
+                fields: {
+                    "pid": true
+                },
+                where: {
+                    'datasetlifecycle.retrievable': false,
+                    pid: {
+                        inq: idList
+                    }
+                }
+            }, ctx.options, function (err2, pmiss) {
+                if (err2) {
+                    return next(err2)
+                } else {
+                    var e = new Error();
+                    e.statusCode = 409;
+                    e.message = 'The following datasets are not in retrievable state - no retrieve job sent:\n' + JSON.stringify(pmiss)
+                    return next(e);
+                }
+            })
+        } else {
+            sendStartJobEmail(job, ctx, function () {
+                publishJob(job, ctx, next)
+            })
+        }
+    });
+}
+
+function TestAllDatasets(job, ctx, idList, next) {
+
+    let Dataset = app.models.Dataset;
+    Dataset.find({
+        fields: {
+            "pid": true
+        },
+        where: {
+            pid: {
+                inq: idList
+            }
+        }
+    }, ctx.options, function (err, p) {
+        if (err || (p.length != idList.length)) {
+            var e = new Error();
+            e.statusCode = 404;
+            e.message = 'At least one of the datasets could not be found - no Job sent';
+            // TODO should I send an email here ? Only if triggered by autoarchive option ?
+            // subjectText =
+            // mailText=
+            // let to = ctx.instance.emailJobInitiator
+            // sendMail(to, subjectText, mailText, next)
+            return next(e)
+        } else {
+            //test if all datasets are in archivable state
+            if (ctx.instance.type == "archive") {
+                TestArchiveJobs(job, ctx, idList, next)
+            } else if (ctx.instance.type == "retrieve") {
+                TestRetrieveJobs(job, ctx, idList, next)
+            } else {
+                sendStartJobEmail(job, ctx, function () {
+                    publishJob(job, ctx, next)
+                })
+            }
+        }
+    });
+}
+
+
 
 module.exports = function (Job) {
 
@@ -226,6 +250,9 @@ module.exports = function (Job) {
         // therefore override this field both for users and functional accounts
         if (ctx.instance) {
             ctx.instance.emailJobInitiator = ctx.options.currentUserEmail;
+            if (ctx.isNewInstance) {
+                ctx.instance.jobStatusMessage = "jobSubmitted"
+            }
         }
         next()
     });
@@ -240,7 +267,9 @@ module.exports = function (Job) {
             } else {
                 // An existing job got some updates - check if you want to send an email
                 if (ctx.instance.jobStatusMessage.startsWith("finish")) {
-                    SendFinishJobEmail(Job, ctx, idList, next)
+                    SendFinishJobEmail(Job, ctx, idList, function () {
+                        publishJob(Job, ctx, next)
+                    })
                 } else {
                     return next()
                 }
@@ -277,7 +306,7 @@ module.exports = function (Job) {
                         return next(null, result)
                     });
                 } else {
-                    return next(null,[])
+                    return next(null, [])
                 }
             }
         });
