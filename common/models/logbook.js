33c6fa1da3255e4982bd4c6adaa818de171cc515
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-15 09:19:01 +0100

    Added methods to post new messages/images

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -1,17 +1,25 @@
 "use strict";
 
 module.exports = function(Logbook) {
+    Logbook.addUserMessage = function() {
+
+    }
+
     Logbook.findAllUserMessages = function(name, cb) {
         Logbook.findOne({ where: { name: name } }, function(err, logbook) {
             cb(null, logbook.userMessages);
         });
     };
 
+    Logbook.addBotMessage = function() {}
+
     Logbook.findAllBotMessages = function(name, cb) {
         Logbook.findOne({ where: { name: name } }, function(err, logbook) {
             cb(null, logbook.botMessages);
         });
     };
+
+    Logbook.addImage = function() {}
 
     Logbook.findAllImages = function(name, cb) {
         Logbook.findOne({ where: { name: name } }, function(err, logbook) {
