9750116a3479faf8d58f93a57127b669637d1b29
Henrik Johansson <henrik.johansson712@gmail.com>
2019-03-15 16:18:44 +0100

    Updated logbook remote methods

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -1,28 +1,58 @@
 "use strict";
 
 module.exports = function(Logbook) {
-    Logbook.addUserMessage = function() {
-
-    }
-
-    Logbook.findAllUserMessages = function(name, cb) {
+    Logbook.findByName = function(name, cb) {
         Logbook.findOne({ where: { name: name } }, function(err, logbook) {
+            cb(null, logbook);
+        });
+    };
+
+    Logbook.addUserMessage = function(id, newMessage, cb) {
+        Logbook.findById(id, function(err, l) {
+            console.log("Update Logbook: " + JSON.stringify(l));
+            console.log("Message added: " + JSON.stringify(newMessage));
+            let allUserMessages = l.userMessages;
+            allUserMessages.push(newMessage);
+            l.updateAttributes({ userMessages: [allUserMessages] }, function(
+                err,
+                instance
+            ) {
+                console.log("newMessage: " + JSON.stringify(newMessage));
+                console.log("instance: " + instance);
+                cb();
+            });
+        });
+    };
+
+    Logbook.findAllUserMessages = function(id, cb) {
+        Logbook.findById(id, function(err, logbook) {
             cb(null, logbook.userMessages);
         });
     };
 
-    Logbook.addBotMessage = function() {}
+    Logbook.addBotMessage = function(newMessage, cb) {
+        Logbook.updateAttribute(botMessages, newMessage, function(
+            err,
+            message
+        ) {
+            cb(null, message);
+        });
+    };
 
-    Logbook.findAllBotMessages = function(name, cb) {
-        Logbook.findOne({ where: { name: name } }, function(err, logbook) {
+    Logbook.findAllBotMessages = function(id, cb) {
+        Logbook.findById(id, function(err, logbook) {
             cb(null, logbook.botMessages);
         });
     };
 
-    Logbook.addImage = function() {}
+    Logbook.addImage = function(newImage, cb) {
+        Logbook.updateAttribute(images, newImage, function(err, image) {
+            cb(null, image);
+        });
+    };
 
-    Logbook.findAllImages = function(name, cb) {
-        Logbook.findOne({ where: { name: name } }, function(err, logbook) {
+    Logbook.findAllImages = function(id, cb) {
+        Logbook.findById(id, function(err, logbook) {
             cb(null, logbook.images);
         });
     };
