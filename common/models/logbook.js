88ee255824fea00cab04face12fcc2e83e6ff1fc
Henrik Johansson <henrik.johansson712@gmail.com>
2019-04-17 16:34:16 +0200

    Added findEntries method to Logbook model

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -2,7 +2,6 @@
 
 const superagent = require("superagent");
 
-
 const scichatHost = "localhost";
 const scichatPort = "3030";
 const scichatBaseUrl = `http://${scichatHost}:${scichatPort}/api`;
@@ -33,6 +32,23 @@ module.exports = function(Logbook) {
     Logbook.findAll = function() {
         return superagent
             .get(scichatBaseUrl + "/Logbooks")
+            .then(response => {
+                return Promise.resolve(response.body);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    /**
+     * Find all Logbook entries matching the query
+     * @param {string} name The name of the Logbook
+     * @param {string} query Query of content in Logbook entry
+     */
+
+    Logbook.findEntries = function(name, query) {
+        return superagent
+            .get(scichatBaseUrl + `/Logbooks/${name}/${query}`)
             .then(response => {
                 return Promise.resolve(response.body);
             })
