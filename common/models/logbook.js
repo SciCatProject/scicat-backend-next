2d04d035ae39ea2932eced2496be699e871234b0
Henrik Johansson <henrik.johansson712@gmail.com>
2019-04-17 10:52:20 +0200

    Added findAll method + refactored code

--- common/models/logbook.js
+++ common/models/logbook.js
@@ -2,7 +2,10 @@
 
 const superagent = require("superagent");
 
-const scichatBaseUrl = "http://localhost:3030/api";
+
+const scichatHost = "localhost";
+const scichatPort = "3030";
+const scichatBaseUrl = `http://${scichatHost}:${scichatPort}/api`;
 
 module.exports = function(Logbook) {
     /**
@@ -12,7 +15,22 @@ module.exports = function(Logbook) {
 
     Logbook.findByName = function(name) {
         return superagent
-            .get(scichatBaseUrl + `/logbooks/${name}`)
+            .get(scichatBaseUrl + `/Logbooks/${name}`)
+            .then(response => {
+                return Promise.resolve(response.body);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    /**
+     * Find all Logbook model instances
+     */
+
+    Logbook.findAll = function() {
+        return superagent
+            .get(scichatBaseUrl + "/Logbooks")
             .then(response => {
                 return Promise.resolve(response.body);
             })
