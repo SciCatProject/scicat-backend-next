7a42404f45076e9bc820e6e2319a8d81a9ef288f
Gareth Murphy <garethcmurphy@gmail.com>
2019-06-24 15:18:55 +0200

    add find groups

--- common/models/share-group.js
+++ common/models/share-group.js
@@ -1,5 +1,21 @@
-'use strict';
+"use strict";
 
 module.exports = function(Sharegroup) {
-
+    Sharegroup.getGroups = function(user, options, cb) {
+        Sharegroup.find(
+            {
+                where: {
+                    members: {
+                        elemMatch: {
+                            instrument: user
+                        }
+                    }
+                }
+            },
+            options,
+            function(err, model) {
+                cb(null, model);
+            }
+        );
+    };
 };
