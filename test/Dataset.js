a3d6f67c6f9dc69ffbdf3289fad6ce56c12ec18b
Henrik Johansson <henrik.johansson712@gmail.com>
2019-08-15 11:13:56 +0200

    Added tests for dataset attachments

--- test/Dataset.js
+++ test/Dataset.js
@@ -1,126 +1,192 @@
 /* jshint node:true */
 /* jshint esversion:6 */
-'use strict';
+"use strict";
 
 // process.env.NODE_ENV = 'test';
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var request = require('supertest');
-var should = chai.should();
-var utils = require('./LoginUtils');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const request = require("supertest");
+const should = chai.should();
+const utils = require("./LoginUtils");
 
-var accessToken = null,
-    pid = null;
+let accessToken = null,
+    pid = null,
+    attachmentId = null;
 
-var testdataset = {
-    "owner": "Bertram Astor",
-    "ownerEmail": "bertram.astor@grumble.com",
-    "orcidOfOwner": "unknown",
-    "contactEmail": "bertram.astor@grumble.com",
-    "sourceFolder": "/iramjet/tif/",
-    "creationTime": "2011-09-14T06:08:25.000Z",
-    "keywords": [
-        "Cryo", "Calibration"
-    ],
-    "description": "None",
-    "type": "raw",
-    "license": "CC BY-SA 4.0",
-    "isPublished": false,
-    "ownerGroup": "p13388",
-    "accessGroups": [],
-    "history": ["this should be deleted"],
-    "createdBy": "this should be deleted as well"
-}
+const testdataset = {
+    owner: "Bertram Astor",
+    ownerEmail: "bertram.astor@grumble.com",
+    orcidOfOwner: "unknown",
+    contactEmail: "bertram.astor@grumble.com",
+    sourceFolder: "/iramjet/tif/",
+    creationTime: "2011-09-14T06:08:25.000Z",
+    keywords: ["Cryo", "Calibration"],
+    description: "None",
+    type: "raw",
+    license: "CC BY-SA 4.0",
+    isPublished: false,
+    ownerGroup: "p13388",
+    accessGroups: [],
+    history: ["this should be deleted"],
+    createdBy: "this should be deleted as well"
+};
 
-var app
-before(function () {
-    app = require('../server/server')
+let app;
+before(function() {
+    app = require("../server/server");
 });
 
-describe('Simple Dataset tests', () => {
-    before((done) => {
-        utils.getToken(app, {
-                'username': 'ingestor',
-                'password': 'aman'
+describe("Simple Dataset tests", () => {
+    before(done => {
+        utils.getToken(
+            app,
+            {
+                username: "ingestor",
+                password: "aman"
             },
-            (tokenVal) => {
+            tokenVal => {
                 accessToken = tokenVal;
                 done();
-            });
+            }
+        );
     });
 
-    it('adds a new dataset', function (done) {
+    it("adds a new dataset", function(done) {
         request(app)
-            .post('/api/v3/Datasets?access_token=' + accessToken)
+            .post("/api/v3/Datasets?access_token=" + accessToken)
             .send(testdataset)
-            .set('Accept', 'application/json')
+            .set("Accept", "application/json")
             .expect(200)
-            .expect('Content-Type', /json/)
-            .end(function (err, res) {
-                if (err)
-                    return done(err);
-                res.body.should.have.property('version').and.be.string;
-                res.body.should.have.property('type').and.equal('raw');
-                res.body.should.have.property('pid').and.be.string;
-                res.body.should.have.property('datasetName').and.be.string;
+            .expect("Content-Type", /json/)
+            .end(function(err, res) {
+                if (err) return done(err);
+                res.body.should.have.property("version").and.be.string;
+                res.body.should.have.property("type").and.equal("raw");
+                res.body.should.have.property("pid").and.be.string;
+                res.body.should.have.property("datasetName").and.be.string;
                 //res.body.should.not.have.property('history')
-                pid = encodeURIComponent(res.body['pid']);
+                pid = encodeURIComponent(res.body["pid"]);
                 done();
             });
     });
 
-    it('should fetch this new dataset', function (done) {
+    it("should fetch this new dataset", function(done) {
         request(app)
-            .get('/api/v3/Datasets/' + pid + '?access_token=' + accessToken)
-            .set('Accept', 'application/json')
+            .get("/api/v3/Datasets/" + pid + "?access_token=" + accessToken)
+            .set("Accept", "application/json")
             .expect(200)
-            .expect('Content-Type', /json/)
+            .expect("Content-Type", /json/)
             .end((err, res) => {
-                if (err)
-                    return done(err);
+                if (err) return done(err);
                 done();
             });
     });
 
-    it('should delete this dataset', function (done) {
+    it("should add a new attachment to this dataset", function(done) {
+        const testAttachment = {
+            thumbnail: "data/abc123",
+            caption: "Some caption",
+            creationTime: new Date(),
+            datsetId: pid
+        };
         request(app)
-            .delete('/api/v3/Datasets/' + pid + '?access_token=' + accessToken)
-            .set('Accept', 'application/json')
+            .post(
+                "/api/v3/Datasets/" +
+                    pid +
+                    "/attachments?access_token=" +
+                    accessToken
+            )
+            .send(testAttachment)
+            .set("Accept", "application/json")
             .expect(200)
-            .expect('Content-Type', /json/)
+            .expect("Content-Type", /json/)
             .end((err, res) => {
-                if (err)
-                    return done(err);
+                if (err) return done(err);
+                res.body.should.have.property("thumbnail").and.be.string;
+                res.body.should.have.property("caption").and.be.string;
+                res.body.should.have.property("creationTime").and.be.date;
+                res.body.should.have.property("id").and.be.string;
+                res.body.should.have.property("datasetId").and.be.string;
+                attachmentId = encodeURIComponent(res.body["id"]);
                 done();
             });
     });
 
-
-    it('fetches array of Datasets', function (done) {
+    it("should fetch this dataset attachment", function(done) {
         request(app)
-            .get('/api/v3/Datasets?filter=%7B%22limit%22%3A10%7D&access_token=' + accessToken)
-            .set('Accept', 'application/json')
+            .get(
+                "/api/v3/Datasets/" +
+                    pid +
+                    "/attachments/" +
+                    attachmentId +
+                    "?access_token=" +
+                    accessToken
+            )
+            .set("Accept", "application/json")
             .expect(200)
-            .expect('Content-Type', /json/)
+            .expect("Content-Type", /json/)
             .end((err, res) => {
-                if (err)
-                    return done(err);
+                if (err) return done(err);
+                done();
+            });
+    });
+
+    it("should delete this dataset attachment", function(done) {
+        request(app)
+            .delete(
+                "/api/v3/Datasets/" +
+                    pid +
+                    "/attachments/" +
+                    attachmentId +
+                    "?access_token=" +
+                    accessToken
+            )
+            .set("Accept", "application/json")
+            .expect(204)
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
+    });
+
+    it("should delete this dataset", function(done) {
+        request(app)
+            .delete("/api/v3/Datasets/" + pid + "?access_token=" + accessToken)
+            .set("Accept", "application/json")
+            .expect(200)
+            .expect("Content-Type", /json/)
+            .end((err, res) => {
+                if (err) return done(err);
+                done();
+            });
+    });
+
+    it("fetches array of Datasets", function(done) {
+        request(app)
+            .get(
+                "/api/v3/Datasets?filter=%7B%22limit%22%3A10%7D&access_token=" +
+                    accessToken
+            )
+            .set("Accept", "application/json")
+            .expect(200)
+            .expect("Content-Type", /json/)
+            .end((err, res) => {
+                if (err) return done(err);
                 res.body.should.be.instanceof(Array);
                 done();
             });
     });
 
-    it('should contain an array of facets', function (done) {
+    it("should contain an array of facets", function(done) {
         request(app)
-            .get('/api/v3/Datasets/fullfacet?access_token=' + accessToken)
-            .set('Accept', 'application/json')
+            .get("/api/v3/Datasets/fullfacet?access_token=" + accessToken)
+            .set("Accept", "application/json")
             .expect(200)
-            .expect('Content-Type', /json/)
+            .expect("Content-Type", /json/)
             .end((err, res) => {
-                if (err)
-                    return done(err);
-                res.body.should.be.an('array');
+                if (err) return done(err);
+                res.body.should.be.an("array");
                 done();
             });
     });
