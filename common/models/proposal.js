c87595308194eea0280a7b56b3955f26a682d6e2
Henrik Johansson <henrik.johansson712@gmail.com>
2019-08-29 10:31:45 +0200

    Refactored fullQuery

--- common/models/proposal.js
+++ common/models/proposal.js
@@ -83,51 +83,12 @@ module.exports = function(Proposal) {
         });
     };
 
-    function searchExpression(key, value) {
-        let type = "string";
-        if (key in ds.properties) {
-            type = ds.properties[key].type;
-        } else if (key in own.properties) {
-            type = own.properties[key].type;
-        }
-        if (key === "text") {
-            return {
-                $search: value,
-                $language: "none"
-            };
-        } else if (type === "string") {
-            if (value.constructor === Array) {
-                if (value.length == 1) {
-                    return value[0];
-                } else {
-                    return {
-                        $in: value
-                    };
-                }
-            } else {
-                return value;
-            }
-        } else if (type === "date") {
-            return {
-                $gte: new Date(value.begin),
-                $lte: new Date(value.end)
-            };
-        } else if (type === "boolean") {
-            return {
-                $eq: value
-            };
-        } else if (type.constructor === Array) {
-            return {
-                $in: value
-            };
-        }
-    }
-
     Proposal.fullquery = function(fields, limits, options, cb) {
-        // if (fields.text) {
         if (fields === undefined) {
             fields = {};
         }
+
+        fields.userGroups = options.currentGroups;
 
         let queryFilter = {
             where: {
@@ -135,9 +96,7 @@ module.exports = function(Proposal) {
             }
         };
 
-        fields.userGroups = options.currentGroups;
-
-        Object.keys(fields).map(key => {
+        Object.keys(fields).forEach(key => {
             if (fields[key] && fields[key] !== "null") {
                 if (key === "text") {
                     let textPatterns = [];
@@ -166,6 +125,12 @@ module.exports = function(Proposal) {
             }
         });
 
+        // If neither text search or groups are present, delete where filter
+        // as mongo does not accept empty $and filters
+        if (queryFilter.where.and.length === 0) {
+            delete queryFilter.where;
+        }
+
         if (limits) {
             if ("limit" in limits) {
                 queryFilter.limit =
@@ -190,133 +155,6 @@ module.exports = function(Proposal) {
             }
         }
 
-        console.log("queryFilter1", queryFilter);
-
-        queryFilter.where.and.forEach(item => {
-            console.log(
-                `queryfilter.where.and[${queryFilter.where.and.indexOf(item)}]`,
-                item
-            );
-        });
-        if (queryFilter.where.and.length === 0) {
-            delete queryFilter.where;
-        }
-
-        console.log("queryFilter2", queryFilter);
         return Proposal.find(queryFilter);
-        // } else {
-        //     // keep the full aggregation pipeline definition
-        //     let pipeline = [];
-        //     if (fields === undefined) {
-        //         fields = {};
-        //     }
-        //     // console.log("++++++++++++ fullquery: after filling fields with usergroup:",fields)
-        //     // let matchJoin = {}
-        //     // construct match conditions from fields value
-        //     Object.keys(fields).map(function(key) {
-        //         if (fields[key] && fields[key] !== "null") {
-        //             if (key === "text") {
-        //                 // unshift because text must be at start of array
-        //                 pipeline.unshift({
-        //                     $match: {
-        //                         $or: [
-        //                             {
-        //                                 $text: searchExpression(
-        //                                     key,
-        //                                     fields[key]
-        //                                 )
-        //                             }
-        //                         ]
-        //                     }
-        //                 });
-        //             }
-        //             // mode is not a field in dataset, just an object for containing a match clause
-        //             else if (key === "mode") {
-        //                 pipeline.push({
-        //                     $match: fields[key]
-        //                 });
-        //             } else if (key === "userGroups") {
-        //                 if (fields["userGroups"].indexOf("globalaccess") < 0) {
-        //                     pipeline.push({
-        //                         $match: {
-        //                             $or: [
-        //                                 {
-        //                                     ownerGroup: searchExpression(
-        //                                         "ownerGroup",
-        //                                         fields["userGroups"]
-        //                                     )
-        //                                 },
-        //                                 {
-        //                                     accessGroups: searchExpression(
-        //                                         "accessGroups",
-        //                                         fields["userGroups"]
-        //                                     )
-        //                                 }
-        //                             ]
-        //                         }
-        //                     });
-        //                 }
-        //             } else {
-        //                 let match = {};
-        //                 match[key] = searchExpression(key, fields[key]);
-        //                 pipeline.push({
-        //                     $match: match
-        //                 });
-        //             }
-        //         }
-        //     });
-
-        //     // }
-        //     // final paging section ===========================================================
-        //     if (limits) {
-        //         if ("order" in limits) {
-        //             // input format: "creationTime:desc,creationLocation:asc"
-        //             const sortExpr = {};
-        //             const sortFields = limits.order.split(",");
-        //             sortFields.map(function(sortField) {
-        //                 const parts = sortField.split(":");
-        //                 const dir = parts[1] == "desc" ? -1 : 1;
-        //                 sortExpr[parts[0]] = dir;
-        //             });
-        //             pipeline.push({
-        //                 $sort: sortExpr
-        //                 // e.g. { $sort : { creationLocation : -1, creationLoation: 1 } }
-        //             });
-        //         }
-
-        //         if ("skip" in limits) {
-        //             pipeline.push({
-        //                 $skip: Number(limits.skip) < 1 ? 0 : Number(limits.skip)
-        //             });
-        //         }
-        //         if ("limit" in limits) {
-        //             pipeline.push({
-        //                 $limit:
-        //                     Number(limits.limit) < 1 ? 1 : Number(limits.limit)
-        //             });
-        //         }
-        //     }
-        //     //console.log("Resulting aggregate query in fullquery method:", JSON.stringify(pipeline, null, 3));
-        //     Proposal.getDataSource().connector.connect(function(err, db) {
-        //         const collection = db.collection("Proposal");
-        //         collection.aggregate(pipeline, function(err, cursor) {
-        //             cursor.toArray(function(err, res) {
-        //                 if (err) {
-        //                     console.log("Facet err handling:", err);
-        //                 }
-        //                 // rename _id to pid
-        //                 res.map(ds => {
-        //                     Object.defineProperty(
-        //                         ds,
-        //                         "proposalId",
-        //                         Object.getOwnPropertyDescriptor(ds, "_id")
-        //                     );
-        //                     delete ds["_id"];
-        //                 });
-        //                 cb(err, res);
-        //             });
-        //         });
-        //     });
-        // }
     };
 };
