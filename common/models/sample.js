08c420f09eb961c10e11131e88e6a5961b80ec83
Henrik Johansson <henrik.johansson712@gmail.com>
2020-11-17 09:36:01 +0100

    Add Sample.metadataKeys endpoint

--- common/models/sample.js
+++ common/models/sample.js
@@ -1,7 +1,9 @@
 "use strict";
 
+var config = require("../../server/config.local");
 const ds = require("./sample.json");
 const own = require("./ownable.json");
+const logger = require("../logger");
 
 module.exports = function(Sample) {
     function searchExpression(key, value) {
@@ -66,7 +68,7 @@ module.exports = function(Sample) {
         }
         next();
     });
-        
+
 
     Sample.fullquery = function(fields, limits, options, cb) {
         // keep the full aggregation pipeline definition
@@ -175,7 +177,123 @@ module.exports = function(Sample) {
                     });
                     cb(err, res);
                 });
+                }
+            );
             });
-        });
+    };
+
+    /**
+     * Get a list of sample characteristic keys
+     * @param {object} fields Define the filter conditions by specifying the name of values of fields requested. There is also support for a `text` search to look for strings anywhere in the sample.
+     * @param {object} limits Define further query parameters like skip, limit, order
+     * @param {object} options
+     * @param {Function(Error, array)} callback
+     */
+
+    Sample.metadataKeys = async function (fields, limits, options) {
+        try {
+            const blacklist = [new RegExp(".*_date")];
+            const returnLimit = config.metadataKeysReturnLimit;
+            const { metadataKey } = fields;
+
+            // ensure that no more than MAXLIMIT samples are read for metadata key extraction
+            let MAXLIMIT;
+            if (config.metadataDatasetsReturnLimit) {
+                MAXLIMIT = config.metadataDatasetsReturnLimit;
+
+                let lm = {};
+
+                if (limits) {
+                    lm = JSON.parse(JSON.stringify(limits));
+                }
+
+                if (!lm.limit) {
+                    lm.limit = MAXLIMIT;
+                }
+
+                if (lm.limit && lm.limit > MAXLIMIT) {
+                    lm.limit = MAXLIMIT;
+                }
+
+                limits = lm;
+
+                logger.logInfo("Fetching metadataKeys", {
+                    fields,
+                    limits,
+                    options,
+                    blacklist: blacklist.map((item) => item.toString()),
+                    returnLimit,
+                });
+
+                let samples;
+                try {
+                    samples = await new Promise((resolve, reject) => {
+                        Sample.fullquery(
+                            fields,
+                            limits,
+                            options,
+                            (err, res) => {
+                                resolve(res);
+                            }
+                        );
+                    });
+                } catch (err) {
+                    logger.logError(err.message, {
+                        location: "Sample.metadataKeys.samples",
+                        fields,
+                        limits,
+                        options,
+                    });
+                }
+
+                if (samples.length > 0) {
+                    logger.logInfo("Found samples", { count: samples.length });
+                } else {
+                    logger.logInfo("No samples found", { samples });
+                }
+
+                const metadata = samples.map((sample) =>
+                    sample.sampleCharacteristics
+                        ? Object.keys(sample.sampleCharacteristics)
+                        : []
+                );
+
+                logger.logInfo("Raw metadata array", {
+                    count: metadata.length,
+                });
+
+                // Flatten array, ensure uniqueness of keys and filter out
+                // blacklisted keys
+                const metadataKeys = [].concat
+                    .apply([], metadata)
+                    .reduce((accumulator, currentValue) => {
+                        if (accumulator.indexOf(currentValue) === -1) {
+                            accumulator.push(currentValue);
+                        }
+                        return accumulator;
+                    }, [])
+                    .filter(
+                        (key) => !blacklist.some((regex) => regex.test(key))
+                    );
+
+                logger.logInfo("Curated metadataKeys", {
+                    count: metadataKeys.length,
+                });
+
+                if (metadataKey && metadataKey.length > 0) {
+                    return metadataKeys
+                        .filter((key) =>
+                            key
+                                .toLowerCase()
+                                .includes(metadataKey.toLowerCase())
+                        )
+                        .slice(0, returnLimit);
+                }
+
+                return metadataKeys.slice(0, returnLimit);
+            }
+        } catch (err) {
+            logger.logError(err.message, { location: "Sample.metadataKeys" });
+        }
     };
 };
