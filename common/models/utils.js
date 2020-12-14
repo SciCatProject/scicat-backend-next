3fe7c82e81fd0a5d2e04d8070b3bf1686cd9e03e
Henrik Johansson <henrik.johansson712@gmail.com>
2020-12-14 11:18:56 +0100

    Update to use copy of data object

--- common/models/utils.js
+++ common/models/utils.js
@@ -185,15 +185,16 @@ function updateHistory(ctx, datasetInstances, ctxdatacopy, index, next) {
         // TODO: this ignores any update which contains these fields among other chanegs
         if (!ctx.data.size && !ctx.data.packedSize) {
             const { updatedAt, updatedBy, ...updatedFields } = ctxdatacopy;
+            const historyItem = Object.assign({}, ctxdatacopy);
             Object.keys(updatedFields).forEach((updatedField) => {
-                ctxdatacopy[updatedField] = {
+                historyItem[updatedField] = {
                     currentValue: ctxdatacopy[updatedField],
                     previousValue: datasetInstance[updatedField],
                 };
             });
             // the following triggers a before save hook . endless recursion must be prevented there
             // console.log("=====Calling create with ctx.data:", JSON.stringify(ctx.data, null, 3))
-            datasetInstance.historyList.create(JSON.parse(JSON.stringify(ctxdatacopy).replace(/\$/g, "")), function (err, instance) {
+            datasetInstance.historyList.create(JSON.parse(JSON.stringify(historyItem).replace(/\$/g, "")), function (err, instance) {
                 if (err) {
                     console.log("Saving auto history failed:", err)
                 }
