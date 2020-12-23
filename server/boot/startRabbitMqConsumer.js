87b228dbea76d83b5b05d2236705eecc6b86ba58
Linh Nguyen <linh.nguyen@maxiv.lu.se>
2020-12-23 14:54:17 +0100

    Implement proposal consumer

--- server/boot/startRabbitMqConsumer.js
+++ server/boot/startRabbitMqConsumer.js
@@ -1,82 +1,150 @@
 "use strict";
+const amqp = require('amqplib/callback_api');
+const config = require('../config.local');
+const logger = require('../../common/logger');
+const utils = require('../../common/models/utils');
 
-const config = require("../config.local");
-const logger = require("../../common/logger");
-
-module.exports = function(app) {
-    const rabbitMqEnabled = config.rabbitmq ? config.rabbitmq.enabled : false;
-    if (rabbitMqEnabled) {
-        let url;
-        if (config.rabbitmq.host) {
-            if (config.rabbitmq.port) {
-                url = `amqp://${user}:${pass}@${config.rabbitmq.host}:${config.rabbitmq.port}`;
+module.exports = function (app) {
+    const Proposal = app.models.Proposal;
+    let connectionDetails;
+    const startConsumer = (connection) => {
+        connection.createChannel(function (error, channel) {
+            if (error) {
+                logger.logError(error.message, {
+                    location: "connection.createChannel",
+                    connection
+                });
             } else {
-                url = `amqp://${user}:${pass}@${config.rabbitmq.host}`;
-            }
+                const queue = config.rabbitmq.queue;
+                channel.assertQueue(queue, {
+                    durable: true
+                });
+                channel.prefetch(1);
 
-            logger.logInfo("Connecting to RabbitMq", { url });
+                logger.logInfo("RABBITMQ Waiting for messages", {
+                    queue
+                });
 
-            amqp.connect(url, function(error0, connection) {
-                if (error0) {
-                    logger.logError(error0.message, {
-                        location: "amqp.connect",
-                        url
-                    });
-                } else {
-                    connection.createChannel(function(error1, channel) {
-                        if (error1) {
-                            logger.logError(error1.message, {
-                                location: "connection.createChannel",
-                                connection
-                            });
-                        } else {
-                            const queue = config.rabbitmq.queue;
-
-                            channel.assertQueue(queue, {
-                                durable: true
-                            });
-
-                            channel.prefetch(1);
-
-                            logger.logInfo("RABBITMQ Waiting for messages", {
-                                queue
-                            });
-
-                            channel.consume(
-                                queue,
-                                async function(msg) {
-                                    const msgJSON = JSON.parse(msg.content);
-                                    switch (msgJSON.eventType) {
-                                        case "PROPOSAL_ACCEPTED": {
-                                            logger.logInfo(
-                                                "RabbitMq received message",
-                                                {
-                                                    message: msg.content.toString()
-                                                }
-                                            );
-                                            try {
-                                                // TODO: Add actual functionality
-                                            } catch (err) {
-                                                logger.logError(err.message, {
-                                                    location: "channel.consume"
+                channel.consume(
+                    queue,
+                    async function (msg) {
+                        const msgJSON = JSON.parse(msg.content);
+                        switch (msgJSON.eventType) {
+                            case "PROPOSAL_INFORMATION": {
+                                logger.logInfo(
+                                    "RabbitMq received message",
+                                    {
+                                        message: msg.content.toString()
+                                    }
+                                );
+                                try {
+                                    let { eventType, ...proposalData } = msgJSON;
+                                    Proposal.replaceOrCreate(proposalData, (error, proposalInstance) => {
+                                        if (error) {
+                                            logger.logError(error.message, {
+                                                location: "Proposal.replaceOrCreate"
+                                            });
+                                        } else {
+                                            if (proposalInstance) {
+                                                logger.logInfo(`Proposal was created/updated`,{
+                                                    proposalId: proposalData.proposalId
                                                 });
                                             }
-                                            channel.ack(msg);
-                                            break;
                                         }
-                                        default:
-                                            channel.ack(msg);
-                                            break;
-                                    }
-                                },
-                                {
-                                    noAck: false
+                                    })
+                                } catch (error) {
+                                    logger.logError(error.message, {
+                                        location: "channel.consume"
+                                    });
                                 }
-                            );
+                                channel.ack(msg);
+                                break;
+                            }
+                            default:
+                                channel.ack(msg);
+                                break;
                         }
-                    });
-                }
+                    },
+                    {
+                        noAck: false
+                    }
+                );
+            }
+        });
+    }
+    const sendNotificationEmail = () => {
+        if ('smtpMessage' in config && 'to' in config.smtpMessage && config.smtpMessage.to) {
+            const subjectText = 'Failed to connect to RabbitMQ';
+            let mailText = 'Hello,\n Scicat backend failed to connect to RabbitMQ\n';
+            mailText += "Connection details:\n";
+            mailText += JSON.stringify(connectionDetails, null, 3);
+            utils.sendMail(config.smtpMessage.to, "", subjectText, mailText, null, null);
+        } else {
+            logger.logWarn("smtpMessage is not configured properly no email was sent",{
+                location: 'sendNotificationEmail'
             });
+        }
+    }
+    const {maxReconnectionAttempts, reconnectionInterval} = config.rabbitmq;
+    let connectionAttempts = 0;
+    const connect = () => {
+        
+        amqp.connect(connectionDetails, function (error, connection) {
+            if (error) {
+                logger.logError(error.message, {
+                    location: "amqp.connect",
+                    connectionDetails
+                });
+                if (connectionAttempts < maxReconnectionAttempts) {
+                    console.log("RABBITMQ - Connection attempt " + connectionAttempts)
+                    connectionAttempts++;
+                    // try to connect again after some amount of time
+                    return setTimeout(connect, reconnectionInterval, connectionDetails);
+                }
+                console.log("RABBITMQ - Unable to connect");
+                sendNotificationEmail();
+                return;
+            }
+            connection.on("error", (error) => {
+                logger.logError(error.message, {
+                    location: "connection.on error",
+                });
+            });
+            connection.on("close", () => {
+                logger.logError("RABBITMQ - Reconnecting", {
+                    location: "connection.on close",
+                });
+                if (connectionAttempts < maxReconnectionAttempts) {
+                    console.log("RABBITMQ - Connection attempt " + connectionAttempts)
+                    connectionAttempts++;
+                    // try to connect again after some amount of time
+                    return setTimeout(connect, reconnectionInterval, connectionDetails);
+                }
+                console.log("RABBITMQ - Unable to reconnect");
+                sendNotificationEmail();
+                return;
+            });
+            console.log("RABBITMQ - Connected");
+            connectionAttempts = 0;
+            startConsumer(connection);;
+        });
+    }
+    const rabbitMqEnabled = config.rabbitmq ? config.rabbitmq.enabled : false;
+    if (rabbitMqEnabled) {
+        if (config.rabbitmq.host) {
+            connectionDetails = {
+                protocol: 'amqp',
+                hostname: config.rabbitmq.host,
+                username: config.rabbitmq.username,
+                password: config.rabbitmq.password,
+                heartbeat: 60,
+                vhost: '/',
+            }
+            if (config.rabbitmq.port) {
+                connectionDetails = { ...connectionDetails, port: config.rabbitmq.port }
+            }
+            logger.logInfo("Connecting to RabbitMq", connectionDetails);
+            connect();
         }
     }
 };
