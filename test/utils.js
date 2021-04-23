1dc50a7350c6898edd159b8138da64f5b8caaaaf
Max Novelli <Max.Novelli@ess.eu>
2021-04-23 14:16:42 +0200

    Fixed linter error. First draft of superagent wrapper test

---
+++ test/utils.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// process.env.NODE_ENV = 'test';
+
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var request = require("supertest");
+var should = chai.should();
+var utils = require("../common/utils.js");
+
+
+const superagent_tests = {
+    "put" : {
+        method: "PUT",
+        body: "<test>This is just a put test</test>",
+        uri: "http://this.is.test/put",
+        headers: {
+          "content-type": "application/xml;charset=UTF-8",
+        },
+        auth: {
+            "username" : "a_user",
+            "password" : "the_password"
+        },
+    },
+    "post" : {
+        method: "POST",
+        body: "<test>This is just a post test</test>",
+        uri: "http://this.is.test/post",
+        headers: {
+          "content-type": "application/xml;charset=UTF-8",
+        },
+        auth: {
+            "username" : "a_user",
+            "password" : "the_password"
+        },
+    }
+}
+
+
+describe("utils.superagent", () => {
+    
+    it("should return an instance of superagent", () => {
+        const res = utils.superagent(superagent_tests["put"]);
+        res.should.be.instanceof('superagent');
+        return console.log(res);
+    });
+
+    it("should return an instance of superagent with put method", () => {
+        const res = utils.superagent(superagent_tests["put"]);
+        res.should.be.instanceof('superagent')
+    });
+
+    it("should return an instance of superagent with post method", () => {
+        const res = utils.superagent(superagent_tests["post"]);
+            res.should.be.instanceof('superagent')
+    });
+
+});
+
+
+
