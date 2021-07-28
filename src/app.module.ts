fd705fed5eca80c6b410c4be97d1bee87ed44679
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-28 16:21:02 +0200

    Set up mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
   controllers: [AppController],
   providers: [AppService],
 })
