c7e7740127e195ebfe4bb4d2c255d288134f806e
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-29 09:36:47 +0200

    Import dataset schema to module

--- src/datasets/datasets.module.ts
+++ src/datasets/datasets.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Dataset, DatasetSchema } from './dataset.schema';
 import { DatasetsController } from './datasets.controller';
 import { DatasetsService } from './datasets.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Dataset.name, schema: DatasetSchema }]),
+  ],
   controllers: [DatasetsController],
   providers: [DatasetsService],
 })
