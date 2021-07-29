388ff4661980e275ead6b16c5af6a7f861d73785
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-29 11:48:28 +0200

    Set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Dacat API')
+    .setDescription('SciCat backend API')
+    .setVersion('4.0.0')
+    .addTag('scicat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('explorer', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
