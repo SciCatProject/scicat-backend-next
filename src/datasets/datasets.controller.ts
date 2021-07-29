d3fb04dddee168218b31faffa636012e1ba5409c
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-29 14:47:20 +0200

    Add swagger api tag

--- src/datasets/datasets.controller.ts
+++ src/datasets/datasets.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { DatasetsService } from './datasets.service';
 import { CreateDatasetDto } from './dto/create-dataset.dto';
 import { Dataset } from './schemas/dataset.schema';
 
+@ApiTags('datasets')
 @Controller('datasets')
 export class DatasetsController {
   constructor(private datasetsService: DatasetsService) {}
