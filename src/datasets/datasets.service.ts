2979ce48aa780078e798d4dd58048c487f652063
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-29 11:37:24 +0200

    Set up dataset service

--- src/datasets/datasets.service.ts
+++ src/datasets/datasets.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateDatasetDto } from './dto/create-dataset.dto';
+import { Dataset, DatasetDocument } from './schemas/dataset.schema';
 
 @Injectable()
-export class DatasetsService {}
+export class DatasetsService {
+  constructor(
+    @InjectModel(Dataset.name) private datasetModel: Model<DatasetDocument>,
+  ) {}
+
+  async create(createDatasetDto: CreateDatasetDto): Promise<Dataset> {
+    const createdDataset = new this.datasetModel(createDatasetDto);
+    return createdDataset.save();
+  }
+
+  async findAll(): Promise<Dataset[]> {
+    return this.datasetModel.find().exec();
+  }
+}
