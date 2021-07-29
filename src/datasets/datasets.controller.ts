45872f45243f552e5a943923ff4da89bd343f5c6
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-29 11:42:19 +0200

    Add get and post endpoints to dataset controller

--- src/datasets/datasets.controller.ts
+++ src/datasets/datasets.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { DatasetsService } from './datasets.service';
+import { CreateDatasetDto } from './dto/create-dataset.dto';
+import { Dataset } from './schemas/dataset.schema';
 
 @Controller('datasets')
-export class DatasetsController {}
+export class DatasetsController {
+  constructor(private datasetsService: DatasetsService) {}
+
+  @Post()
+  async create(@Body() createDatasetDto: CreateDatasetDto): Promise<Dataset> {
+    return this.datasetsService.create(createDatasetDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Dataset[]> {
+    return this.datasetsService.findAll();
+  }
+}
