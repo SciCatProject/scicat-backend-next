ed6a88b845045a719d9695ebecf2dcdd9e0354f6
Henrik Johansson <henrik.johansson712@gmail.com>
2021-07-30 14:24:18 +0200

    Remove default endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatasetsModule } from './datasets/datasets.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/nest'), DatasetsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
