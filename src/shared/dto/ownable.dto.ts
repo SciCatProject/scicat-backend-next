f64f9a6d562b996ce34b8c180b9a5c63cfeba7c3
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-15 16:50:54 +0200

    Add descriptions

--- src/shared/dto/ownable.dto.ts
+++ src/shared/dto/ownable.dto.ts
@@ -1,15 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class OwnableDto {
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'Defines the group which owns the data, and therefore has unrestricted access to this data. Usually a pgroup like p12151',
+  })
   readonly ownerGroup: string;
 
-  @ApiProperty({ type: [String] })
+  @ApiProperty({
+    type: [String],
+    description:
+      "Optional additional groups which have read access to the data. Users which are member in one of the groups listed here are allowed to access this data. The special group 'public' makes data available to all users",
+  })
   readonly accessGroups: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'Functional or user account name who created this instance',
+  })
   readonly createdBy: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'Functional or user account name who last updated this instance',
+  })
   readonly updatedBy: string;
 }
