8eb1b537aea083d63595d64e1f047374810c7543
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-15 16:51:30 +0200

    Set properties as required and add descriptions

--- src/auth/dto/credentials.dto.ts
+++ src/auth/dto/credentials.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CredentialsDto {
-  @ApiProperty()
+  @ApiProperty({ required: true, description: 'The username of the user' })
   readonly username: string;
 
-  @ApiProperty()
+  @ApiProperty({ required: true, description: 'The password of the user' })
   readonly password: string;
 }
