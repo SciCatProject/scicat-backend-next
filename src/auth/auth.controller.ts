8e59f218594d55554fd481bff79ee7861b7438d9
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-20 11:05:27 +0200

    Implement local auth guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,10 +6,9 @@ import {
   Post,
   Get,
 } from '@nestjs/common';
-import { LocalAuthGuard } from './guards/local-auth.guards';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { AuthGuard } from '@nestjs/passport';
 import { ApiTags } from '@nestjs/swagger';
 import { CredentialsDto } from './dto/credentials.dto';
 
@@ -18,13 +17,14 @@ import { CredentialsDto } from './dto/credentials.dto';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Body() credentials: CredentialsDto): Promise<any> {
     return await this.authService.login(credentials);
   }
 
-  @Get('whoami')
   @UseGuards(JwtAuthGuard)
+  @Get('whoami')
   async whoami(@Request() req: any): Promise<any> {
     const { password, ...user } = req.user;
     console.log(user);
