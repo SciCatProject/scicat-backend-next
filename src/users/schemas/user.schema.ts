b7c735907a3c0e72b13afc4b1f180518f4cf2b84
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-20 10:47:47 +0200

    Add user schema

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema({
+  collection: 'User',
+})
+export class User {
+  @ApiProperty()
+  @Prop()
+  realm: string;
+
+  @ApiProperty()
+  @Prop({ required: true })
+  username: string;
+
+  @ApiProperty()
+  @Prop({ required: true })
+  password: string;
+
+  @ApiProperty()
+  @Prop({ required: true })
+  email: string;
+
+  @ApiProperty()
+  @Prop()
+  emailVerified: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
