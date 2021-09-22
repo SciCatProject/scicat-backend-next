e2decd48983bbed22558bef85ae1492af6cc3102
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-22 14:27:41 +0200

    Automatically create functional accounts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
+import { genSalt, hash } from 'bcrypt';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+export class UsersService implements OnModuleInit {
+  constructor(
+    private configService: ConfigService,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
+
+  async onModuleInit() {
+    const functionalAccounts =
+      this.configService.get<User[]>('functionalAccounts');
+
+    if (functionalAccounts && functionalAccounts.length > 0) {
+      functionalAccounts.forEach(async (account) => {
+        await this.create(account);
+      });
+    }
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
+    const userExists = await this.userModel.exists({
+      $or: [
+        { username: createUserDto.username },
+        { email: createUserDto.email },
+      ],
+    });
+
+    if (userExists) {
+      return null;
+    }
+
+    Logger.log(
+      `Creating functional account ${createUserDto.username}`,
+      'UsersService',
+    );
+    const hashedPassword = await hash(createUserDto.password, await genSalt());
+    const createUser = { ...createUserDto, password: hashedPassword };
+    const createdUser = new this.userModel(createUser);
     return createdUser.save();
   }
 
