7246a534c8aa96f2a1fb992630ea1b3bfee66321
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-28 09:29:10 +0200

    Add user identity and user controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import {
+  UserIdentity,
+  UserIdentitySchema,
+} from './schemas/user-identity.schema';
 
 @Module({
   imports: [
@@ -12,9 +17,14 @@ import { UsersService } from './users.service';
         name: User.name,
         schema: UserSchema,
       },
+      {
+        name: UserIdentity.name,
+        schema: UserIdentitySchema,
+      },
     ]),
   ],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
