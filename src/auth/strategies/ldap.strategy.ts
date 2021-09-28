e00e5262278f0f2627ebad39dad6937bcf171923
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-28 09:27:21 +0200

    Add user creation to validate method

--- src/auth/strategies/ldap.strategy.ts
+++ src/auth/strategies/ldap.strategy.ts
@@ -2,16 +2,58 @@ import * as Strategy from 'passport-ldapauth';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { CreateUserIdentityDto } from 'src/users/dto/create-user-identity.dto';
 
 @Injectable()
 export class LdapStrategy extends PassportStrategy(Strategy, 'ldap') {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+  ) {
     super(configService.get<Record<string, any>>('ldap'));
   }
 
   async validate(payload: any) {
+    const userExists = await this.usersService.userExists({
+      $or: [
+        { username: `ldap.${payload.displayName}` },
+        { email: payload.mail },
+      ],
+    });
+
+    if (!userExists) {
+      const createUser: CreateUserDto = {
+        username: `ldap.${payload.displayName}`,
+        email: payload.mail,
+      };
+      const user = await this.usersService.create(createUser);
+      const createUserIdentity: CreateUserIdentityDto = {
+        authScheme: 'ldap',
+        created: new Date(),
+        credentials: {},
+        externalId: payload.sAMAccountName,
+        modified: new Date(),
+        profile: {
+          displayName: payload.displayName,
+          email: payload.mail,
+          username: payload.displayName,
+          thumbnailPhoto:
+            'data:image/jpeg;base64,' +
+            Buffer.from(payload.thumbnailPhoto, 'binary').toString('base64'),
+          emails: [],
+          accessGroups: [],
+          id: payload.sAMAccountName,
+        },
+        provider: 'ldap',
+        userId: user._id,
+      };
+      await this.usersService.createUserIdentity(createUserIdentity);
+    }
+
     return {
-      username: payload.sAMAccountName,
+      username: `ldap.${payload.displayName}`,
       email: payload.mail,
     };
   }
