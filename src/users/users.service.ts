32827df60b0ef240c7b3ad6ec0c3b89de1d9e6c9
Henrik Johansson <henrik.johansson712@gmail.com>
2021-09-30 11:37:23 +0200

    Set up automatic role creation for funtional accounts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,16 +5,20 @@ import { genSalt, hash } from 'bcrypt';
 import { FilterQuery, Model } from 'mongoose';
 import { CreateUserIdentityDto } from './dto/create-user-identity.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { RolesService } from './roles.service';
 import {
   UserIdentity,
   UserIdentityDocument,
 } from './schemas/user-identity.schema';
 import { User, UserDocument } from './schemas/user.schema';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { CreateUserRoleDto } from './dto/create-user-role.dto';
 
 @Injectable()
 export class UsersService implements OnModuleInit {
   constructor(
     private configService: ConfigService,
+    private rolesService: RolesService,
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     @InjectModel(UserIdentity.name)
     private userIdentityModel: Model<UserIdentityDocument>,
@@ -22,11 +26,43 @@ export class UsersService implements OnModuleInit {
 
   async onModuleInit() {
     const functionalAccounts =
-      this.configService.get<User[]>('functionalAccounts');
+      this.configService.get<CreateUserDto[]>('functionalAccounts');
 
     if (functionalAccounts && functionalAccounts.length > 0) {
       functionalAccounts.forEach(async (account) => {
-        await this.create(account);
+        const { role, global, ...createAccount } = account;
+        const user = await this.findOrCreate(createAccount);
+
+        if (role) {
+          const createRole: CreateRoleDto = {
+            name: role,
+            created: new Date(),
+            modified: new Date(),
+          };
+          const createdRole = await this.rolesService.findOrCreate(createRole);
+          if (createdRole) {
+            const createUserRole: CreateUserRoleDto = {
+              userId: user._id,
+              roleId: createdRole._id,
+            };
+            await this.rolesService.findOrCreateUserRole(createUserRole);
+          }
+        }
+        if (global) {
+          const createRole: CreateRoleDto = {
+            name: 'globalaccess',
+            created: new Date(),
+            modified: new Date(),
+          };
+          const createdRole = await this.rolesService.findOrCreate(createRole);
+          if (createdRole) {
+            const createUserRole: CreateUserRoleDto = {
+              userId: user._id,
+              roleId: createdRole._id,
+            };
+            await this.rolesService.findOrCreateUserRole(createUserRole);
+          }
+        }
       });
     }
   }
@@ -36,17 +72,6 @@ export class UsersService implements OnModuleInit {
   }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const userExists = await this.userExists({
-      $or: [
-        { username: createUserDto.username },
-        { email: createUserDto.email },
-      ],
-    });
-
-    if (userExists) {
-      return null;
-    }
-
     Logger.log(`Creating user ${createUserDto.username}`, 'UsersService');
 
     if (!createUserDto.password && createUserDto.username.startsWith('ldap')) {
@@ -58,6 +83,22 @@ export class UsersService implements OnModuleInit {
     const createUser = { ...createUserDto, password: hashedPassword };
     const createdUser = new this.userModel(createUser);
     return createdUser.save();
+  }
+
+  async findOrCreate(createUserDto: CreateUserDto): Promise<User> {
+    const userFilter: FilterQuery<UserDocument> = {
+      $or: [
+        { username: createUserDto.username },
+        { email: createUserDto.email },
+      ],
+    };
+    const userExists = await this.userExists(userFilter);
+
+    if (userExists) {
+      return await this.findOne(userFilter);
+    }
+
+    return await this.create(createUserDto);
   }
 
   async findOne(filter: FilterQuery<UserDocument>): Promise<User | undefined> {
