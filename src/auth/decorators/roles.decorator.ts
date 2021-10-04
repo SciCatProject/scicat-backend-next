3425ca77ca63a470b7aa0b5d5f9c8a2348f17be2
Henrik Johansson <henrik.johansson712@gmail.com>
2021-10-04 16:25:17 +0200

    Fix typo

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { Role } from '../role.enum';
 
 export const ROLES_KEY = 'roles';
-export const roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
