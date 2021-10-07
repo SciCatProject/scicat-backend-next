761ca44dd18690b83e9e9736e11ca31641e710b5
Henrik Johansson <henrik.johansson712@gmail.com>
2021-10-07 16:08:12 +0200

    Combine roles and accessGroups to currentGroups

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,10 +3,14 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from '../constants';
 import { RolesService } from 'src/users/roles.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private rolesService: RolesService) {
+  constructor(
+    private rolesService: RolesService,
+    private usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -17,13 +21,27 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   async validate(payload: any) {
     console.log({ payload });
     const roles = await this.rolesService.find({ userId: payload._id });
-    const roleNames = roles.map(({ name }) => name);
+
+    const userIdentity = await this.usersService.findByIdUserIdentity(
+      payload._id,
+    );
+
+    let currentGroups = [];
+
+    if (roles) {
+      const roleNames = roles.map(({ name }) => name);
+      currentGroups = currentGroups.concat(roleNames);
+    }
+
+    if (userIdentity) {
+      currentGroups = currentGroups.concat(userIdentity.profile.accessGroups);
+    }
 
     return {
       _id: payload._id,
       username: payload.username,
       email: payload.email,
-      roles: roleNames ?? [],
+      currentGroups,
     };
   }
 }
