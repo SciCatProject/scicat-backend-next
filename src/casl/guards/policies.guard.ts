6be83fc0b4b451d0f8668210604b5415df5bdeb6
Henrik Johansson <henrik.johansson712@gmail.com>
2021-10-07 16:11:15 +0200

    Create policies guard and check policies decorator

---
+++ src/casl/guards/policies.guard.ts
@@ -0,0 +1,39 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Dataset } from 'src/datasets/schemas/dataset.schema';
+import { Action } from '../action.enum';
+import { AppAbility, CaslAbilityFactory } from '../casl-ability.factory';
+import { CHECK_POLICIES_KEY } from '../decorators/check-policies.decorator';
+import { PolicyHandler } from '../interfaces/policy-handler.interface';
+
+@Injectable()
+export class PoliciesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const policyHandlers =
+      this.reflector.get<PolicyHandler[]>(
+        CHECK_POLICIES_KEY,
+        context.getHandler(),
+      ) || [];
+
+    const { user } = context.switchToHttp().getRequest();
+    const ability = this.caslAbilityFactory.createForUser(user);
+    const canCreateDataset = ability.can(Action.Create, Dataset);
+    console.log({ canCreateDataset });
+
+    return policyHandlers.every((handler) =>
+      this.execPolicyHandler(handler, ability),
+    );
+  }
+
+  private execPolicyHandler(handler: PolicyHandler, ability: AppAbility) {
+    if (typeof handler === 'function') {
+      return handler(ability);
+    }
+    return handler.handle(ability);
+  }
+}
