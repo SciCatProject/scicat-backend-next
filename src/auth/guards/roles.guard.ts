50bb64afb99e1c76fed9a52b3b520db1a1b72fc9
Henrik Johansson <henrik.johansson712@gmail.com>
2021-10-07 16:08:33 +0200

    Combine roles and accessGroups to currentGroups

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -18,6 +18,6 @@ export class RolesGuard implements CanActivate {
     }
 
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles.includes(role));
+    return requiredRoles.some((role) => user.currentGroups.includes(role));
   }
 }
