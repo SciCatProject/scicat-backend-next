8b87aa7af13b742314e8e992cb960a59a27a7bbc
Henrik Johansson <henrik.johansson712@gmail.com>
2021-10-21 14:14:11 +0200

    Add dtos for updating raw and derived datasets

---
+++ src/datasets/dto/update-raw-dataset.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateRawDatasetDto } from './create-raw-dataset.dto';
+
+export class UpdateRawDatasetDto extends PartialType(CreateRawDatasetDto) {}
