5ad1cc1366f5e03fcbb8834c0bf92b8d428b92ca
Henrik Johansson <henrik.johansson712@gmail.com>
2021-10-26 11:20:40 +0200

    Update jwt auth guard to check for allow-any in header

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
+    const allowAny = this.reflector.get<string[]>(
+      'allow-any',
+      context.getHandler(),
+    );
+
+    if (user) {
+      return user;
+    }
+    if (allowAny) {
+      return null;
+    }
+    throw new UnauthorizedException();
+  }
+}
