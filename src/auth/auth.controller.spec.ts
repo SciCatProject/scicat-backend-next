343212d8a8e2bb50d5850522d34a3ce5b0ef646d
Henrik Johansson <henrik.johansson712@gmail.com>
2021-11-16 14:07:02 +0100

    Update providers

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,4 +1,6 @@
+import { JwtService } from "@nestjs/jwt";
 import { Test, TestingModule } from "@nestjs/testing";
+import { UsersService } from "src/users/users.service";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
@@ -9,7 +11,7 @@ describe("AuthController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [AuthService, UsersService, JwtService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
