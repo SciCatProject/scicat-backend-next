64807a89fcd350900d0ce038fc7d439781b01744
Henrik Johansson <henrik.johansson712@gmail.com>
2021-11-16 13:50:28 +0100

    Update tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,23 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 describe("AuthController", () => {
   let controller: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [AuthService],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,26 @@
+import { JwtService } from "@nestjs/jwt";
 import { Test, TestingModule } from "@nestjs/testing";
+import { UsersService } from "src/users/users.service";
 import { AuthService } from "./auth.service";
 
 describe("AuthService", () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let jwtService: JwtService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, JwtService, UsersService],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it("should be defined", () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+    expect(jwtService).toBeDefined();
+    expect(usersService).toBeDefined();
   });
 });
