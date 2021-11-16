660ca1a068a16a6c0e27053901ee812821807a11
Henrik Johansson <henrik.johansson712@gmail.com>
2021-11-16 11:20:53 +0100

    Add attachments service and datasets service to providers

--- src/datasets/datasets.controller.spec.ts
+++ src/datasets/datasets.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { AttachmentsService } from "src/attachments/attachments.service";
 import { DatasetsController } from "./datasets.controller";
+import { DatasetsService } from "./datasets.service";
 
 describe("DatasetsController", () => {
   let controller: DatasetsController;
@@ -7,6 +9,7 @@ describe("DatasetsController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DatasetsController],
+      providers: [AttachmentsService, DatasetsService],
     }).compile();
 
     controller = module.get<DatasetsController>(DatasetsController);
