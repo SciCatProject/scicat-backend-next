1964974313b2149d827472dbeb9ebd229a381536
Henrik Johansson <henrik.johansson712@gmail.com>
2021-11-17 14:30:30 +0100

    Add fields from dataset schema

--- src/datasets/schemas/raw-dataset.schema.ts
+++ src/datasets/schemas/raw-dataset.schema.ts
@@ -1,14 +1,36 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { ApiProperty } from "@nestjs/swagger";
+import { Lifecycle } from "./lifecycle.schema";
+import { Technique } from "./technique.schema";
 
 @Schema()
 export class RawDataset {
-  _id: string;
+  pid: string;
   owner: string;
+  ownerEmail: string;
+  orcidOfOwner: string;
   contactEmail: string;
   sourceFolder: string;
+  sourceFolderHost: string;
+  size: number;
+  packedSize: number;
+  numberOfFiles: number;
+  numberOfFilesArchived: number;
   creationTime: Date;
   type: string;
+  validationStatus: string;
+  keywords: string[];
+  description: string;
+  datasetName: string;
+  classification: string;
+  license: string;
+  version: string;
+  isPublished: boolean;
+  history: Record<string, unknown>;
+  datasetLifeCycle: Lifecycle;
+  createdAt: Date;
+  updatedAt: Date;
+  techniques: Technique[];
 
   @ApiProperty()
   @Prop({ type: String, required: true })
