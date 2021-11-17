92eac88a3681f91ac25ec6929a57707aadcea8e5
Henrik Johansson <henrik.johansson712@gmail.com>
2021-11-17 16:20:40 +0100

    Set up providers

--- src/attachments/attachments.service.spec.ts
+++ src/attachments/attachments.service.spec.ts
@@ -1,15 +1,45 @@
+import { getModelToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
+import { Model } from "mongoose";
 import { AttachmentsService } from "./attachments.service";
+import { Attachment } from "./schemas/attachment.schema";
+
+const mockAttachment: Attachment = {
+  _id: "testId",
+  thumbnail: "testThumbnail",
+  caption: "Test caption",
+  datasetId: "testDatasetId",
+  ownerGroup: "test",
+  accessGroups: ["accessTest"],
+  createdBy: "testUser",
+  updatedBy: "testUser",
+};
 
 describe("AttachmentsService", () => {
   let service: AttachmentsService;
+  let attachmentModel: Model<Attachment>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AttachmentsService],
+      providers: [
+        AttachmentsService,
+        {
+          provide: getModelToken("Attachment"),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockAttachment),
+            constructor: jest.fn().mockResolvedValue(mockAttachment),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AttachmentsService>(AttachmentsService);
+    attachmentModel = module.get<Model<Attachment>>(
+      getModelToken("Attachment"),
+    );
   });
 
   it("should be defined", () => {
