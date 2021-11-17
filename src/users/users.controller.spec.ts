938fffbe9761f81a35db9413c28194bda5371ea2
Henrik Johansson <henrik.johansson712@gmail.com>
2021-11-17 11:26:26 +0100

    Set up imports and providers

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { CaslModule } from "src/casl/casl.module";
 import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+
+class UsersServiceMock {
+  findByIdUserIdentity(id: string) {
+    return { id };
+  }
+}
 
 describe("UsersController", () => {
   let controller: UsersController;
@@ -7,6 +15,8 @@ describe("UsersController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      imports: [CaslModule],
+      providers: [{ provide: UsersService, useClass: UsersServiceMock }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
