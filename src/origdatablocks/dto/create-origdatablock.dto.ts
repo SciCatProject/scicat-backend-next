4052e28e82241ea090ba8218d4e4025abf988b79
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-12 13:56:55 +0100

    Add create origdatablock dto

--- src/origdatablocks/dto/create-origdatablock.dto.ts
+++ src/origdatablocks/dto/create-origdatablock.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOrigdatablockDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { DataFile } from "../../common/schemas/datafile.schema";
+
+export class CreateOrigdatablockDto {
+  @ApiProperty()
+  readonly datasetId: string;
+
+  @ApiProperty()
+  ownerGroup: string;
+
+  @ApiProperty({ type: [String] })
+  accessGroups: string[];
+
+  @ApiProperty()
+  readonly size: number;
+
+  @ApiProperty({ type: [DataFile] })
+  dataFileList: DataFile[];
+}
