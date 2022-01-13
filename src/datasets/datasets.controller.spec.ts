8e837744b448744e33c449c24d30ffd23bfc6276
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-13 09:35:06 +0100

    Add mock datablocks and origdatablocks services

--- src/datasets/datasets.controller.spec.ts
+++ src/datasets/datasets.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AttachmentsService } from "src/attachments/attachments.service";
 import { CaslModule } from "src/casl/casl.module";
+import { DatablocksService } from "src/datablocks/datablocks.service";
+import { OrigdatablocksService } from "src/origdatablocks/origdatablocks.service";
 import { DatasetsController } from "./datasets.controller";
 import { DatasetsService } from "./datasets.service";
 
 class AttachmentsServiceMock {}
 
+class DatablocksServiceMock {}
+
 class DatasetsServiceMock {}
+
+class OrigDatablocksServiceMock {}
 
 describe("DatasetsController", () => {
   let controller: DatasetsController;
@@ -17,7 +23,9 @@ describe("DatasetsController", () => {
       imports: [CaslModule],
       providers: [
         { provide: AttachmentsService, useClass: AttachmentsServiceMock },
+        { provide: DatablocksService, useClass: DatablocksServiceMock },
         { provide: DatasetsService, useClass: DatasetsServiceMock },
+        { provide: OrigdatablocksService, useClass: OrigDatablocksServiceMock },
       ],
     }).compile();
 
