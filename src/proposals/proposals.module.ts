f5e9c19dcdbd8e6ae3b71cfbef720d11d4f2fc20
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-18 09:44:52 +0100

    Set up proposal schema

--- src/proposals/proposals.module.ts
+++ src/proposals/proposals.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { ProposalsService } from './proposals.service';
-import { ProposalsController } from './proposals.controller';
+import { Module } from "@nestjs/common";
+import { ProposalsService } from "./proposals.service";
+import { ProposalsController } from "./proposals.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Proposal, ProposalSchema } from "./schemas/proposal.schema";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Proposal.name,
+        schema: ProposalSchema,
+      },
+    ]),
+  ],
   controllers: [ProposalsController],
-  providers: [ProposalsService]
+  providers: [ProposalsService],
 })
 export class ProposalsModule {}
--- src/proposals/schemas/proposal.schema.ts
+++ src/proposals/schemas/proposal.schema.ts
@@ -1,1 +1,52 @@
-export class Proposal {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+import { Document } from "mongoose";
+import { Ownable } from "src/common/schemas/ownable.schema";
+
+export type ProposalDocument = Proposal & Document;
+@Schema({
+  collection: "Proposal",
+})
+export class Proposal extends Ownable {
+  @ApiProperty()
+  @Prop({ type: String, index: true })
+  pi_email: string;
+
+  @ApiProperty()
+  @Prop()
+  pi_firstname: string;
+
+  @ApiProperty()
+  @Prop()
+  pi_lastname: string;
+
+  @ApiProperty()
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @ApiProperty()
+  @Prop()
+  firstname: string;
+
+  @ApiProperty()
+  @Prop()
+  lastname: string;
+
+  @ApiProperty()
+  @Prop()
+  title: string;
+
+  @ApiProperty()
+  @Prop()
+  abstract: string;
+
+  @ApiProperty()
+  @Prop({ type: Date })
+  startTime: Date;
+
+  @ApiProperty()
+  @Prop({ type: Date })
+  endTime: Date;
+}
+
+export const ProposalSchema = SchemaFactory.createForClass(Proposal);
