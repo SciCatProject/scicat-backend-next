39d92f7c9a3833d8c4c3a110c4b028865a938b0d
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-18 09:48:22 +0100

    Set up create proposal dto

--- src/proposals/dto/create-proposal.dto.ts
+++ src/proposals/dto/create-proposal.dto.ts
@@ -1,1 +1,39 @@
-export class CreateProposalDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateProposalDto {
+  @ApiProperty()
+  readonly ownerGroup: string;
+
+  @ApiProperty()
+  readonly accessGroups: string[];
+
+  @ApiProperty()
+  readonly pi_email: string;
+
+  @ApiProperty()
+  readonly pi_firstname: string;
+
+  @ApiProperty()
+  readonly pi_lastname: string;
+
+  @ApiProperty()
+  readonly email: string;
+
+  @ApiProperty()
+  readonly firstname: string;
+
+  @ApiProperty()
+  readonly lastname: string;
+
+  @ApiProperty()
+  readonly title: string;
+
+  @ApiProperty()
+  readonly abstract: string;
+
+  @ApiProperty()
+  readonly startTime: Date;
+
+  @ApiProperty()
+  readonly endTime: Date;
+}
