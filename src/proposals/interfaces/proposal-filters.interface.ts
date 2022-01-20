99c05358bbe6eedf3d1eb06cf16617ca9be1f028
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-20 13:57:40 +0100

    Add interfaces for proposal filters

---
+++ src/proposals/interfaces/proposal-filters.interface.ts
@@ -0,0 +1,43 @@
+export enum ProposalField {
+  Text = "text",
+  StartTime = "startTime",
+  EndTime = "endTime",
+  ProposalId = "proposalId",
+  Title = "title",
+  Firstname = "firstName",
+  Lastname = "lastName",
+}
+
+interface IProposalFieldObject {
+  $regex: string;
+  $options: string;
+}
+
+interface IDateRange {
+  begin: string;
+  end: string;
+}
+
+export interface IProposalFields {
+  text?: string;
+  startTime?: IDateRange;
+  proposalId?: IProposalFieldObject;
+  title?: IProposalFieldObject;
+  firstname?: IProposalFieldObject;
+  lastname?: IProposalFieldObject;
+  endTime?: IDateRange;
+}
+
+export interface IProposalFilters {
+  fields?: IProposalFields;
+  limits?: {
+    skip: number;
+    limit: number;
+    order: string;
+  };
+}
+
+export interface IProposalFacets {
+  fields?: IProposalFields;
+  facets?: string[];
+}
