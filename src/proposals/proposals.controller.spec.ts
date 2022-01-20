d896bc4cc3c466e17886fb2ac89184e217e6ce13
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-20 14:29:55 +0100

    Provide mock datasets service

--- src/proposals/proposals.controller.spec.ts
+++ src/proposals/proposals.controller.spec.ts
@@ -1,10 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AttachmentsService } from "src/attachments/attachments.service";
 import { CaslModule } from "src/casl/casl.module";
+import { DatasetsService } from "src/datasets/datasets.service";
 import { ProposalsController } from "./proposals.controller";
 import { ProposalsService } from "./proposals.service";
 
 class AttachmentsServiceMock {}
+
+class DatasetsServiceMock {}
 
 class ProposalsServiceMock {}
 
@@ -17,6 +20,7 @@ describe("ProposalsController", () => {
       imports: [CaslModule],
       providers: [
         { provide: AttachmentsService, useClass: AttachmentsServiceMock },
+        { provide: DatasetsService, useClass: DatasetsServiceMock },
         { provide: ProposalsService, useClass: ProposalsServiceMock },
       ],
     }).compile();
