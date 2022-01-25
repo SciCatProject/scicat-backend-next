fd39b156123ba09953655d05bf5741d49aac69dc
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-25 14:57:22 +0100

    Set up sample schema

--- src/samples/samples.module.ts
+++ src/samples/samples.module.ts
@@ -1,8 +1,34 @@
 import { Module } from "@nestjs/common";
 import { SamplesService } from "./samples.service";
 import { SamplesController } from "./samples.controller";
+import { AttachmentsModule } from "src/attachments/attachments.module";
+import { DatasetsModule } from "src/datasets/datasets.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Sample, SampleSchema } from "./schemas/sample.schema";
 
 @Module({
+  imports: [
+    AttachmentsModule,
+    DatasetsModule,
+    MongooseModule.forFeatureAsync([
+      {
+        name: Sample.name,
+        useFactory: () => {
+          const schema = SampleSchema;
+
+          schema.pre<Sample>("save", function (next) {
+            // if _id is empty or different than sampleId,
+            // set _id to sampleId
+            if (!this._id) {
+              this._id = this.sampleId;
+            }
+            next();
+          });
+          return schema;
+        },
+      },
+    ]),
+  ],
   controllers: [SamplesController],
   providers: [SamplesService],
 })
--- src/samples/schemas/sample.schema.ts
+++ src/samples/schemas/sample.schema.ts
@@ -1,1 +1,57 @@
-export class Sample {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { ApiProperty, getSchemaPath } from "@nestjs/swagger";
+import { Document } from "mongoose";
+import {
+  Attachment,
+  AttachmentSchema,
+} from "src/attachments/schemas/attachment.schema";
+import { Ownable } from "src/common/schemas/ownable.schema";
+import { Dataset, DatasetSchema } from "src/datasets/schemas/dataset.schema";
+import { v4 as uuidv4 } from "uuid";
+
+export type SampleDocument = Sample & Document;
+
+@Schema({
+  collection: "Sample",
+})
+export class Sample extends Ownable {
+  @Prop({ type: String, unique: true })
+  _id: string;
+
+  @ApiProperty({
+    type: String,
+    default: uuidv4(),
+  })
+  @Prop({ type: String, unique: true, default: uuidv4() })
+  sampleId: string;
+
+  @ApiProperty()
+  @Prop()
+  owner: string;
+
+  @ApiProperty()
+  @Prop()
+  description: string;
+
+  @ApiProperty()
+  @Prop({ type: Date })
+  createdAt: Date;
+
+  @ApiProperty()
+  @Prop({ type: Object })
+  sampleCharacteristics: Record<string, unknown>;
+
+  @ApiProperty()
+  @Prop({ type: Boolean, default: false })
+  isPublished: boolean;
+
+  @ApiProperty({ type: "array", items: { $ref: getSchemaPath(Attachment) } })
+  @Prop([AttachmentSchema])
+  attachments: Attachment[];
+
+  @ApiProperty({ type: "array", items: { $ref: getSchemaPath(Dataset) } })
+  @Prop([DatasetSchema])
+  datasets: Dataset[];
+}
+
+export const SampleSchema = SchemaFactory.createForClass(Sample);
