feed92abeb2476aaff10fb368d3324be43b6ad07
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-25 15:01:01 +0100

    Set up create sample dto

--- src/samples/dto/create-sample.dto.ts
+++ src/samples/dto/create-sample.dto.ts
@@ -1,1 +1,24 @@
-export class CreateSampleDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateSampleDto {
+  @ApiProperty()
+  readonly ownerGroup: string;
+
+  @ApiProperty()
+  readonly accessGroups: string[];
+
+  @ApiProperty()
+  readonly owner: string;
+
+  @ApiProperty()
+  readonly description: string;
+
+  @ApiProperty()
+  readonly createdAt: Date;
+
+  @ApiProperty()
+  readonly sampleCharacteristics: Record<string, unknown>;
+
+  @ApiProperty()
+  readonly isPublished: boolean;
+}
