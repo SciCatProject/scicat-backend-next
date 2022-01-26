5c1ae246b2efbf4d6dafff7dc26c83651474c874
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-26 10:31:09 +0100

    Set up crud endpoints

--- src/samples/samples.controller.ts
+++ src/samples/samples.controller.ts
@@ -6,37 +6,69 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Query,
 } from "@nestjs/common";
 import { SamplesService } from "./samples.service";
 import { CreateSampleDto } from "./dto/create-sample.dto";
 import { UpdateSampleDto } from "./dto/update-sample.dto";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { PoliciesGuard } from "src/casl/guards/policies.guard";
+import { CheckPolicies } from "src/casl/decorators/check-policies.decorator";
+import { AppAbility } from "src/casl/casl-ability.factory";
+import { Action } from "src/casl/action.enum";
+import { Sample, SampleDocument } from "./schemas/sample.schema";
+import { FilterQuery } from "mongoose";
 
+@ApiBearerAuth()
+@ApiTags("samples")
 @Controller("samples")
 export class SamplesController {
   constructor(private readonly samplesService: SamplesService) {}
 
+  // POST /samples
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Create, Sample))
   @Post()
-  create(@Body() createSampleDto: CreateSampleDto) {
+  async create(@Body() createSampleDto: CreateSampleDto): Promise<Sample> {
     return this.samplesService.create(createSampleDto);
   }
 
+  // GET /samples
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Sample))
   @Get()
-  findAll() {
-    return this.samplesService.findAll();
+  async findAll(
+    @Query() filters?: FilterQuery<SampleDocument>,
+  ): Promise<Sample[]> {
+    const sampleFilters = filters ?? {};
+    return this.samplesService.findAll(sampleFilters);
   }
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.samplesService.findOne(+id);
+  // GET /samples/:id
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Sample))
+  @Get("/:id")
+  async findOne(@Param("id") id: string): Promise<Sample | null> {
+    return this.samplesService.findOne({ sampleId: id });
   }
 
-  @Patch(":id")
-  update(@Param("id") id: string, @Body() updateSampleDto: UpdateSampleDto) {
-    return this.samplesService.update(+id, updateSampleDto);
+  // PATCH /samples/:id
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Update, Sample))
+  @Patch("/:id")
+  async update(
+    @Param("id") id: string,
+    @Body() updateSampleDto: UpdateSampleDto,
+  ): Promise<Sample | null> {
+    return this.samplesService.update({ sampleId: id }, updateSampleDto);
   }
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.samplesService.remove(+id);
+  // DELETE /samples/:id
+  @UseGuards()
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Delete, Sample))
+  @Delete("/:id")
+  async remove(@Param("id") id: string): Promise<unknown> {
+    return this.samplesService.remove({ sampleId: id });
   }
 }
