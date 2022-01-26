cc9c038f41127dcb010fc3688f1104fa48259018
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-26 09:45:48 +0100

    Set up samples service

--- src/samples/samples.service.ts
+++ src/samples/samples.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { FilterQuery, Model } from "mongoose";
 import { CreateSampleDto } from "./dto/create-sample.dto";
 import { UpdateSampleDto } from "./dto/update-sample.dto";
+import { Sample, SampleDocument } from "./schemas/sample.schema";
 
 @Injectable()
 export class SamplesService {
-  create(createSampleDto: CreateSampleDto) {
-    return "This action adds a new sample";
+  constructor(
+    @InjectModel(Sample.name) private sampleModel: Model<SampleDocument>,
+  ) {}
+
+  async create(createSampleDto: CreateSampleDto): Promise<Sample> {
+    const createdSample = new this.sampleModel(createSampleDto);
+    return createdSample.save();
   }
 
-  findAll() {
-    return `This action returns all samples`;
+  async findAll(filter: FilterQuery<SampleDocument>): Promise<Sample[]> {
+    return this.sampleModel.find(filter).exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} sample`;
+  async findOne(filter: FilterQuery<SampleDocument>): Promise<Sample | null> {
+    return this.sampleModel.findOne(filter).exec();
   }
 
-  update(id: number, updateSampleDto: UpdateSampleDto) {
-    return `This action updates a #${id} sample`;
+  async update(
+    filter: FilterQuery<SampleDocument>,
+    updateSampleDto: UpdateSampleDto,
+  ): Promise<Sample | null> {
+    return this.sampleModel
+      .findOneAndUpdate(filter, updateSampleDto, { new: true })
+      .exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} sample`;
+  async remove(filter: FilterQuery<SampleDocument>): Promise<unknown> {
+    return this.sampleModel.findOneAndRemove(filter).exec();
   }
 }
