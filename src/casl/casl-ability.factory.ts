72164a7417e9401bce1ba8f99a859d975c138a03
Henrik Johansson <henrik.johansson712@gmail.com>
2022-01-26 16:42:35 +0100

    Update sample casl

--- src/casl/casl-ability.factory.ts
+++ src/casl/casl-ability.factory.ts
@@ -65,6 +65,7 @@ export class CaslAbilityFactory {
     can(Action.Read, Proposal, { ownerGroup: { $in: user.currentGroups } });
     can(Action.Read, Proposal, { accessGroups: { $in: user.currentGroups } });
 
+    can(Action.Create, Sample);
     can(Action.Read, Sample, { ownerGroup: { $in: user.currentGroups } });
     can(Action.Read, Sample, { accessGroups: { $in: user.currentGroups } });
 
