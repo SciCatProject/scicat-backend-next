63e7129e5e4daf544e6e00e311403789b2888b45
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-15 11:32:51 +0100

    Set up published data endpoints

--- src/published-data/published-data.controller.ts
+++ src/published-data/published-data.controller.ts
@@ -6,40 +6,89 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Query,
 } from "@nestjs/common";
 import { PublishedDataService } from "./published-data.service";
 import { CreatePublishedDataDto } from "./dto/create-published-data.dto";
 import { UpdatePublishedDataDto } from "./dto/update-published-data.dto";
+import { ApiBearerAuth, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { PoliciesGuard } from "src/casl/guards/policies.guard";
+import { CheckPolicies } from "src/casl/decorators/check-policies.decorator";
+import { AppAbility } from "src/casl/casl-ability.factory";
+import { Action } from "src/casl/action.enum";
+import { PublishedData } from "./schemas/published-data.schema";
+import { IPublishedDataFilters } from "./interfaces/published-data-filters.interface";
 
+@ApiBearerAuth()
+@ApiTags("publisheddata")
 @Controller("published-data")
 export class PublishedDataController {
   constructor(private readonly publishedDataService: PublishedDataService) {}
 
+  // POST /publisheddata
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Create, PublishedData),
+  )
   @Post()
-  create(@Body() createPublishedDataDto: CreatePublishedDataDto) {
+  async create(
+    @Body() createPublishedDataDto: CreatePublishedDataDto,
+  ): Promise<PublishedData> {
     return this.publishedDataService.create(createPublishedDataDto);
   }
 
+  // GET /publisheddata
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Read, PublishedData),
+  )
   @Get()
-  findAll() {
-    return this.publishedDataService.findAll();
+  @ApiQuery({
+    name: "filter",
+    description: "Database filters to apply when retrieve all published data",
+    required: false,
+  })
+  async findAll(@Query("filter") filters?: string): Promise<PublishedData[]> {
+    const publishedDataFilters: IPublishedDataFilters = JSON.parse(
+      filters ?? "{}",
+    );
+    return this.publishedDataService.findAll(publishedDataFilters);
   }
 
-  @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.publishedDataService.findOne(+id);
+  // GET /publisheddata/:id
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Read, PublishedData),
+  )
+  @Get("/:id")
+  async findOne(@Param("id") id: string): Promise<PublishedData | null> {
+    return this.publishedDataService.findOne({ doi: id });
   }
 
-  @Patch(":id")
-  update(
+  // PATCH /publisheddata/:id
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Update, PublishedData),
+  )
+  @Patch("/:id")
+  async update(
     @Param("id") id: string,
     @Body() updatePublishedDataDto: UpdatePublishedDataDto,
-  ) {
-    return this.publishedDataService.update(+id, updatePublishedDataDto);
+  ): Promise<PublishedData | null> {
+    return this.publishedDataService.update(
+      { doi: id },
+      updatePublishedDataDto,
+    );
   }
 
-  @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.publishedDataService.remove(+id);
+  // DELETE /pubvlisheddata/:id
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Delete, PublishedData),
+  )
+  @Delete("/:id")
+  async remove(@Param("id") id: string): Promise<unknown> {
+    return this.publishedDataService.remove({ doi: id });
   }
 }
