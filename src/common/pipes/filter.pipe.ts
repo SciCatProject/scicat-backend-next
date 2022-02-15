e8b25a233f92c20f7c815dd396253365a66a6c1f
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-15 10:05:29 +0100

    Fix lint errors

--- src/common/pipes/filter.pipe.ts
+++ src/common/pipes/filter.pipe.ts
@@ -1,32 +1,41 @@
-
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata } from "@nestjs/common";
 
 @Injectable()
-export class FilterPipe implements PipeTransform<
-        {filter?: string, fields?: string}, 
-        {filter?: string, fields?: string}> {
-
-    transform(inValue: {filter?: string, fields?: string}, metadata: ArgumentMetadata): {filter?: string, fields?: string} {
-        /*
-         * intercept filter and make sure to convert loopback operators to mongo operators
-         */
-        console.log("Filter pipe ---------");
-        console.log(inValue);
-        const outValue = inValue;
-        if (inValue.filter) {
-            var filter = inValue.filter;
-            // subsitute the loopback operators to mongo equivalent
-            // nin => $in
-            filter = filter.replace(/{"inq":/g,'{"$in":');
-            // nin => $nin
-            filter = filter.replace(/{"nin":/g,'{"$nin":');
-            // and => $and
-            filter = filter.replace(/{"and":\[/g,'{"$and":[');
-            // and => $or
-            filter = filter.replace(/{"or":\[/g,'{"$or":[');
-            outValue.filter = filter;
-        } 
-        console.log("Output :" + JSON.stringify(outValue));
-        return outValue;
+export class FilterPipe
+  implements
+    PipeTransform<
+      { filter?: string; fields?: string },
+      { filter?: string; fields?: string }
+    >
+{
+  transform(
+    inValue: { filter?: string; fields?: string },
+    metadata: ArgumentMetadata,
+  ): { filter?: string; fields?: string } {
+    /*
+     * intercept filter and make sure to convert loopback operators to mongo operators
+     */
+    console.log("Filter pipe ---------");
+    console.log(inValue);
+    const outValue = inValue;
+    if (inValue.filter) {
+      let filter = inValue.filter;
+      // subsitute the loopback operators to mongo equivalent
+      // nin => $in
+      // eslint-disable-next-line @typescript-eslint/quotes
+      filter = filter.replace(/{"inq":/g, '{"$in":');
+      // nin => $nin
+      // eslint-disable-next-line @typescript-eslint/quotes
+      filter = filter.replace(/{"nin":/g, '{"$nin":');
+      // and => $and
+      // eslint-disable-next-line @typescript-eslint/quotes
+      filter = filter.replace(/{"and":\[/g, '{"$and":[');
+      // and => $or
+      // eslint-disable-next-line @typescript-eslint/quotes
+      filter = filter.replace(/{"or":\[/g, '{"$or":[');
+      outValue.filter = filter;
     }
+    console.log("Output :" + JSON.stringify(outValue));
+    return outValue;
+  }
 }
