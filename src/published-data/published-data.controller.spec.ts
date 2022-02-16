0d56700c52e9fa88374fdd5e333076bf5cc81fab
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-16 14:01:17 +0100

    Add mock services to providers

--- src/published-data/published-data.controller.spec.ts
+++ src/published-data/published-data.controller.spec.ts
@@ -1,7 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { AttachmentsService } from "src/attachments/attachments.service";
 import { CaslModule } from "src/casl/casl.module";
+import { DatasetsService } from "src/datasets/datasets.service";
+import { ProposalsService } from "src/proposals/proposals.service";
 import { PublishedDataController } from "./published-data.controller";
 import { PublishedDataService } from "./published-data.service";
+
+class AttachmentsServiceMock {}
+
+class DatasetsServiceMock {}
+
+class ProposalsServiceMock {}
 
 class PublishedDataServiceMock {}
 
@@ -13,6 +22,9 @@ describe("PublishedDataController", () => {
       controllers: [PublishedDataController],
       imports: [CaslModule],
       providers: [
+        { provide: AttachmentsService, useClass: AttachmentsServiceMock },
+        { provide: DatasetsService, useClass: DatasetsServiceMock },
+        { provide: ProposalsService, useClass: ProposalsServiceMock },
         { provide: PublishedDataService, useClass: PublishedDataServiceMock },
       ],
     }).compile();
