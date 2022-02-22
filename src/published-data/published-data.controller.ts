bc95186ab389d57ee8d873912174546d520699d9
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-22 11:22:14 +0100

    Add context to axios error handler

--- src/published-data/published-data.controller.ts
+++ src/published-data/published-data.controller.ts
@@ -293,7 +293,7 @@ export class PublishedDataController {
             }),
           );
         } catch (err) {
-          handleAxiosRequestError(err);
+          handleAxiosRequestError(err, "PublishedDataController.register");
           return null;
         }
 
@@ -305,7 +305,7 @@ export class PublishedDataController {
             }),
           );
         } catch (err) {
-          handleAxiosRequestError(err);
+          handleAxiosRequestError(err, "PublishedDataController.register");
           return null;
         }
 
@@ -333,7 +333,7 @@ export class PublishedDataController {
             }),
           );
         } catch (err) {
-          handleAxiosRequestError(err);
+          handleAxiosRequestError(err, "PublishedDataController.register");
           return null;
         }
 
@@ -398,7 +398,7 @@ export class PublishedDataController {
         }),
       );
     } catch (error) {
-      handleAxiosRequestError(error);
+      handleAxiosRequestError(error, "PublishedDataController.resync");
     }
 
     try {
