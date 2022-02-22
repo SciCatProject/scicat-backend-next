6153d09e50cb63a30a6b751897af557c2e091d17
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-22 11:40:46 +0100

    Set up imports and providers for logbook controller tests

--- src/logbooks/logbooks.controller.spec.ts
+++ src/logbooks/logbooks.controller.spec.ts
@@ -1,6 +1,9 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { CaslModule } from "src/casl/casl.module";
 import { LogbooksController } from "./logbooks.controller";
 import { LogbooksService } from "./logbooks.service";
+
+class LogbooksServiceMock {}
 
 describe("LogbooksController", () => {
   let controller: LogbooksController;
@@ -8,7 +11,8 @@ describe("LogbooksController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LogbooksController],
-      providers: [LogbooksService],
+      imports: [CaslModule],
+      providers: [{ provide: LogbooksService, useClass: LogbooksServiceMock }],
     }).compile();
 
     controller = module.get<LogbooksController>(LogbooksController);
