ceac12759027fff0fb33d29ca9397b7e6944c0d0
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-22 11:54:20 +0100

    Set up mock for interceptors providers

--- src/logbooks/logbooks.controller.spec.ts
+++ src/logbooks/logbooks.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { CaslModule } from "src/casl/casl.module";
+import { ProposalsService } from "src/proposals/proposals.service";
 import { LogbooksController } from "./logbooks.controller";
 import { LogbooksService } from "./logbooks.service";
 
 class LogbooksServiceMock {}
+
+class ProposalsServiceMock {}
 
 describe("LogbooksController", () => {
   let controller: LogbooksController;
@@ -12,7 +15,10 @@ describe("LogbooksController", () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LogbooksController],
       imports: [CaslModule],
-      providers: [{ provide: LogbooksService, useClass: LogbooksServiceMock }],
+      providers: [
+        { provide: LogbooksService, useClass: LogbooksServiceMock },
+        { provide: ProposalsService, useClass: ProposalsServiceMock },
+      ],
     }).compile();
 
     controller = module.get<LogbooksController>(LogbooksController);
