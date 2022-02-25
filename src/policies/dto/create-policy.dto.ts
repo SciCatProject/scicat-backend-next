b39d6cfb25819d84dbd253efe892f89fd7da3678
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-25 10:14:18 +0100

    Set up create policy dto

--- src/policies/dto/create-policy.dto.ts
+++ src/policies/dto/create-policy.dto.ts
@@ -1,1 +1,31 @@
-export class CreatePolicyDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { OwnableDto } from "src/common/dto/ownable.dto";
+
+export class CreatePolicyDto extends OwnableDto {
+  @ApiProperty({ type: [String] })
+  readonly manager: string[];
+
+  @ApiProperty()
+  readonly tapeRedundancy: string;
+
+  @ApiProperty()
+  readonly autoArchive: boolean;
+
+  @ApiProperty()
+  readonly autoArchiveDelay: number;
+
+  @ApiProperty()
+  readonly archiveEmailNotification: boolean;
+
+  @ApiProperty({ type: [String] })
+  readonly archiveEmailsToBeNotified: string[];
+
+  @ApiProperty()
+  readonly retrieveEmailNotification: boolean;
+
+  @ApiProperty({ type: [String] })
+  readonly retrieveEmailsToBeNotified: string[];
+
+  @ApiProperty()
+  readonly embargoPeriod: number;
+}
