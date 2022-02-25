e9d89848027996c8be334bc282ff73df65e5aef8
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-25 14:34:47 +0100

    Set up mock model

--- src/initial-datasets/initial-datasets.service.spec.ts
+++ src/initial-datasets/initial-datasets.service.spec.ts
@@ -1,15 +1,38 @@
+import { getModelToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
+import { Model } from "mongoose";
 import { InitialDatasetsService } from "./initial-datasets.service";
+import { InitialDataset } from "./schemas/initial-dataset.schema";
+
+const mockInitialDataset: InitialDataset = {
+  _id: "testId",
+};
 
 describe("InitialDatasetsService", () => {
   let service: InitialDatasetsService;
+  let initialDatasetModel: Model<InitialDataset>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InitialDatasetsService],
+      providers: [
+        InitialDatasetsService,
+        {
+          provide: getModelToken("InitialDataset"),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockInitialDataset),
+            constructor: jest.fn().mockResolvedValue(mockInitialDataset),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<InitialDatasetsService>(InitialDatasetsService);
+    initialDatasetModel = module.get<Model<InitialDataset>>(
+      getModelToken("InitialDataset"),
+    );
   });
 
   it("should be defined", () => {
