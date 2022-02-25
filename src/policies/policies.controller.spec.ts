95ebae0fe55863a4be44dc278a09f32090daeff9
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-25 14:21:35 +0100

    Create mock policies service

--- src/policies/policies.controller.spec.ts
+++ src/policies/policies.controller.spec.ts
@@ -2,13 +2,15 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { PoliciesController } from "./policies.controller";
 import { PoliciesService } from "./policies.service";
 
+class PoliciesServiceMock {}
+
 describe("PoliciesController", () => {
   let controller: PoliciesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PoliciesController],
-      providers: [PoliciesService],
+      providers: [{ provide: PoliciesService, useClass: PoliciesServiceMock }],
     }).compile();
 
     controller = module.get<PoliciesController>(PoliciesController);
