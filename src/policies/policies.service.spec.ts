b29bd7de8c1e5003a6564ecb8fb5b5a7e57e695e
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-25 14:28:28 +0100

    Set up mocks for test

--- src/policies/policies.service.spec.ts
+++ src/policies/policies.service.spec.ts
@@ -1,15 +1,58 @@
+import { ConfigService } from "@nestjs/config";
+import { getModelToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
+import { Model } from "mongoose";
+import { DatasetsService } from "src/datasets/datasets.service";
+import { UsersService } from "src/users/users.service";
 import { PoliciesService } from "./policies.service";
+import { Policy } from "./schemas/policy.schema";
+
+class DatasetsServiceMock {}
+class UsersServiceMock {}
+
+const mockPolicy: Policy = {
+  _id: "testId",
+  manager: ["test@email.com"],
+  tapeRedundancy: "low",
+  autoArchive: true,
+  autoArchiveDelay: 7,
+  archiveEmailNotification: true,
+  archiveEmailsToBeNotified: [],
+  retrieveEmailNotification: true,
+  retrieveEmailsToBeNotified: [],
+  embargoPeriod: 3,
+  ownerGroup: "testOwnerGroup",
+  accessGroups: ["testAccessGroup"],
+  createdBy: "testUser",
+  updatedBy: "testUser",
+};
 
 describe("PoliciesService", () => {
   let service: PoliciesService;
+  let policyModel: Model<Policy>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PoliciesService],
+      providers: [
+        ConfigService,
+        { provide: DatasetsService, useClass: DatasetsServiceMock },
+        PoliciesService,
+        { provide: UsersService, useClass: UsersServiceMock },
+        {
+          provide: getModelToken("Policy"),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockPolicy),
+            constructor: jest.fn().mockResolvedValue(mockPolicy),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PoliciesService>(PoliciesService);
+    policyModel = module.get<Model<Policy>>(getModelToken("Policy"));
   });
 
   it("should be defined", () => {
