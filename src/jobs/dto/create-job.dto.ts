fed9e0d2ac3f6ce80ed412462623b5a5fa82c329
Henrik Johansson <henrik.johansson712@gmail.com>
2022-02-28 11:13:01 +0100

    Set up create job dto

--- src/jobs/dto/create-job.dto.ts
+++ src/jobs/dto/create-job.dto.ts
@@ -1,1 +1,27 @@
-export class CreateJobDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateJobDto {
+  @ApiProperty({ required: true })
+  readonly emailJobInitiator: string;
+
+  @ApiProperty({ required: true })
+  readonly type: string;
+
+  @ApiProperty({ required: true })
+  readonly creationTime: Date;
+
+  @ApiProperty({ required: false })
+  readonly executionTime: Date;
+
+  @ApiProperty({ required: false })
+  readonly jobParams: Record<string, unknown>;
+
+  @ApiProperty({ required: false })
+  readonly jobStatusMessage: string;
+
+  @ApiProperty({ required: false })
+  readonly datasetsList: Record<string, unknown>;
+
+  @ApiProperty({ required: false })
+  readonly jobResultObject: Record<string, unknown>;
+}
