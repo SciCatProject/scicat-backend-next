db5ca3950c759582563d3d107be1271a56529424
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-01 14:30:56 +0100

    Add job field enum

--- src/jobs/interfaces/job-filters.interface.ts
+++ src/jobs/interfaces/job-filters.interface.ts
@@ -1,9 +1,21 @@
 import { FilterQuery } from "mongoose";
 import { JobDocument } from "../schemas/job.schema";
 
+export enum JobField {
+  Text = "text",
+  Id = "id",
+  EmailJobInitiator = "emailJobInitiator",
+  Type = "type",
+  CreationTime = "creationTime",
+  JobParams = "jobParams",
+  JobStatusMessage = "jobStatusMessage",
+  DatasetList = "datasetList",
+  JobResultObject = "jobResultProject",
+}
+
 export interface IJobFilters {
   where?: FilterQuery<JobDocument>;
-  fields?: Record<string, unknown>;
+  fields?: FilterQuery<JobDocument>;
   limits?: {
     skip: number;
     limit: number;
