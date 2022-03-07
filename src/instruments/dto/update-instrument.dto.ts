60e5cc5fd727c386c5915a81f12a7bac6713e999
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-07 15:48:00 +0100

    Fix quotes

--- src/instruments/dto/update-instrument.dto.ts
+++ src/instruments/dto/update-instrument.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateInstrumentDto } from './create-instrument.dto';
+import { PartialType } from "@nestjs/swagger";
+import { CreateInstrumentDto } from "./create-instrument.dto";
 
 export class UpdateInstrumentDto extends PartialType(CreateInstrumentDto) {}
