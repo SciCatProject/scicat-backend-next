7a2ee5f0d2e4f00d7b57f8c0a9b2f5c3d6827fad
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-07 11:52:41 +0100

    Set up instruments service

--- src/instruments/instruments.service.ts
+++ src/instruments/instruments.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { CreateInstrumentDto } from './dto/create-instrument.dto';
-import { UpdateInstrumentDto } from './dto/update-instrument.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { FilterQuery, Model } from "mongoose";
+import { CreateInstrumentDto } from "./dto/create-instrument.dto";
+import { UpdateInstrumentDto } from "./dto/update-instrument.dto";
+import { IInstrumentFilters } from "./interfaces/instrument-filters.interface";
+import { Instrument, InstrumentDocument } from "./schemas/instrument.schema";
 
 @Injectable()
 export class InstrumentsService {
-  create(createInstrumentDto: CreateInstrumentDto) {
-    return 'This action adds a new instrument';
+  constructor(
+    @InjectModel(Instrument.name)
+    private instrumentModel: Model<InstrumentDocument>,
+  ) {}
+
+  async create(createInstrumentDto: CreateInstrumentDto): Promise<Instrument> {
+    const createdInstrument = new this.instrumentModel(createInstrumentDto);
+    return createdInstrument.save();
   }
 
-  findAll() {
-    return `This action returns all instruments`;
+  async findAll(filter: IInstrumentFilters): Promise<Instrument[]> {
+    const whereFilter: FilterQuery<InstrumentDocument> = filter.where ?? {};
+    let limit = 100;
+    let skip = 0;
+    let sort = {};
+    if (filter.limits) {
+      if (filter.limits.limit) {
+        limit = filter.limits.limit;
+      }
+      if (filter.limits.skip) {
+        skip = filter.limits.skip;
+      }
+      if (filter.limits.order) {
+        const [field, direction] = filter.limits.order.split(":");
+        sort = { [field]: direction };
+      }
+    }
+    return this.instrumentModel
+      .find(whereFilter)
+      .limit(limit)
+      .skip(skip)
+      .sort(sort)
+      .exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} instrument`;
+  async findOne(
+    filter: FilterQuery<InstrumentDocument>,
+  ): Promise<Instrument | null> {
+    return this.instrumentModel.findOne(filter).exec();
   }
 
-  update(id: number, updateInstrumentDto: UpdateInstrumentDto) {
-    return `This action updates a #${id} instrument`;
+  async update(
+    filter: FilterQuery<InstrumentDocument>,
+    updateInstrumentDto: UpdateInstrumentDto,
+  ): Promise<Instrument | null> {
+    return this.instrumentModel
+      .findOneAndUpdate(filter, updateInstrumentDto, { new: true })
+      .exec();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} instrument`;
+  async remove(filter: FilterQuery<InstrumentDocument>): Promise<unknown> {
+    return this.instrumentModel.findOneAndRemove(filter).exec();
   }
 }
