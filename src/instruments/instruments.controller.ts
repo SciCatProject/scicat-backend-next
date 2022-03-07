21e3780d47b420632f2176761fa002a043e0ff5b
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-07 15:46:23 +0100

    Set up crud endpoints

--- src/instruments/instruments.controller.ts
+++ src/instruments/instruments.controller.ts
@@ -1,34 +1,80 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { InstrumentsService } from './instruments.service';
-import { CreateInstrumentDto } from './dto/create-instrument.dto';
-import { UpdateInstrumentDto } from './dto/update-instrument.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Query,
+} from "@nestjs/common";
+import { InstrumentsService } from "./instruments.service";
+import { CreateInstrumentDto } from "./dto/create-instrument.dto";
+import { UpdateInstrumentDto } from "./dto/update-instrument.dto";
+import { ApiBearerAuth, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { PoliciesGuard } from "src/casl/guards/policies.guard";
+import { CheckPolicies } from "src/casl/decorators/check-policies.decorator";
+import { AppAbility } from "src/casl/casl-ability.factory";
+import { Action } from "src/casl/action.enum";
+import { Instrument } from "./schemas/instrument.schema";
+import { IInstrumentFilters } from "./interfaces/instrument-filters.interface";
 
-@Controller('instruments')
+@ApiBearerAuth()
+@ApiTags("instruments")
+@Controller("instruments")
 export class InstrumentsController {
   constructor(private readonly instrumentsService: InstrumentsService) {}
 
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Create, Instrument),
+  )
   @Post()
-  create(@Body() createInstrumentDto: CreateInstrumentDto) {
+  async create(
+    @Body() createInstrumentDto: CreateInstrumentDto,
+  ): Promise<Instrument> {
     return this.instrumentsService.create(createInstrumentDto);
   }
 
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Instrument))
   @Get()
-  findAll() {
-    return this.instrumentsService.findAll();
+  @ApiQuery({
+    name: "filter",
+    description: "Database filters to apply when retrieve all instruments",
+    required: false,
+  })
+  async findAll(@Query("filter") filter?: string): Promise<Instrument[]> {
+    const instrumentFilter: IInstrumentFilters = JSON.parse(filter ?? "{}");
+    return this.instrumentsService.findAll(instrumentFilter);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.instrumentsService.findOne(+id);
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Instrument))
+  @Get(":id")
+  async findOne(@Param("id") id: string): Promise<Instrument | null> {
+    return this.instrumentsService.findOne({ id });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInstrumentDto: UpdateInstrumentDto) {
-    return this.instrumentsService.update(+id, updateInstrumentDto);
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Update, Instrument),
+  )
+  @Patch(":id")
+  async update(
+    @Param("id") id: string,
+    @Body() updateInstrumentDto: UpdateInstrumentDto,
+  ): Promise<Instrument | null> {
+    return this.instrumentsService.update({ id }, updateInstrumentDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.instrumentsService.remove(+id);
+  @UseGuards(PoliciesGuard)
+  @CheckPolicies((ability: AppAbility) =>
+    ability.can(Action.Delete, Instrument),
+  )
+  @Delete(":id")
+  async remove(@Param("id") id: string): Promise<unknown> {
+    return this.instrumentsService.remove({ id });
   }
 }
