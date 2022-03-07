c0d6b8d264a08f82230a125ba8e86cca8034ca92
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-07 15:33:18 +0100

    Mock instrument model for tests

--- src/instruments/instruments.service.spec.ts
+++ src/instruments/instruments.service.spec.ts
@@ -1,18 +1,42 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { InstrumentsService } from './instruments.service';
+import { getModelToken } from "@nestjs/mongoose";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Model } from "mongoose";
+import { InstrumentsService } from "./instruments.service";
+import { Instrument } from "./schemas/instrument.schema";
 
-describe('InstrumentsService', () => {
+const mockInstrument: Instrument = {
+  _id: "testPid",
+  pid: "testPid",
+  name: "Test",
+  customMetadata: {},
+};
+
+describe("InstrumentsService", () => {
   let service: InstrumentsService;
+  let model: Model<Instrument>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InstrumentsService],
+      providers: [
+        InstrumentsService,
+        {
+          provide: getModelToken("Instrument"),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockInstrument),
+            constructor: jest.fn().mockResolvedValue(mockInstrument),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<InstrumentsService>(InstrumentsService);
+    model = module.get<Model<Instrument>>(getModelToken("Instrument"));
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
