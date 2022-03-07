19fc6d38495cbe61a1bf53dc19a0afab404ee49a
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-07 11:25:43 +0100

    Set up mongoose module import

--- src/instruments/instruments.module.ts
+++ src/instruments/instruments.module.ts
@@ -1,9 +1,31 @@
-import { Module } from '@nestjs/common';
-import { InstrumentsService } from './instruments.service';
-import { InstrumentsController } from './instruments.controller';
+import { Module } from "@nestjs/common";
+import { InstrumentsService } from "./instruments.service";
+import { InstrumentsController } from "./instruments.controller";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Instrument, InstrumentSchema } from "./schemas/instrument.schema";
 
 @Module({
   controllers: [InstrumentsController],
-  providers: [InstrumentsService]
+  imports: [
+    MongooseModule.forFeatureAsync([
+      {
+        name: Instrument.name,
+        useFactory: () => {
+          const schema = InstrumentSchema;
+
+          schema.pre<Instrument>("save", function (next) {
+            // if _id is empty or different than pid,
+            // set _id to pid
+            if (!this._id) {
+              this._id = this.pid;
+            }
+            next();
+          });
+          return schema;
+        },
+      },
+    ]),
+  ],
+  providers: [InstrumentsService],
 })
 export class InstrumentsModule {}
