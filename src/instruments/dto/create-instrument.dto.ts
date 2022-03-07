274c04c9ccf9155367dfbe6142b39edde1b5efb7
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-07 11:32:12 +0100

    Set up create instrument dto

--- src/instruments/dto/create-instrument.dto.ts
+++ src/instruments/dto/create-instrument.dto.ts
@@ -1,1 +1,9 @@
-export class CreateInstrumentDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateInstrumentDto {
+  @ApiProperty({ type: String, required: true })
+  readonly name: string;
+
+  @ApiProperty({ type: Object, required: false, default: {} })
+  readonly customMetadata?: Record<string, unknown>;
+}
