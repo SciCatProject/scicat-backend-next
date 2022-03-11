71d46ccd05e2bd96b059513000ee0d671f3dbbdc
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-11 16:02:13 +0100

    Create mail service

---
+++ src/common/mail.service.ts
@@ -0,0 +1,29 @@
+import { MailerService } from "@nestjs-modules/mailer";
+import { Injectable, Logger } from "@nestjs/common";
+
+@Injectable()
+export class MailService {
+  constructor(private readonly mailerService: MailerService) {}
+
+  async sendMail(
+    to: string,
+    cc: string,
+    subject: string,
+    mailText: string | null,
+    html: string | null = null,
+  ) {
+    try {
+      Logger.log("Sending email to: " + to, "Utils.sendMail");
+      await this.mailerService.sendMail({
+        to,
+        ...(cc && { cc }),
+        ...(subject && { subject }),
+        ...(html && { html }),
+        ...(mailText && { mailText }),
+      });
+    } catch (error) {
+      Logger.error("Failed sending email to: " + to, "MailService.sendMail");
+      Logger.error(error, "MailService.sendMail");
+    }
+  }
+}
