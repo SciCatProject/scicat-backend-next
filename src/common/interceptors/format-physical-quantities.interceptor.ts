6715d8a12c99fa6b97d6ee418db6a1047c767e70
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-17 11:30:21 +0100

    Create common interceptor for formatting physical quantities

---
+++ src/common/interceptors/format-physical-quantities.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { appendSIUnitToPhysicalQuantity } from "../utils";
+
+@Injectable()
+export class FormatPhysicalQuantitiesInterceptor<T> implements NestInterceptor {
+  propName: keyof T;
+
+  constructor(propName: keyof T) {
+    this.propName = propName;
+  }
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<unknown> | Promise<Observable<unknown>> {
+    const req = context.switchToHttp().getRequest();
+    const instance = (req.body as T)[this.propName];
+    req.body[this.propName] =
+      appendSIUnitToPhysicalQuantity<T[keyof T]>(instance);
+    return next.handle();
+  }
+}
