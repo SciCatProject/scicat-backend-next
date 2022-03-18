b3b65aea63c08217aef4c6aba832680a41f50534
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-18 11:13:47 +0100

    Add descriptions for common schemas props

--- src/common/schemas/datafile.schema.ts
+++ src/common/schemas/datafile.schema.ts
@@ -5,31 +5,47 @@ export type DataFileDocument = DataFile & Document;
 
 @Schema()
 export class DataFile {
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "Relative path of the file within the dataset folder",
+  })
   @Prop({ type: String, required: true })
   path: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: Number, description: "Uncompressed file size in bytes" })
   @Prop()
   size: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: Date,
+    description:
+      "Time of file creation on disk, format according to chapter 5.6 internet date/time format in RFC 3339. Local times without timezone/offset info are automatically transformed to UTC using the timezone of the API server",
+  })
   @Prop()
   time: Date;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "Checksum for the file, e.g. its sha-2 hashstring",
+  })
   @Prop()
   chk: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "optional: user ID name as seen on filesystem",
+  })
   @Prop()
   uid: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description: "optional: group ID name as seen on filesystem",
+  })
   @Prop()
   gid: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: String, description: "optional: Posix permission bits" })
   @Prop()
   perm: string;
 }
--- src/common/schemas/ownable.schema.ts
+++ src/common/schemas/ownable.schema.ts
@@ -2,13 +2,29 @@ import { Prop } from "@nestjs/mongoose";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class Ownable {
-  @ApiProperty()
+  @ApiProperty({
+    type: String,
+    description:
+      "Defines the group which owns the data, and therefore has unrestricted access to this data. Usually a pgroup like p12151",
+  })
   @Prop({ type: String, index: true })
   ownerGroup: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    type: [String],
+    description:
+      "Optional additional groups which have read access to the data. Users which are member in one of the groups listed here are allowed to access this data. The special group 'public' makes data available to all users",
+  })
   @Prop({ type: [String], index: true })
   accessGroups: string[];
+
+  @ApiProperty({
+    type: String,
+    description:
+      "Optional additional groups which have read and write access to the data. Users which are member in one of the groups listed here are allowed to access this data.",
+  })
+  @Prop({ type: String, required: false })
+  instrumentGroup: string;
 
   @ApiProperty()
   @Prop({ type: String, index: true })
