a60ef86b2e70dce044df9cae594052c7754e0f5d
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-30 10:29:38 +0200

    Remove unused enums

--- src/proposals/proposal-field.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum ProposalField {
-  Text = "text",
-  StartTime = "startTime",
-  EndTime = "endTime",
-  ProposalId = "proposalId",
-  Title = "title",
-  Firstname = "firstName",
-  Lastname = "lastName",
-}
