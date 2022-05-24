7be0dbf4df90e20982250a9d67625dea4088bcc0
Henrik Johansson <henrik.johansson712@gmail.com>
2022-05-24 16:44:04 +0200

    Add scripts for migration of id fields

---
+++ scripts/migrateIdFields.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# This command migrates the id fields of the relevant docuemtns in the database
+# It uses the mongo client for this, so you must adjust the host and password parameters
+echo "You need to adjust the script to contain the proper username/password information"
+echo
+echo "You should make a backup of your MongoDB before running this command!"
+echo
+read -p "Are you sure you want to run the script? [y/N]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    mongo -u "dacatDBAdmin" -p "...."  --authenticationDatabase "dacat" HOSTNAME/dacat  migrateIdFields.js
+fi
