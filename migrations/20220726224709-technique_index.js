bd05db1f8d31d7050fcc6392575364a99dff5b31
Dylan McReynolds <dmcreynolds@lbl.gov>
2022-07-26 16:02:21 -0700

    Add migrate-mongo and a migration

---
+++ migrations/20220726224709-technique_index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  async up(db, client) {
+    await db.collection("Dataset").dropIndex("techniques.pid_1");
+    await db
+      .collection("Dataset")
+      .createIndex({ "techniques.pid": 1 }, { sparse: true });
+  },
+
+  async down(db, client) {
+    await db.collection("Dataset").dropIndex("techniques.pid_1");
+    await db
+      .collection("Dataset")
+      .createIndex({ "techniques.pid": 1 }, { unique: true, sparse: true });
+  }
+};
