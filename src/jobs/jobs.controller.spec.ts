aeaa66e0276692ad33791c6d8bc936102b9e0513
Martin Trajanovski <martin.trajanovski@gmail.com>
2022-11-29 10:28:44 +0100

    fix unit tests

--- src/jobs/jobs.controller.spec.ts
+++ src/jobs/jobs.controller.spec.ts
@@ -1,11 +1,26 @@
+import { getModelToken } from "@nestjs/mongoose";
 import { Test, TestingModule } from "@nestjs/testing";
 import { CaslAbilityFactory } from "src/casl/casl-ability.factory";
 import { DatasetsService } from "src/datasets/datasets.service";
 import { JobsController } from "./jobs.controller";
 import { JobsService } from "./jobs.service";
+import { Job } from "./schemas/job.schema";
 
 class JobsServiceMock {}
 class DatasetsServiceMock {}
+
+const mockJob: Job = {
+  _id: "testId",
+  id: "testId",
+  emailJobInitiator: "test@email.com",
+  type: "retrieve",
+  creationTime: new Date(),
+  executionTime: new Date(),
+  jobParams: {},
+  jobStatusMessage: "testStatus",
+  datasetList: [],
+  jobResultObject: {},
+};
 
 describe("JobsController", () => {
   let controller: JobsController;
@@ -17,6 +32,16 @@ describe("JobsController", () => {
         CaslAbilityFactory,
         { provide: JobsService, useClass: JobsServiceMock },
         { provide: DatasetsService, useClass: DatasetsServiceMock },
+        {
+          provide: getModelToken("Job"),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockJob),
+            constructor: jest.fn().mockResolvedValue(mockJob),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
