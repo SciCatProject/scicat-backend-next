7fe8275efa52e18593f6a70be7c3ff6d4b42671d
Max Novelli <max.novelli@ess.eu>
2023-03-17 19:28:37 +0100

    Added user endpoint to return if user is authorized to create datasets

--- test/LoginUtils.js
+++ test/LoginUtils.js
@@ -15,6 +15,20 @@ exports.getToken = function (appUrl, user, cb) {
     });
 };
 
+exports.getIdAndToken = function (appUrl, user, cb) {
+  request(appUrl)
+    .post("/api/v3/Users/Login?include=user")
+    .send(user)
+    .set("Accept", "application/json")
+    .end((err, res) => {
+      if (err) {
+        cb(err);
+      } else {
+        cb(res.body.userId,res.body.id);
+      }
+    });
+};
+
 exports.getTokenAD = function (appUrl, user, cb) {
   request(appUrl)
     .post("/auth/msad")
