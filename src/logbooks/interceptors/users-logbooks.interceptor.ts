a016ab46c5ac0aeda541673235ae9b8907e84fbd
Jay Quan <a331998513@gmail.com>
2023-04-04 15:20:42 +0200

    fix: admin should have access to all logbooks for proposals (#429)

--- src/logbooks/interceptors/users-logbooks.interceptor.ts
+++ src/logbooks/interceptors/users-logbooks.interceptor.ts
@@ -7,7 +7,7 @@ import {
 import { map, Observable } from "rxjs";
 import { ProposalsService } from "src/proposals/proposals.service";
 import { Logbook } from "../schemas/logbook.schema";
-
+import { Role } from "src/auth/role.enum";
 @Injectable()
 export class UsersLogbooksInterceptor implements NestInterceptor {
   constructor(private readonly proposalsService: ProposalsService) {}
@@ -17,6 +17,7 @@ export class UsersLogbooksInterceptor implements NestInterceptor {
     next: CallHandler,
   ): Promise<Observable<Logbook | Logbook[] | null>> {
     const usersGroups = context.getArgs()[1].req.user.currentGroups;
+    const isAdmin = usersGroups.includes(Role.Admin);
     const proposals = await this.proposalsService.findAll({
       where: { ownerGroup: { $in: usersGroups } },
     });
@@ -25,12 +26,13 @@ export class UsersLogbooksInterceptor implements NestInterceptor {
     return next.handle().pipe(
       map((payload: unknown) => {
         if (Array.isArray(payload)) {
-          const filteredLogbook = (payload as Logbook[]).filter((logbook) =>
-            proposalIds.includes(logbook?.name),
+          const filteredLogbook = (payload as Logbook[]).filter(
+            (logbook) => proposalIds.includes(logbook?.name) || isAdmin,
           );
           return filteredLogbook;
         }
-        return proposalIds.includes((payload as Logbook)?.name)
+
+        return proposalIds.includes((payload as Logbook)?.name) || isAdmin
           ? (payload as Logbook)
           : null;
       }),
