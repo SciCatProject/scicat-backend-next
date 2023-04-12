681593ea230c6d3fa1ff8023ee657a9f862f49d0
Max Novelli <max.novelli@ess.eu>
2023-04-12 16:39:18 +0200

    updated swagger interface and added dto for accepted options

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -293,9 +293,11 @@ export class UsersService implements OnModuleInit {
     user: JWTUser,
     jwtProperties: JwtSignOptions,
   ): Promise<CreateUserJWT | null> {
-    const signAndVerifyOptions = { ...jwtProperties };
+    const signAndVerifyOptions: JwtSignOptions = { ...jwtProperties } as JwtSignOptions;
     if (signAndVerifyOptions.expiresIn == "never") {
       delete signAndVerifyOptions.expiresIn;
+    } else if ( typeof signAndVerifyOptions.expiresIn === 'string' && signAndVerifyOptions.expiresIn && !isNaN(signAndVerifyOptions.expiresIn as any) ) {
+      signAndVerifyOptions.expiresIn = parseInt(signAndVerifyOptions.expiresIn);
     }
     signAndVerifyOptions.secret = this.configService.get<string>("jwt.secret");
     const payload = {
