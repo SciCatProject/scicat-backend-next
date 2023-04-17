6bb1b9f02d1650445af66ce718734060b19716a4
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-04-17 11:09:06 +0200

    feat: Add admin module with config and theme endpoints

---
+++ src/admin/admin.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { AdminService } from "./admin.service";
+import { AdminController } from "./admin.controller";
+
+@Module({
+  controllers: [AdminController],
+  imports: [],
+  providers: [AdminService],
+  exports: [AdminService],
+})
+export class AdminModule {}
---
+++ src/admin/admin.service.spec.ts
@@ -0,0 +1,33 @@
+import { getModelToken } from "@nestjs/mongoose";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AdminService } from "./admin.service";
+import config from "../config/frontend.config";
+
+const mockConfig: Record<string, unknown> = config;
+
+describe("PoliciesService", () => {
+  let service: AdminService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: getModelToken("Policy"),
+          useValue: {
+            new: jest.fn().mockResolvedValue(mockConfig),
+            constructor: jest.fn().mockResolvedValue(mockConfig),
+            find: jest.fn(),
+            create: jest.fn(),
+            exec: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    service = module.get<AdminService>(AdminService);
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+});
