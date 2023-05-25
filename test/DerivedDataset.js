13f0b137629b0157e8cfc1c0f229731d64bfd5c6
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-05-25 12:06:14 +0200

    add API tests that cover the changes

--- test/DerivedDataset.js
+++ test/DerivedDataset.js
@@ -6,7 +6,10 @@ const { TestData } = require("./TestData");
 
 var accessToken = null;
 var accessTokenArchiveManager = null;
+var accessTokenUser1 = null;
+var accessTokenUser2 = null;
 var pid = null;
+var explicitPid = "B69A6239-5FD1-4244-8363-58C2DA1B6915";
 var minPid = null;
 
 describe("DerivedDataset: Derived Datasets", () => {
@@ -22,12 +25,32 @@ describe("DerivedDataset: Derived Datasets", () => {
         utils.getToken(
           appUrl,
           {
-            username: "archiveManager",
-            password: "aman",
+            username: "user1",
+            password: "a609316768619f154ef58db4d847b75e",
           },
           (tokenVal) => {
-            accessTokenArchiveManager = tokenVal;
-            done();
+            accessTokenUser1 = tokenVal;
+            utils.getToken(
+              appUrl,
+              {
+                username: "user2",
+                password: "f522d1d715970073a6413474ca0e0f63",
+              },
+              (tokenVal) => {
+                accessTokenUser2 = tokenVal;
+                utils.getToken(
+                  appUrl,
+                  {
+                    username: "archiveManager",
+                    password: "aman",
+                  },
+                  (tokenVal) => {
+                    accessTokenArchiveManager = tokenVal;
+                    done();
+                  },
+                );
+              },
+            );
           },
         );
       },
@@ -76,6 +99,57 @@ describe("DerivedDataset: Derived Datasets", () => {
         res.body.should.have.property("type").and.equal("derived");
         res.body.should.have.property("pid").and.be.string;
         pid = res.body["pid"];
+      });
+  });
+
+  it("should not be able to add new derived dataset with incorrect explicit pid", async () => {
+    const derivedDatasetWithExplicitPID = {
+      ...TestData.DerivedCorrect,
+      pid: "test-pid-1",
+    };
+    return request(appUrl)
+      .post("/api/v3/Datasets")
+      .send(derivedDatasetWithExplicitPID)
+      .set("Accept", "application/json")
+      .set({ Authorization: `Bearer ${accessToken}` })
+      .expect(400)
+      .expect("Content-Type", /json/);
+  });
+
+  it("should not be able to add new derived dataset with group that is not part of allowed groups", async () => {
+    const derivedDatasetWithExplicitPID = {
+      ...TestData.DerivedCorrect,
+      pid: "test-pid-1",
+    };
+    return request(appUrl)
+      .post("/api/v3/Datasets")
+      .send(derivedDatasetWithExplicitPID)
+      .set("Accept", "application/json")
+      .set({ Authorization: `Bearer ${accessTokenUser1}` })
+      .expect(403)
+      .expect("Content-Type", /json/);
+  });
+
+  it("should be able to add new derived dataset with group that is part of allowed groups and correct explicit PID", async () => {
+    const derivedDatasetWithExplicitPID = {
+      ...TestData.DerivedCorrect,
+      ownerGroup: "group2",
+      pid: explicitPid,
+    };
+    return request(appUrl)
+      .post("/api/v3/Datasets")
+      .send(derivedDatasetWithExplicitPID)
+      .set("Accept", "application/json")
+      .set({ Authorization: `Bearer ${accessTokenUser2}` })
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        res.body.should.have.property("owner").and.be.string;
+        res.body.should.have.property("type").and.equal("derived");
+        res.body.should.have
+          .property("pid")
+          .and.equal(derivedDatasetWithExplicitPID.pid);
+        explicitPid = res.body["pid"];
       });
   });
 
@@ -199,6 +273,14 @@ describe("DerivedDataset: Derived Datasets", () => {
   it("should delete a minimal derived dataset", async () => {
     return request(appUrl)
       .delete("/api/v3/Datasets/" + minPid)
+      .set("Accept", "application/json")
+      .set({ Authorization: `Bearer ${accessTokenArchiveManager}` })
+      .expect(200)
+      .expect("Content-Type", /json/);
+  });
+  it("should delete a derived dataset with explicit PID", async () => {
+    return request(appUrl)
+      .delete("/api/v3/Datasets/" + explicitPid)
       .set("Accept", "application/json")
       .set({ Authorization: `Bearer ${accessTokenArchiveManager}` })
       .expect(200)
