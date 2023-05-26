a8cf51945bf285cdd8b1e676d272c368c7202a71
Jay Quan <a331998513@gmail.com>
2023-05-26 11:57:32 +0200

    fix: refactor policies filter handling method

--- src/policies/policies.controller.ts
+++ src/policies/policies.controller.ts
@@ -13,27 +13,94 @@ import {
   Query,
   HttpCode,
   HttpStatus,
+  HttpException,
+  Req,
 } from "@nestjs/common";
+import { Request } from "express";
 import { PoliciesService } from "./policies.service";
 import { CreatePolicyDto } from "./dto/create-policy.dto";
 import { UpdatePolicyDto } from "./dto/update-policy.dto";
-import { ApiBearerAuth, ApiHeaders, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { PoliciesGuard } from "src/casl/guards/policies.guard";
 import { CheckPolicies } from "src/casl/decorators/check-policies.decorator";
-import { AppAbility } from "src/casl/casl-ability.factory";
+import { AppAbility, CaslAbilityFactory } from "src/casl/casl-ability.factory";
 import { Action } from "src/casl/action.enum";
 import { Policy, PolicyDocument } from "./schemas/policy.schema";
 import { FilterQuery } from "mongoose";
 import { IPolicyFilter } from "./interfaces/policy-filters.interface";
 import { HistoryInterceptor } from "src/common/interceptors/history.interceptor";
 import { UpdateWherePolicyDto } from "./dto/update-where-policy.dto";
+import { IFilters } from "src/common/interfaces/common.interface";
+import { JWTUser } from "src/auth/interfaces/jwt-user.interface";
+import { replaceLikeOperator } from "src/common/utils";
+import { FilterPipe } from "src/common/pipes/filter.pipe";
 
 @ApiBearerAuth()
 @ApiTags("policies")
 @Controller("policies")
 export class PoliciesController {
-  constructor(private readonly policiesService: PoliciesService) {}
+  constructor(
+    private readonly policiesService: PoliciesService,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
+  getFilters(
+    headers: Record<string, string>,
+    queryFilter: { filter?: string },
+  ) {
+    // NOTE: If both headers and query filters are present return error because we don't want to support this scenario.
+    if (queryFilter?.filter && (headers?.filter || headers?.where)) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          message:
+            "Using two different types(query and headers) of filters is not supported and can result with inconsistencies",
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    } else if (queryFilter?.filter) {
+      const jsonQueryFilters: IFilters<PolicyDocument, IPolicyFilter> =
+        JSON.parse(queryFilter.filter);
 
+      return jsonQueryFilters;
+    } else if (headers?.filter) {
+      const jsonHeadersFilters: IFilters<PolicyDocument, IPolicyFilter> =
+        JSON.parse(headers.filter);
+
+      return jsonHeadersFilters;
+    } else if (headers?.where) {
+      const jsonHeadersWhereFilters: IFilters<PolicyDocument, IPolicyFilter> =
+        JSON.parse(headers.where);
+
+      return jsonHeadersWhereFilters;
+    }
+
+    return {};
+  }
+
+  updateMergedFiltersForList(
+    request: Request,
+    mergedFilters: IFilters<PolicyDocument, IPolicyFilter>,
+  ): IFilters<PolicyDocument, IPolicyFilter> {
+    const user: JWTUser = request.user as JWTUser;
+
+    if (user) {
+      const ability = this.caslAbilityFactory.createForUser(user);
+      const canViewAll = ability.can(Action.ListAll, Policy);
+      const canViewTheirOwn = ability.can(Action.ListOwn, Policy);
+      if (!canViewAll && canViewTheirOwn) {
+        if (!mergedFilters.where) {
+          mergedFilters.where = {};
+        }
+        mergedFilters.where["$or"] = [
+          { ownerGroup: { $in: user.currentGroups } },
+          { accessGroups: { $in: user.currentGroups } },
+          { isPublished: true },
+        ];
+      }
+    }
+
+    return mergedFilters;
+  }
   @UseGuards(PoliciesGuard)
   @CheckPolicies((ability: AppAbility) => ability.can(Action.Create, Policy))
   @Post()
@@ -44,19 +111,25 @@ export class PoliciesController {
   @UseGuards(PoliciesGuard)
   @CheckPolicies((ability: AppAbility) => ability.can(Action.Read, Policy))
   @Get()
-  @ApiHeaders([
-    {
-      name: "filter",
-      description: "Database filters to apply when retrieve all policies",
-      required: false,
-      schema: {
-        example: '{"order":"ownerGroup:desc","skip":0,"limit":25}',
-      },
-    },
-  ])
-  async findAll(@Headers("filter") filter?: string): Promise<Policy[]> {
-    const parsedFilter: IPolicyFilter = JSON.parse(filter ?? "{}");
-    return this.policiesService.findAll(parsedFilter);
+  @ApiQuery({
+    name: "filter",
+    description: "Database filters to apply when retrieve all policies",
+    required: false,
+    example: '{"order":"ownerGroup:desc","skip":0,"limit":25}',
+  })
+  async findAll(
+    @Req() request: Request,
+    @Headers() headers: Record<string, string>,
+    @Query(new FilterPipe()) queryFilter: { filter?: string },
+  ): Promise<Policy[]> {
+    const mergedFilters = replaceLikeOperator(
+      this.updateMergedFiltersForList(
+        request,
+        this.getFilters(headers, queryFilter),
+      ) as Record<string, unknown>,
+    ) as IFilters<PolicyDocument, IPolicyFilter>;
+
+    return this.policiesService.findAll(mergedFilters);
   }
 
   @UseGuards(PoliciesGuard)
