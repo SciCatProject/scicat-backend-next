6ff947fd1a2cf25ca30fa87ebc12d7a1c7ea0aef
HayenNico <141850426+HayenNico@users.noreply.github.com>
2023-10-31 12:58:22 +0100

    fix eslint complaints

--- src/jobs/dto/update-job.dto.ts
+++ src/jobs/dto/update-job.dto.ts
@@ -1,14 +1,13 @@
 import { ApiProperty,  PartialType } from "@nestjs/swagger";
 import { CreateJobDto } from "./create-job.dto";
-import {
-    IsString,
-  } from "class-validator";
+import { IsString } from "class-validator";
 
 export class UpdateJobDto extends PartialType(CreateJobDto) {
   @ApiProperty({
     type: String,
     required: true,
-    description: "Id for the job to be updated.",
+    description:
+      "Id for the job to be updated.",
   })
   @IsString()
   readonly id: string;
