f5545af40d387b686fb4f1b1fcac626bcf2cc3a4
Max Novelli <max.novelli@ess.eu>
2023-11-10 11:12:34 +0100

    FIxed all linting, started testing DatasetAuthorization

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -19,19 +19,19 @@ const configuration = () => {
   const createJobGroups = process.env.CREATE_JOB_GROUPS || ("" as string);
   const updateJobGroups = process.env.UPDATE_JOB_GROUPS || ("" as string);
 
-  Logger.log("Config SETUP");
-  Logger.log("- Access groups statisc values : " + accessGroupsStaticValues);
-  Logger.log("- Admin groups : " + adminGroups);
-  Logger.log("- Delete groups : " + deleteGroups);
-  Logger.log("- Create dataset groups : " + createDatasetGroups);
-  Logger.log(
-    "- Create dataset with pid groups : " + createDatasetWithPidGroups,
-  );
-  Logger.log(
-    "- Create dataset privileged groups : " + createDatasetPrivilegedGroups,
-  );
-  Logger.log("- Create job groups : " + createJobGroups);
-  Logger.log("- Update job groups : " + updateJobGroups);
+  // Logger.log("Config SETUP");
+  // Logger.log("- Access groups statisc values : " + accessGroupsStaticValues);
+  // Logger.log("- Admin groups : " + adminGroups);
+  // Logger.log("- Delete groups : " + deleteGroups);
+  // Logger.log("- Create dataset groups : " + createDatasetGroups);
+  // Logger.log(
+  //   "- Create dataset with pid groups : " + createDatasetWithPidGroups,
+  // );
+  // Logger.log(
+  //   "- Create dataset privileged groups : " + createDatasetPrivilegedGroups,
+  // );
+  // Logger.log("- Create job groups : " + createJobGroups);
+  // Logger.log("- Update job groups : " + updateJobGroups);
 
   return {
     adminGroups: adminGroups.split(",").map((v) => v.trim()) ?? [],
@@ -56,78 +56,7 @@ const configuration = () => {
     },
     doiPrefix: process.env.DOI_PREFIX,
     expressSessionSecret: process.env.EXPRESS_SESSION_SECRET,
-    functionalAccounts: [
-      {
-        username: "admin",
-        email: "admin@your.site",
-        password: "am2jf70TPNZsSan",
-        role: "admin",
-        global: true,
-      },
-      {
-        username: "ingestor",
-        email: "scicatingestor@your.site",
-        password: "aman",
-        role: "ingestor",
-        global: false,
-      },
-      {
-        username: "archiveManager",
-        email: "scicatarchivemanager@your.site",
-        password: "aman",
-        role: "archivemanager",
-        global: false,
-      },
-      {
-        username: "proposalIngestor",
-        email: "scicatproposalingestor@your.site",
-        password: "aman",
-        role: "proposalingestor",
-        global: false,
-      },
-      {
-        username: "user1",
-        email: "user1@your.site",
-        password: "a609316768619f154ef58db4d847b75e",
-        role: "group1",
-        global: false,
-      },
-      {
-        username: "user2",
-        email: "user2@your.site",
-        password: "f522d1d715970073a6413474ca0e0f63",
-        role: "group2",
-        global: false,
-      },
-      {
-        username: "user3",
-        email: "user3@your.site",
-        password: "70dc489e8ee823ae815e18d664424df2",
-        role: "group3",
-        global: false,
-      },
-      {
-        username: "user4",
-        email: "user4@your.site",
-        password: "0014890e7020f515b92b767227ef2dfa",
-        role: "group4",
-        global: false,
-      },
-      {
-        username: "user5.1",
-        email: "user5.1@your.site",
-        password: "359a5fda99bfe5dbc42ee9b3ede77fb7",
-        role: "group5",
-        global: false,
-      },
-      {
-        username: "user5.2",
-        email: "user5.2@your.site",
-        password: "f3ebd2e4def95db59ef95ee32ef45242",
-        role: "group5",
-        global: false,
-      },
-    ],
+    functionalAccounts: [],
     httpMaxRedirects: process.env.HTTP_MAX_REDIRECTS ?? 5,
     httpTimeOut: process.env.HTTP_TIMEOUT ?? 5000,
     jwt: {
