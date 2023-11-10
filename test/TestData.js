7622dd293bd472c89bb3a0492f7b19fafc453e44
POTIER Frederic <frederic.potier@synchrotron-soleil.fr>
2023-11-10 14:12:34 +0100

    Add `MeasurementPeriods` to `ProposalCorrectComplete`

--- test/TestData.js
+++ test/TestData.js
@@ -23,7 +23,19 @@ const TestData = {
     abstract: "Abstract of test proposal",
     ownerGroup: "proposalingestor",
     accessGroups: [],
-    MeasurementPeriodList: [],
+    MeasurementPeriodList: [
+      {
+        instrument: "ESS3-1",
+        start: "2017-07-24T13:56:30.000Z",
+        end: "2017-07-25T13:56:30.000Z",
+        comment: "Some comment"
+      },
+      {
+        instrument: "ESS3-2",
+        start: "2017-07-28T13:56:30.000Z",
+        end: "2017-07-29T13:56:30.000Z",
+      }
+    ],
   },
 
   ProposalWrong_1: {
