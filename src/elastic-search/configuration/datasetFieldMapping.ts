f89dc363de2386b14c9951e3a7e98cd32242467b
Jay Quan <a331998513@gmail.com>
2023-11-23 16:07:20 +0100

    added ignore_above 256 for elasticsearch keyword field

--- src/elastic-search/configuration/datasetFieldMapping.ts
+++ src/elastic-search/configuration/datasetFieldMapping.ts
@@ -25,6 +25,7 @@ export const datasetMappings: MappingObject = {
   },
   pid: {
     type: "keyword",
+    ignore_above: 256,
   },
   creationTime: {
     type: "date",
@@ -39,29 +40,37 @@ export const datasetMappings: MappingObject = {
   },
   proposalId: {
     type: "keyword",
+    ignore_above: 256,
   },
   sourceFolder: {
     type: "keyword",
+    ignore_above: 256,
   },
   isPublished: {
     type: "boolean",
   },
   type: {
     type: "keyword",
+    ignore_above: 256,
   },
   keywords: {
     type: "keyword",
+    ignore_above: 256,
   },
   creationLocation: {
     type: "keyword",
+    ignore_above: 256,
   },
   ownerGroup: {
     type: "keyword",
+    ignore_above: 256,
   },
   accessGroups: {
     type: "keyword",
+    ignore_above: 256,
   },
   sharedWith: {
     type: "keyword",
+    ignore_above: 256,
   },
 };
