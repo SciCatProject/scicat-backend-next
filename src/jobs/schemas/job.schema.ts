a04eab5b158bf97f2f2652fe1ce10bb8c29b5b69
HayenNico <141850426+HayenNico@users.noreply.github.com>
2023-11-24 12:12:07 +0100

    Minor refactor job.schema

--- src/jobs/schemas/job.schema.ts
+++ src/jobs/schemas/job.schema.ts
@@ -9,7 +9,7 @@ export type JobDocument = JobClass & Document;
 
 @Schema({
   collection: "Job",
-  minimize: false, /* function? dataset schema has it*/
+  minimize: false,
   timestamps: { createdAt: "DateTime", updatedAt: false },
   toJSON: {
     getters: true,
@@ -20,7 +20,8 @@ export class JobClass extends OwnableClass{
   @ApiProperty({
     type: String,
     default: () => uuidv4(),
-    description: "Globally unique identifier of a job.",
+    description:
+      "Globally unique identifier of a job.",
   })
   @Prop({
     type: String,
@@ -37,9 +38,10 @@ export class JobClass extends OwnableClass{
 
   // type
   @ApiProperty({
-    type: String, 
-    description: "Type of job as defined in the job configuration, e.g. archive, retrieve, etc",
-    required: true
+    type: String,
+    description:
+      "Type of job as defined in the job configuration, e.g. archive, retrieve, etc",
+    required: true,
   })
   @Prop({
     type: String,
@@ -51,80 +53,80 @@ export class JobClass extends OwnableClass{
   @ApiProperty({
     type: String,
     required: false, 
-    description: "Defines the current status code of the job." 
+    description:
+      "Defines the current status code of the job.",
   })
   @Prop({ 
-    type: String, 
-    required: false 
+    type: String,
+    required: false,
   })
-  status_code: string;
+  statusCode: string;
 
   // update 
   @ApiProperty({
     type: String,
-    required: false, 
-    description: "stores the latest message received with the last status update for the job." 
+    required: false,
+    description:
+      "stores the latest message received with the last status update for the job.",
   })
-  @Prop({ 
-    type: String, 
-    required: false 
+  @Prop({
+    type: String,
+    required: false,
   })
-  status_message: string;
+  statusMessage: string;
 
   // history of status codes		
   @ApiProperty({
     type: [Object],
     required: false,
     description:
-        "Array of status updates containing status code and message",
+      "Array of status updates containing status code and message",
   })
-  @Prop({ 
-    type: [Object], 
-    required: false 
+  @Prop({
+    type: [Object],
+    required: false,
   })
-  status_history: Record<string, string>[];
+  statusHistory: Record<string, string>[];
 
   // messages
   @ApiProperty({
     type: Object,
     required: false,
     description:
-        "This is the equivalent object of the message sent to external service.",
+      "This is the equivalent object of the message sent to external service.",
   })
-  @Prop({ 
-    type: Object, 
-    required: false 
+  @Prop({
+    type: Object,
+    required: false,
   })
-  message_sent: Record<string,unknown>;
+  messageSent: Record<string,unknown>;
 
-  // configuration
+  // configuration definition (schema)
   @ApiProperty({
     type: Object,
     required: false,
     description:
-        "This is the equivalent object of the job configuration used to create this job.",
+      "This is the equivalent object of the job configuration used to create this job.",
   })
-  @Prop({ 
-    type: Object, 
-    required: false 
+  @Prop({
+    type: Object,
+    required: false,
   })
-  // definition NOT VALUE
   configuration: Record<string,unknown>;
 
-  // parameters
+  // parameters (instance)
   @ApiProperty({
     type: Object,
     required: false,
     description:
-        "This is the equivalent object of the jobs parameters provided by the user.",
+      "This is the equivalent object of the jobs parameters provided by the user.",
   })
-  @Prop({ 
-    type: Object, 
-    required: false 
+  @Prop({
+    type: Object,
+    required: false,
   })
   parameters: Record<string,unknown>;
   // in case email is needed it goes into params, and other values too
-
 }
 export const JobSchema = SchemaFactory.createForClass(JobClass);
 
