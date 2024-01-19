d4b5d9ec1922b98a2bed1d0e981eb36ddffa4055
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2024-01-19 10:55:50 +0100

    Bump chai to 5.0.0
     - fix requires -> dynamic import
     - should registered vi -r on command
    
    Change-Id: Iadf9a5146282cbbc183b2d6b8e5db2da33bf2f13

--- test/config/pretest.js
+++ test/config/pretest.js
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 //NOTE: Here we load and initialize some global variables that are used throughout the tests
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-chai.should();
-chai.use(chaiHttp);
+
+var chaiHttp = require("chai-http");
+async function loadChai() {
+  const { chai } = import("chai");
+  chai.use(chaiHttp);
+}
+loadChai();
 
 global.appUrl = "http://localhost:3000";
 global.request = require("supertest");
