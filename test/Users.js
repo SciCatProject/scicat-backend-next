d81cbf06bfc3cf8ea7b67025c3efc94555be78a7
Jay Quan <a331998513@gmail.com>
2024-04-05 19:49:43 +0200

    fix: 400 error on the datasets page when changing the number of items per page (#1151)
    * fix: datasets page 400 error on changing items per page
    
    * added api test for update users settings

--- test/Users.js
+++ test/Users.js
@@ -1,8 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { TestData } = require("./TestData");
+"use strict";
 
-describe("Users: Login with functional accounts", () => {
-  it("Admin ingestor login fails with incorrect credentials", async () => {
+const { TestData } = require("./TestData");
+const utils = require("./LoginUtils");
+
+let userIdUser1 = null,
+  accessTokenUser1 = null;
+
+describe("2350: Users: Login with functional accounts", () => {
+  it("0010: Admin ingestor login fails with incorrect credentials", async () => {
     return request(appUrl)
       .post("/api/v3/Users/Login?include=user")
       .send({
@@ -15,7 +21,7 @@ describe("Users: Login with functional accounts", () => {
       });
   });
 
-  it("Login should succeed with correct credentials", async () => {
+  it("0020: Login should succeed with correct credentials", async () => {
     return request(appUrl)
       .post("/api/v3/Users/Login?include=user")
       .send({
@@ -27,6 +33,37 @@ describe("Users: Login with functional accounts", () => {
       .expect("Content-Type", /json/)
       .then((res) => {
         res.body.should.have.property("user").and.be.instanceof(Object);
+      });
+  });
+});
+
+describe("2360: Users settings", () => {
+  beforeEach((done) => {
+    utils.getIdAndToken(
+      appUrl,
+      {
+        username: "user1",
+        password: TestData.Accounts["user1"]["password"],
+      },
+      (idVal, tokenVal) => {
+        accessTokenUser1 = tokenVal;
+        userIdUser1 = idVal;
+        done();
+      },
+    );
+  });
+
+  it("0010: Update users settings with valid value should sucess ", async () => {
+    return request(appUrl)
+      .put(`/api/v3/Users/${userIdUser1}/settings`)
+      .set("Accept", "application/json")
+      .set({ Authorization: `Bearer ${accessTokenUser1}` })
+      .expect(TestData.SuccessfulPatchStatusCode)
+      .expect("Content-Type", /json/)
+      .then((res) => {
+        res.body.should.have.property("userId", userIdUser1);
+        res.body.should.have.property("datasetCount");
+        res.body.should.have.property("jobCount");
       });
   });
 });
