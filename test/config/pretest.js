8a07ea27ca6532f51baf1738e3897458b1d504bc
Despina Adamopoulou <16343312+despadam@users.noreply.github.com>
2024-07-25 14:35:10 +0200

    Fix pretest.js after merging suggestions

--- test/config/pretest.js
+++ test/config/pretest.js
@@ -1,13 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 //NOTE: Here we load and initialize some global variables that are used throughout the tests
 
-var chaiHttp = require("chai-http");
-const { MongoClient } = require("mongodb");
 require("dotenv").config();
 var chaiHttp = require("chai-http");
-
 const { MongoClient } = require("mongodb");
-
 const client = new MongoClient(process.env.MONGODB_URI);
 
 async function loadChai() {
@@ -22,14 +18,3 @@ global.request = require("supertest");
 global.db = client.db();
 
 const client = new MongoClient(uri);
-
-async function loadChai() {
-  const { chai } = import("chai");
-  chai.use(chaiHttp);
-  await client.connect();
-}
-loadChai();
-
-global.appUrl = "http://localhost:3000";
-global.request = require("supertest");
-global.db = client.db(uri.split("/", ).pop());
