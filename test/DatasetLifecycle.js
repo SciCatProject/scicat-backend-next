50d07831abb00c127cbc1e35472335f73aae62ac
sofyalaski <sofyalaskina@gmail.com>
2024-07-30 14:15:49 +0200

    remove a mistake

--- test/DatasetLifecycle.js
+++ test/DatasetLifecycle.js
@@ -12,7 +12,7 @@ var pidRaw2 = null;
 var policyIds = null;
 const raw2 = { ...TestData.RawCorrect };
 
-describe.only("0500: DatasetLifecycle: Test facet and filter queries", () => {
+describe("0500: DatasetLifecycle: Test facet and filter queries", () => {
   before(() => {
     db.collection("Dataset").deleteMany({});
   });
