0a0c792737683067434c433e101372caf490bc4e
ingvord <ingvord.mail@gmail.com>
2024-08-01 18:19:57 +0200

    Fix #1347

--- src/users/dto/update-user-settings.dto.ts
+++ src/users/dto/update-user-settings.dto.ts
@@ -3,21 +3,27 @@ import {
   FilterConfig,
   ScientificCondition,
 } from "../schemas/user-settings.schema";
+import { IsArray, IsNumber } from "class-validator";
 
 export class UpdateUserSettingsDto {
   @ApiProperty()
+  @IsArray()
   readonly columns: Record<string, unknown>[];
 
   @ApiProperty({ type: Number, required: false, default: 25 })
+  @IsNumber()
   readonly datasetCount?: number;
 
   @ApiProperty({ type: Number, required: false, default: 25 })
+  @IsNumber()
   readonly jobCount?: number;
 
   @ApiProperty()
+  @IsArray()
   readonly filters: FilterConfig[];
 
   @ApiProperty()
+  @IsArray()
   readonly conditions: ScientificCondition[];
 }
 
