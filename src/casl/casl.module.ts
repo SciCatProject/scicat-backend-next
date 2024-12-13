848633fad6ef69ed436c49eb43c26f35e70c02af
martintrajanovski <martin.trajanovski@gmail.com>
2024-12-13 16:40:32 +0100

    try to revert some chages that are making tests fail

--- src/casl/casl.module.ts
+++ src/casl/casl.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { CaslAbilityFactory } from "./casl-ability.factory";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   providers: [CaslAbilityFactory],
   exports: [CaslAbilityFactory],
 })
