e91fc9a7eebcf650ad1dee1abacc4ba1ff75ac4c
Despina Adamopoulou <despoina.adamopoulou@empa.ch>
2025-01-07 16:27:54 +0100

    lint

--- src/config/job-config/actions/rabbitmqaction/rabbitmqaction.service.ts
+++ src/config/job-config/actions/rabbitmqaction/rabbitmqaction.service.ts
@@ -23,7 +23,7 @@ export class RabbitMQJobActionCreator implements JobActionCreator<JobDto> {
     return new RabbitMQJobAction(
       this.configService,
       this.rabbitMQService,
-      options
+      options,
     );
   }
 }
