c2b375bc57847b95e1b3243745a9c16e8e6ac24a
Despina Adamopoulou <despoina.adamopoulou@empa.ch>
2025-03-18 15:47:29 +0100

    fix job.v3 schema

--- src/jobs/schemas/job.v3.schema.ts
+++ src/jobs/schemas/job.v3.schema.ts
@@ -1,56 +1,28 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { ApiProperty } from "@nestjs/swagger";
-import { Document } from "mongoose";
-import { v4 as uuidv4 } from "uuid";
+import { ApiHideProperty, ApiProperty } from "@nestjs/swagger";
 import { DatasetListDto } from "../dto/dataset-list.dto";
 
-export type JobDocumentV3 = JobClassV3 & Document;
-
-@Schema({
-  collection: "Job",
-  timestamps: false,
-  toJSON: {
-    getters: true,
-  },
-})
 export class JobClassV3 {
+  @ApiHideProperty()
+  _id: string;
+
   @ApiProperty({
     type: String,
     required: true,
     description: "Globally unique identifier of a job.",
   })
-  @Prop({
-    type: String,
-    required: true,
-    unique: true,
-    default: () => uuidv4(),
-  })
   id: string;
-
-  @Prop({
-    type: String,
-  })
-  _id: string;
 
   @ApiProperty({
     type: String,
     required: false,
     description: "The email of the person initiating the job request.",
   })
-  @Prop({
-    type: String,
-    required: false,
-  })
-  emailJobInitiator: string;
+  emailJobInitiator?: string;
 
   @ApiProperty({
     type: String,
     required: true,
-    description: "Type of job, e.g. archive, retrieve etc",
-  })
-  @Prop({
-    type: String,
-    required: true,
+    description: "Type of job, e.g. archive, retrieve etc.",
   })
   type: string;
 
@@ -60,10 +32,6 @@ export class JobClassV3 {
     description:
       "Time when job is created. Format according to chapter 5.6 internet date/time format in RFC 3339. This is handled automatically by mongoose with timestamps flag.",
   })
-  @Prop({
-    type: Date,
-    required: true,
-  })
   creationTime: Date;
 
   @ApiProperty({
@@ -72,22 +40,14 @@ export class JobClassV3 {
     description:
       "Time when job should be executed. If not specified then the Job will be executed asap. Format according to chapter 5.6 internet date/time format in RFC 3339.",
   })
-  @Prop({
-    type: Date,
-    required: false,
-  })
-  executionTime: Date;
+  executionTime?: Date;
 
   @ApiProperty({
     type: Object,
     required: true,
+    default: {},
     description:
       "Object of key-value pairs defining job input parameters, e.g. 'destinationPath' for retrieve jobs or 'tapeCopies' for archive jobs.",
-  })
-  @Prop({
-    type: Object,
-    required: true,
-    default: {},
   })
   jobParams: Record<string, unknown>;
 
@@ -96,38 +56,21 @@ export class JobClassV3 {
     required: false,
     description: "Defines current status of job lifecycle.",
   })
-  @Prop({
-    type: String,
-    required: false,
-  })
-  jobStatusMessage: string;
+  jobStatusMessage?: string;
 
   @ApiProperty({
-    type: DatasetListDto,
-    isArray: true,
-    required: false,
-    description:
-      "Array of objects with keys: pid, files. The value for the pid key defines the dataset ID, the value for the files key is an array of file names. This array is either an empty array, implying that all files within the dataset are selected or an explicit list of dataset-relative file paths, which should be selected.",
-  })
-  @Prop({
     type: [DatasetListDto],
     required: false,
+    description:
+      "Array of objects with keys: pid, files. The value for the pid key defines the dataset ID, the value for the files key is an array of file names. This array is either an empty array, implying that all files within the dataset are selected, or an explicit list of dataset-relative file paths, which should be selected.",
   })
   datasetList: DatasetListDto[];
 
   @ApiProperty({
     type: Object,
     required: true,
-    description: "Detailed return value after job is finished.",
-  })
-  @Prop({
-    type: Object,
-    required: true,
     default: {},
+    description: "Detailed return value after job is finished.",
   })
   jobResultObject: Record<string, unknown>;
 }
-
-export const JobSchema = SchemaFactory.createForClass(JobClassV3);
-
-JobSchema.index({ "$**": "text" });
