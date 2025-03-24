49cb501a027efaccb3abf2ef67093e53991648b0
Spencer Bliven <spencer.bliven@gmail.com>
2025-03-24 13:52:02 +0100

    Error action occurs during validation
    - Change the HTTP error to be thrown before database operations.
    - Add docstring

--- src/config/job-config/actions/erroraction/erroraction.ts
+++ src/config/job-config/actions/erroraction/erroraction.ts
@@ -1,15 +1,27 @@
-/**
- * Simple JobAction for logging events.
- */
 import { JobAction, JobDto } from "../../jobconfig.interface";
 import { JobClass } from "../../../../jobs/schemas/job.schema";
 import { ErrorJobActionOptions, actionType } from "./erroraction.interface";
 import { compileJob, TemplateJob } from "../../handlebar-utils";
 import { makeHttpException } from "src/common/utils";
 import { HttpStatus, Logger } from "@nestjs/common";
+import { compile, TemplateDelegate } from "handlebars";
 
+/**
+ * Raise an HTTP error
+ *
+ * This is useful for testing or in combination with a `switch` action to raise errors
+ * for specific job situations.
+ *
+ * Errors are thrown during the `validate` step, before job is written to the database.
+ *
+ * Configuration parameters:
+ *
+ * - message (string, required): Error message returned in the response body. May include
+ *   handlebars templates referring to the DTO
+ * - status (number, optional): HTTP error code. Defaults to 400 bad request.
+ */
 export class ErrorJobAction<T extends JobDto> implements JobAction<T> {
-  private messageTemplate: TemplateJob;
+  private messageTemplate: TemplateDelegate<T>;
   private status?: number;
 
   getActionType(): string {
@@ -17,14 +29,15 @@ export class ErrorJobAction<T extends JobDto> implements JobAction<T> {
   }
 
   constructor(options: ErrorJobActionOptions) {
-    this.messageTemplate = compileJob(options.message || "");
+    this.messageTemplate = compile(options.message || "");
     this.status = options.status;
   }
 
-  async validate(dto: T) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async performJob(job: JobClass): Promise<void> {}
 
-  async performJob(job: JobClass) {
-    const message = this.messageTemplate(job);
+  async validate(dto: T) {
+    const message = this.messageTemplate(dto);
     Logger.error(
       `Executing error action [${this.status || HttpStatus.BAD_REQUEST}]: ${message}`,
     );
