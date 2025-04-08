bbc552a17bb47f39b53d62344ee8ef8831ac837f
HayenNico <141850426+HayenNico@users.noreply.github.com>
2025-04-08 13:07:38 +0200

    fix: admin.service.spec unit tests with mock objects

--- src/admin/admin.service.spec.ts
+++ src/admin/admin.service.spec.ts
@@ -1,8 +1,94 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { ConfigService } from "@nestjs/config";
 import { AdminService } from "./admin.service";
-import config from "../config/frontend.config.json";
-import theme from "../config/frontend.theme.json";
+
+const mockConfig: Record<string, unknown> = {
+  "accessTokenPrefix": "Bearer ",
+  "addDatasetEnabled": false,
+  "archiveWorkflowEnabled": false,
+  "datasetReduceEnabled": true,
+  "datasetJsonScientificMetadata": true,
+  "editDatasetSampleEnabled": true,
+  "editMetadataEnabled": true,
+  "editPublishedData": false,
+  "addSampleEnabled": false,
+  "externalAuthEndpoint": "/api/v3/auth/msad",
+  "facility": "SciCat Vanilla",
+  "siteIcon": "site-header-logo.png",
+  "loginFacilityLabel": "SciCat Vanilla",
+  "loginLdapLabel": "Ldap",
+  "loginLocalLabel": "Local",
+  "loginFacilityEnabled": true,
+  "loginLdapEnabled": true,
+  "loginLocalEnabled": true,
+  "localLoginLabel": "Local",
+  "fileColorEnabled": true,
+  "fileDownloadEnabled": true,
+  "gettingStarted": null,
+  "ingestManual": null,
+  "jobsEnabled": true,
+  "jsonMetadataEnabled": true,
+  "jupyterHubUrl": "",
+  "landingPage": "doi.ess.eu/detail/",
+  "lbBaseURL": "http://localhost:3000",
+  "logbookEnabled": true,
+  "loginFormEnabled": true,
+  "metadataPreviewEnabled": true,
+  "metadataStructure": "",
+  "multipleDownloadAction": "http:/127.0.0.1:3012/zip",
+  "multipleDownloadEnabled": true,
+  "oAuth2Endpoints": [
+    {
+      "authURL": "api/v3/auth/oidc",
+      "displayText": "ESS One Identity"
+    }
+  ],
+  "policiesEnabled": true,
+  "retrieveDestinations": [],
+  "riotBaseUrl": "http://scichat.esss.lu.se",
+  "scienceSearchEnabled": true,
+  "scienceSearchUnitsEnabled": true,
+  "searchPublicDataEnabled": true,
+  "searchSamples": true,
+  "sftpHost": "login.esss.dk",
+  "sourceFolder": "/data/ess",
+  "maxDirectDownloadSize": 5000000000,
+  "maxFileSizeWarning": "Some files are above <maxDirectDownloadSize> and cannot be downloaded directly. These file can be downloaded via sftp host: <sftpHost> in directory: <sourceFolder>",
+  "shareEnabled": true,
+  "shoppingCartEnabled": true,
+  "shoppingCartOnHeader": true,
+  "tableSciDataEnabled": true,
+  "datasetDetailsShowMissingProposalId": false,
+  "notificationInterceptorEnabled": true,
+  "metadataEditingUnitListDisabled": true,
+  "datafilesActionsEnabled": true,
+  "datafilesActions": [],
+  "labelMaps": {
+    "filters": {}
+  },
+  "defaultDatasetsListSettings": {
+    "columns": [],
+    "filters": [],
+    "conditions": []
+  },
+  "labelsLocalization": {
+    "datasetDefault": {},
+    "datasetCustom": {},
+    "proposalDefault": {}
+  },
+  "dateFormat": "yyyy-MM-dd HH:mm",
+  "datasetDetailComponent": {
+    "enableCustomizedComponent": false,
+    "customization": []
+  }
+}
+
+const mockTheme: Record<string, unknown> = {
+  "name": "light",
+  "properties": {
+    "--theme-primary-default": "#27aae1"
+  }
+}
 
 describe("AdminService", () => {
   let service: AdminService;
@@ -10,6 +96,8 @@ describe("AdminService", () => {
     get: jest.fn((propertyPath: string) => {
       const config = {
         maxFileUploadSizeInMb: "12mb",
+        frontendConfig: mockConfig,
+        frontendTheme: mockTheme,
       } as Record<string, unknown>;
 
       return config[propertyPath];
@@ -39,7 +127,7 @@ describe("AdminService", () => {
       const result = await service.getConfig();
 
       expect(result).toEqual({
-        ...config,
+        ...mockConfig,
         maxFileUploadSizeInMb: "12mb",
       });
     });
@@ -48,7 +136,7 @@ describe("AdminService", () => {
   describe("getTheme", () => {
     it("should return theme config", async () => {
       const result = await service.getTheme();
-      expect(result).toEqual(theme);
+      expect(result).toEqual(mockTheme);
     });
   });
 });
