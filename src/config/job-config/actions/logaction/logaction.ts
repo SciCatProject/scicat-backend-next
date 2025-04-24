bedc8a78e4e6e81352f5b84cc48ff4c1120983f0
Spencer Bliven <spencer.bliven@gmail.com>
2025-04-24 13:38:44 +0200

    Provide a consistent context to all job actions
    The change is most visible in handlebars templates. Before, `{{this}}`
    would correspond to different objects depending on how the action is
    called; either a DTO or a Job or even a Dataset. Now these are accessed
    consistently through top-level fields; thus `{{id}}` becomes `{{job.id}}`,
    `{{jobParams}}` becomes `{{request.jobParams}}`, etc.
    
    This also adds access to environmental variables via `{{env}}`, making
    it easier to include secrets actions without hard-coding them.
    
    Tests and example files should be up-to-date.

--- src/config/job-config/actions/logaction/logaction.ts
+++ src/config/job-config/actions/logaction/logaction.ts
@@ -2,14 +2,35 @@
  * Simple JobAction for logging events.
  */
 import { Logger } from "@nestjs/common";
-import { JobAction, JobDto } from "../../jobconfig.interface";
-import { JobClass } from "../../../../jobs/schemas/job.schema";
+import {
+  JobAction,
+  JobDto,
+  JobValidateContext,
+  JobPerformContext,
+} from "../../jobconfig.interface";
 import { LogJobActionOptions, actionType } from "./logaction.interface";
-import { compile, TemplateDelegate } from "handlebars";
-import { compileJob, TemplateJob } from "../../handlebar-utils";
+import { compile } from "handlebars";
+import { compileJobTemplate, TemplateJob } from "../../handlebar-utils";
 
+/**
+ * Log job events
+ *
+ * Three phases can be logged. Each is a handlebars template.
+ * 1. `init`: when the action is created.
+ *    Context: The options from the job configuration file.
+ *    Default: ""
+ * 2. `validate`: Runs during the `validate` phase, before the job is written to the
+ *    database.
+ *    Context: {request: <the http request>, env: <environment variables>}.
+ *    Default: ""
+ * 3. `performJob`: Runs during the `perform` phase, after the job is written to the
+ *    database.
+ *    Context: {request: <the http request>, job: <the job>, env: <environment
+ *    variables>}.
+ *    Default: "Performing job for {{{ job.type }}}"
+ */
 export class LogJobAction<T extends JobDto> implements JobAction<T> {
-  private validateTemplate: TemplateDelegate<T>;
+  private validateTemplate: TemplateJob;
   private performJobTemplate: TemplateJob;
 
   getActionType(): string {
@@ -20,13 +41,13 @@ export class LogJobAction<T extends JobDto> implements JobAction<T> {
     options = {
       init: "",
       validate: "",
-      performJob: "Performing job for {{{ type }}}",
+      performJob: "Performing job for {{{ job.type }}}",
       ...options,
     };
 
     const initTemplate = compile<LogJobActionOptions>(options.init);
-    this.validateTemplate = compile(options.validate || "");
-    this.performJobTemplate = compileJob(options.performJob || "");
+    this.validateTemplate = compileJobTemplate(options.validate || "");
+    this.performJobTemplate = compileJobTemplate(options.performJob || "");
 
     const msg = initTemplate(options);
     if (msg) {
@@ -34,14 +55,14 @@ export class LogJobAction<T extends JobDto> implements JobAction<T> {
     }
   }
 
-  async validate(dto: T) {
-    const msg = this.validateTemplate(dto);
+  async validate(context: JobValidateContext<T>) {
+    const msg = this.validateTemplate(context);
     if (msg) {
       Logger.log(msg, "LogJobAction");
     }
   }
 
-  async performJob(job: JobClass) {
-    Logger.log(this.performJobTemplate(job), "LogJobAction");
+  async performJob(context: JobPerformContext<T>) {
+    Logger.log(this.performJobTemplate(context), "LogJobAction");
   }
 }
