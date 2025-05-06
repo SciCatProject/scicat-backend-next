d7b9bf28f101eb1b955b22422462cad2ada7f4b2
Marco Tulio Ávila Cerón <marco.avila@nagarro.com>
2025-05-06 20:52:09 +0200

    adds history

--- src/datasets/datasets.controller.ts
+++ src/datasets/datasets.controller.ts
@@ -70,7 +70,6 @@ import { FormatPhysicalQuantitiesInterceptor } from "src/common/interceptors/for
 import { IFacets, IFilters } from "src/common/interfaces/common.interface";
 import { ClassConstructor, plainToInstance } from "class-transformer";
 import { validate, ValidationError, ValidatorOptions } from "class-validator";
-import { HistoryInterceptor } from "src/common/interceptors/history.interceptor";
 import { CreateDatasetOrigDatablockDto } from "src/origdatablocks/dto/create-dataset-origdatablock";
 import {
   PartialUpdateRawDatasetObsoleteDto,
@@ -1353,7 +1352,6 @@ export class DatasetsController {
     new UTCTimeInterceptor<DatasetClass>(["creationTime"]),
     new UTCTimeInterceptor<DatasetClass>(["endTime"]),
     new FormatPhysicalQuantitiesInterceptor<DatasetClass>("scientificMetadata"),
-    HistoryInterceptor,
   )
   @Patch("/:pid")
   @ApiOperation({
@@ -1463,7 +1461,6 @@ export class DatasetsController {
     new UTCTimeInterceptor<DatasetClass>(["creationTime"]),
     new UTCTimeInterceptor<DatasetClass>(["endTime"]),
     new FormatPhysicalQuantitiesInterceptor<DatasetClass>("scientificMetadata"),
-    HistoryInterceptor,
   )
   @Put("/:pid")
   @ApiOperation({
--- src/datasets/datasets.v4.controller.ts
+++ src/datasets/datasets.v4.controller.ts
@@ -45,7 +45,6 @@ import { UTCTimeInterceptor } from "src/common/interceptors/utc-time.interceptor
 import { FormatPhysicalQuantitiesInterceptor } from "src/common/interceptors/format-physical-quantities.interceptor";
 import { IFacets, IFilters } from "src/common/interfaces/common.interface";
 import { validate } from "class-validator";
-import { HistoryInterceptor } from "src/common/interceptors/history.interceptor";
 
 import { HistoryClass } from "./schemas/history.schema";
 import { TechniqueClass } from "./schemas/technique.schema";
@@ -664,7 +663,6 @@ export class DatasetsV4Controller {
     new UTCTimeInterceptor<DatasetClass>(["creationTime"]),
     new UTCTimeInterceptor<DatasetClass>(["endTime"]),
     new FormatPhysicalQuantitiesInterceptor<DatasetClass>("scientificMetadata"),
-    HistoryInterceptor,
   )
   @Patch("/:pid")
   @ApiOperation({
@@ -722,7 +720,6 @@ export class DatasetsV4Controller {
     new UTCTimeInterceptor<DatasetClass>(["creationTime"]),
     new UTCTimeInterceptor<DatasetClass>(["endTime"]),
     new FormatPhysicalQuantitiesInterceptor<DatasetClass>("scientificMetadata"),
-    HistoryInterceptor,
   )
   @Put("/:pid")
   @ApiOperation({
