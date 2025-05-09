88d3f0e2c2845a8695c4a424282044674d7953d3
junjiequan <a331998513@gmail.com>
2025-05-09 14:20:26 +0200

    fix: models.object import in the python SDK

--- src/attachments/types/attachment-filter-contents.ts
+++ src/attachments/types/attachment-filter-contents.ts
@@ -2,54 +2,21 @@ import {
   ContentObject,
   SchemaObject,
 } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
-import { boolean } from "mathjs";
 
 const FILTERS: Record<"limits" | "fields" | "where" | "include", object> = {
   where: {
-    type: "object",
-    example: {
-      "relationships.targetId": "datasetId",
-      "relationships.targetType": "dataset",
-      "relationships.relationType": "is attached to",
-    },
+    "relationships.targetId": "datasetId",
+    "relationships.targetType": "dataset",
+    "relationships.relationType": "is attached to",
   },
-  include: {},
-  fields: {
-    type: "array",
-    items: {
-      type: "string",
-      example: "relationships",
-    },
-  },
+  include: [],
+  fields: ["relationships"],
   limits: {
-    type: "object",
-    properties: {
-      limit: {
-        type: "number",
-        example: 10,
-      },
-      skip: {
-        type: "number",
-        example: 0,
-      },
-      sort: {
-        type: "object",
-        properties: {
-          createdAt: {
-            type: "string",
-            example: "asc | desc",
-          },
-        },
-      },
-    },
+    limit: 10,
+    skip: 0,
+    sort: { createdAt: "desc" },
   },
 };
-
-/**
- * NOTE: This is disabled only for the official sdk package generation as the schema validation complains about the content field.
- * But we want to have it when we run the application as it improves swagger documentation and usage a lot.
- * We use "content" property as it is described in the swagger specification: https://swagger.io/docs/specification/v3_0/describing-parameters/#schema-vs-content:~:text=explode%3A%20false-,content,-is%20used%20in
- */
 export const getSwaggerAttachmentFilterContent = (
   filtersToInclude: Record<keyof typeof FILTERS, boolean> = {
     where: true,
@@ -58,15 +25,11 @@ export const getSwaggerAttachmentFilterContent = (
     limits: true,
   },
 ): ContentObject | undefined => {
-  if (boolean(process.env.SDK_PACKAGE_SWAGGER_HELPERS_DISABLED ?? false)) {
-    return undefined;
-  }
-
   const filterContent: Record<string, { schema: SchemaObject }> = {
     "application/json": {
       schema: {
         type: "object",
-        properties: {},
+        example: {},
       },
     },
   };
@@ -75,7 +38,7 @@ export const getSwaggerAttachmentFilterContent = (
     const key = filtersKey as keyof typeof FILTERS;
 
     if (filtersToInclude[key] && FILTERS[key]) {
-      filterContent["application/json"].schema.properties![key] = FILTERS[key];
+      filterContent["application/json"].schema.example[key] = FILTERS[key];
     }
   }
 
--- src/datasets/types/dataset-filter-content.ts
+++ src/datasets/types/dataset-filter-content.ts
@@ -2,58 +2,18 @@ import {
   ContentObject,
   SchemaObject,
 } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
-import { boolean } from "mathjs";
 
 const FILTERS: Record<"limits" | "fields" | "where" | "include", object> = {
-  where: {
-    type: "object",
-    example: {
-      datasetName: { $regex: "Dataset", $options: "i" },
-    },
-  },
-  include: {
-    type: "array",
-    items: {
-      type: "string",
-      example: "attachments",
-    },
-  },
-  fields: {
-    type: "array",
-    items: {
-      type: "string",
-      example: "datasetName",
-    },
-  },
+  where: { datasetName: { $regex: "Dataset", $options: "i" } },
+  include: ["attachments"],
+  fields: ["datasetName"],
   limits: {
-    type: "object",
-    properties: {
-      limit: {
-        type: "number",
-        example: 10,
-      },
-      skip: {
-        type: "number",
-        example: 0,
-      },
-      sort: {
-        type: "object",
-        properties: {
-          datasetName: {
-            type: "string",
-            example: "asc | desc",
-          },
-        },
-      },
-    },
+    limit: 10,
+    skip: 0,
+    sort: { createdAt: "desc" },
   },
 };
 
-/**
- * NOTE: This is disabled only for the official sdk package generation as the schema validation complains about the content field.
- * But we want to have it when we run the application as it improves swagger documentation and usage a lot.
- * We use "content" property as it is described in the swagger specification: https://swagger.io/docs/specification/v3_0/describing-parameters/#schema-vs-content:~:text=explode%3A%20false-,content,-is%20used%20in
- */
 export const getSwaggerDatasetFilterContent = (
   filtersToInclude: Record<keyof typeof FILTERS, boolean> = {
     where: true,
@@ -62,15 +22,11 @@ export const getSwaggerDatasetFilterContent = (
     limits: true,
   },
 ): ContentObject | undefined => {
-  if (boolean(process.env.SDK_PACKAGE_SWAGGER_HELPERS_DISABLED ?? false)) {
-    return undefined;
-  }
-
   const filterContent: Record<string, { schema: SchemaObject }> = {
     "application/json": {
       schema: {
         type: "object",
-        properties: {},
+        example: {},
       },
     },
   };
@@ -79,7 +35,7 @@ export const getSwaggerDatasetFilterContent = (
     const key = filtersKey as keyof typeof FILTERS;
 
     if (filtersToInclude[key] && FILTERS[key]) {
-      filterContent["application/json"].schema.properties![key] = FILTERS[key];
+      filterContent["application/json"].schema.example[key] = FILTERS[key];
     }
   }
 
