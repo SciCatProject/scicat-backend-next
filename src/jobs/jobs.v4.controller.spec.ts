221651569b731f2bbbbd75edaa3b84e041656bea
Despina Adamopoulou <despoina.adamopoulou@empa.ch>
2025-06-02 14:43:51 +0200

    fix unit tests

--- src/jobs/jobs.v4.controller.spec.ts
+++ src/jobs/jobs.v4.controller.spec.ts
@@ -1,15 +1,24 @@
+import { EventEmitter2 } from "@nestjs/event-emitter";
 import { Test, TestingModule } from "@nestjs/testing";
 import { CaslAbilityFactory } from "src/casl/casl-ability.factory";
+import { DatasetsService } from "src/datasets/datasets.service";
+import { OrigDatablocksService } from "src/origdatablocks/origdatablocks.service";
 import { JobsV4Controller } from "./jobs.v4.controller";
 import { JobsService } from "./jobs.service";
+import { UsersService } from "src/users/users.service";
 import { JobsControllerUtils } from "./jobs.controller.utils";
 import { RabbitMQService } from "src/common/rabbitmq/rabbitmq.service";
 import { MailerModule, MailerService } from "@nestjs-modules/mailer";
+import { JobConfigService } from "src/config/job-config/jobconfig.service";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 
 class JobsServiceMock {}
 class JobsControllerUtilsMock {}
+class DatasetsServiceMock {}
+class OrigDatablocksServiceMock {}
+class UsersServiceMock {}
 class MailerServiceMock {}
+class JobsConfigMock {}
 class RabbitMQMock {}
 class CaslAbilityFactoryMock {}
 
@@ -38,8 +47,13 @@ describe("JobsV4Controller", () => {
         MailerModule.forRoot(),
       ],
       providers: [
+        { provide: JobConfigService, useClass: JobsConfigMock },
         { provide: JobsService, useClass: JobsServiceMock },
         { provide: JobsControllerUtils, useClass: JobsControllerUtilsMock },
+        { provide: DatasetsService, useClass: DatasetsServiceMock },
+        { provide: OrigDatablocksService, useClass: OrigDatablocksServiceMock },
+        { provide: UsersService, useClass: UsersServiceMock },
+        { provide: EventEmitter2, useClass: EventEmitter2 },
         { provide: CaslAbilityFactory, useClass: CaslAbilityFactoryMock },
         { provide: RabbitMQService, useClass: RabbitMQMock },
         {
