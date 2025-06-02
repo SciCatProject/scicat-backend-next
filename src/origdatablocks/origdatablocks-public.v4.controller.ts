957193e154dc7615eed0d66150c848398ae2431a
HayenNico <141850426+HayenNico@users.noreply.github.com>
2025-06-02 11:17:13 +0200

    add basic endpoints to public controller

--- src/origdatablocks/origdatablocks-public.v4.controller.ts
+++ src/origdatablocks/origdatablocks-public.v4.controller.ts
@@ -1,65 +1,37 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Patch,
   Param,
-  Delete,
-  UseGuards,
   Query,
-  HttpCode,
   HttpStatus,
-  Req,
-  ForbiddenException,
-  NotFoundException,
 } from "@nestjs/common";
-import { Request } from "express";
 import { OrigDatablocksService } from "./origdatablocks.service";
-import { CreateOrigDatablockDto } from "./dto/create-origdatablock.dto";
-import { PartialUpdateOrigDatablockDto } from "./dto/update-origdatablock.dto";
 import {
-  ApiBearerAuth,
-  ApiBody,
   ApiOperation,
   ApiParam,
   ApiQuery,
   ApiResponse,
   ApiTags,
 } from "@nestjs/swagger";
-import { PoliciesGuard } from "src/casl/guards/policies.guard";
-import { CheckPolicies } from "src/casl/decorators/check-policies.decorator";
-import { AppAbility, CaslAbilityFactory } from "src/casl/casl-ability.factory";
-import { Action } from "src/casl/action.enum";
 import {
   OrigDatablock,
   OrigDatablockDocument,
 } from "./schemas/origdatablock.schema";
-import { IFilters } from "src/common/interfaces/common.interface";
 import {
   IOrigDatablockFields,
   IOrigDatablockFiltersV4,
 } from "./interfaces/origdatablocks.interface";
-import { plainToInstance } from "class-transformer";
-import { validate } from "class-validator";
-import { DatasetsService } from "src/datasets/datasets.service";
-import { PartialUpdateDatasetDto } from "src/datasets/dto/update-dataset.dto";
-import { JWTUser } from "src/auth/interfaces/jwt-user.interface";
-import { DatasetClass } from "src/datasets/schemas/dataset.schema";
-import { CreateRawDatasetObsoleteDto } from "src/datasets/dto/create-raw-dataset-obsolete.dto";
-import { CreateDerivedDatasetObsoleteDto } from "src/datasets/dto/create-derived-dataset-obsolete.dto";
-import { IsValidResponse } from "src/common/types";
 import { getSwaggerOrigDatablockFilterContent } from "./origdatablock-filter-content";
 import { OrigDatablockLookupKeysEnum } from "./origdatablock-lookup";
 import { IncludeValidationPipe } from "./pipes/include-validation.pipe";
 import { FilterValidationPipe } from "./pipes/filter-validation.pipe";
+import { AllowAny } from "src/auth/decorators/allow-any.decorator";
 
 @ApiTags("origdatablocks v4")
 @Controller({ path: "origdatablocks/public", version: "4" })
 export class OrigDatablocksPublicV4Controller {
   constructor(
-    private readonly origDatablocksService: OrigDatablocksService,
-    private readonly datasetsService: DatasetsService,
+    private readonly origDatablocksService: OrigDatablocksService
   ) {}
 
   addPublicFilter(
@@ -74,5 +46,79 @@ export class OrigDatablocksPublicV4Controller {
 
     filter.where = { ...filter.where, isPublished: true };
   }
-  //TODO: Add endpoints
+
+  // GET /origdatablocks/public
+  @AllowAny()
+  @Get()
+  @ApiOperation({
+    summary: "It returns a list of public origdatablocks.",
+    description:
+      "It returns a list of public original datablocks. The list returned can be modified by providing a filter.",
+  })
+  @ApiQuery({
+    name: "filter",
+    description:
+      "Database filters to apply when retrieving the public origdatablocks",
+    required: false,
+    type: String,
+    content: getSwaggerOrigDatablockFilterContent(),
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: OrigDatablock,
+    isArray: true,
+    description: "Return the origdatablocks requested",
+  })
+  async findAllPublic(
+    @Query("filter", new FilterValidationPipe(), new IncludeValidationPipe())
+    queryFilter: string,
+  ) {
+    const parsedFilter = JSON.parse(queryFilter ?? "{}");
+
+    this.addPublicFilter(parsedFilter);
+
+    // TODO: Update service to complete query
+    const origdatablocks = await this.origDatablocksService.findAll(parsedFilter);
+
+    return origdatablocks;
+  }
+
+  // GET /origdatablocks/public/:id
+  @AllowAny()
+  @Get("/:pid")
+  @ApiParam({
+    name: "pid",
+    description: "Id of the public origdatablock to return",
+    type: String,
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: OrigDatablock,
+    isArray: false,
+    description: "Return public origdatablock with pid specified",
+  })
+  @ApiQuery({
+    name: "include",
+    enum: OrigDatablockLookupKeysEnum,
+    type: String,
+    required: false,
+    isArray: true,
+  })
+  async findByIdPublic(
+    @Param("pid") id: string,
+    @Query("include", new IncludeValidationPipe())
+    include: OrigDatablockLookupKeysEnum[] | OrigDatablockLookupKeysEnum,
+  ) {
+    const includeArray = Array.isArray(include)
+      ? include
+      : include && Array(include);
+
+    //TODO: Update to findOneComplete in service
+    const origdatablock = await this.origDatablocksService.findOne({
+      where: { pid: id, isPublished: true },
+      include: includeArray,
+    });
+
+    return origdatablock;
+  }
 }
