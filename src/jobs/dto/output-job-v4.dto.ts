7252778880b9efb6ba61f5684595a0db3b2d76be
sofyalaski <sofyalaskina@gmail.com>
2025-08-21 07:44:36 +0000

    improve documentation of DTOs

--- src/jobs/dto/output-job-v4.dto.ts
+++ src/jobs/dto/output-job-v4.dto.ts
@@ -9,44 +9,78 @@ import {
 } from "class-validator";
 import { PartialOutputDatasetDto } from "src/datasets/dto/output-dataset.dto";
 
+
 export class OutputJobDto extends CreateJobDto {
+
+  /**
+   * Globally unique identifier of a job.
+   */
   @IsString()
   id: string;
-
+  
+  /**
+   * Current status code of the job.
+   */
   @IsOptional()
   @IsString()
   statusCode?: string;
 
+  /**
+   * Latest status message received for this job.
+   */
   @IsOptional()
   @IsString()
   statusMessage?: string;
 
-  @IsOptional()
+  /**
+   * Configuration version that was used to create this job.
+   */
   @IsString()
   configVersion: string;
 
-  @IsOptional()
+  /**
+   * "Contains the dataset archiving results.
+   */
   @IsObject()
   jobResultObject: Record<string, unknown>;
 
+  /**
+   * User who created this job. System managed.
+   */
   @IsString()
   createdBy: string;
 
+  /**
+   * User who last updated this job. System managed.
+   */
   @IsString()
   updatedBy: string;
 
+  /**
+   * Date/time when the job was created. ISO 8601 format.
+   */
   @IsDateString()
   createdAt: Date;
 
+  /**
+   * Date/time when the job was last updated. ISO 8601 format.
+   */
   @IsDateString()
   updatedAt: Date;
 }
 
 export class PartialOutputJobDto extends PartialType(OutputJobDto) {
+  /**
+   * Populated when the job is joined with datasets. Contains the related datasets.
+   */
   @IsOptional()
   @IsArray()
   datasets: PartialOutputDatasetDto[];
 
+  /**
+   * Populated when the job is joined with datasets and their related entities 
+   * (datablocks, origdatablocks, attachments). Contains datasets with nested details.
+   */
   @IsOptional()
   @IsArray()
   datasetDetails: PartialOutputDatasetDto[];
