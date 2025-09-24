7f81e00cc71a8b544f509c2d775689266ecdb503
Abdi Abdulle <abdi.abdulle@outlook.dk>
2025-09-24 14:57:32 +0200

    added api test

--- test/DatasetV4.js
+++ test/DatasetV4.js
@@ -7,7 +7,6 @@ let accessTokenAdminIngestor = null,
   accessTokenArchiveManager = null,
   accessTokenUser1 = null,
   accessTokenUser2 = null,
-
   derivedDatasetMinPid = null,
   rawDatasetWithMetadataPid = null,
   datasetScientificPid = null,
@@ -353,6 +352,29 @@ describe("2500: Datasets v4 tests", () => {
           rawDatasetWithMetadataPid = res.body.pid;
         });
     });
+
+    it("0128: should increment numberOfDatasets in linked proposals when creating a new dataset", async () => {
+      const proposalRes = await request(appUrl)
+        .post("/api/v3/Proposals")
+        .send(TestData.ProposalCorrectMin)
+        .auth(accessTokenAdminIngestor, { type: "bearer" });
+      const proposalId = proposalRes.body.proposalId;
+
+      const dataset = {
+        ...TestData.DerivedCorrectMinV4,
+        proposalIds: [proposalId],
+      };
+      await request(appUrl)
+        .post("/api/v4/datasets")
+        .send(dataset)
+        .auth(accessTokenAdminIngestor, { type: "bearer" });
+
+      const proposal = await request(appUrl)
+        .get(`/api/v3/Proposals/${encodeURIComponent(proposalId)}`)
+        .auth(accessTokenAdminIngestor, { type: "bearer" });
+      console.log("numberOfDatasets: ", JSON.stringify(proposal.body.numberOfDatasets, null, 2));
+      proposal.body.should.have.property("numberOfDatasets").and.equal(1);
+    });
   });
 
   describe("Datasets v4 findAll tests", () => {
@@ -626,7 +648,7 @@ describe("2500: Datasets v4 tests", () => {
         },
       };
       const malformedJson = JSON.stringify(filter).replace("}", "},");
-      
+
       return request(appUrl)
         .get(`/api/v4/datasets`)
         .query({ filter: malformedJson })
@@ -1038,7 +1060,7 @@ describe("2500: Datasets v4 tests", () => {
 
     it("0604: should be able to partially update dataset's scientific metadata field", () => {
       const updatedDataset = {
-        datasetlifecycle:{storageLocation:"new location"},
+        datasetlifecycle: { storageLocation: "new location" },
         scientificMetadata: {
           with_unit_and_value_si: {
             value: 600,
@@ -1070,7 +1092,9 @@ describe("2500: Datasets v4 tests", () => {
             value: 111,
             unit: "",
           });
-          res.body.datasetlifecycle.should.have.property("storageLocation").and.equal("new location");
+          res.body.datasetlifecycle.should.have
+            .property("storageLocation")
+            .and.equal("new location");
         });
     });
 
@@ -1457,7 +1481,9 @@ describe("2500: Datasets v4 tests", () => {
         .then((res) => {
           res.body.should.be.a("object");
           res.body.should.be.deep.include(updatedDataset);
-          res.body.should.have.property("storageLocation").and.equal("new location");
+          res.body.should.have
+            .property("storageLocation")
+            .and.equal("new location");
         });
     });
 
