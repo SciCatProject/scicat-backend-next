c5b918ed1f8940f63cd074dda7f57be527aee3e8
Max Novelli <max.novelli@ess.eu>
2025-11-27 14:01:18 +0100

    Added template for module, controller and service

---
+++ src/metadata-keys/metadatakeys.v4.service.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable, Scope } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { InjectModel } from "@nestjs/mongoose";
+import { MetadataKeyClass, MetadataKeyDocument } from "./schemas/metadatakey.schema";
+import { Model } from "mongoose";
+import { REQUEST } from "@nestjs/core";
+
+@Injectable({ scope: Scope.REQUEST })
+export class MetadataKeysV4Service {
+  constructor(
+    private configService: ConfigService,
+    @InjectModel(MetadataKeyClass.name)
+    private metadataKeyModel: Model<MetadataKeyDocument>,
+    @Inject(REQUEST) private request: Request,
+  ) {}
+}
