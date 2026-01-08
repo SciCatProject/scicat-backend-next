b8bd9954cda9b1deee401a8ecbd8dce6bfd35772
junjiequan <a331998513@gmail.com>
2026-01-08 15:50:13 +0100

    Restart application overwrittes previous config record with config file

--- src/config/runtime-config/runtime-config.service.ts
+++ src/config/runtime-config/runtime-config.service.ts
@@ -7,14 +7,11 @@ import {
 import { ConfigService } from "@nestjs/config";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { isEqual } from "lodash";
-import { reconcileData } from "./utils";
 import { OutputRuntimeConfigDto } from "./dto/runtime-config.dto";
 import {
   RuntimeConfig,
   RuntimeConfigDocument,
 } from "./schemas/runtime-config.schema";
-import { computeDeltaWithOriginals } from "src/common/utils/delta.util";
 
 @Injectable()
 export class RuntimeConfigService implements OnModuleInit {
@@ -29,94 +26,78 @@ export class RuntimeConfigService implements OnModuleInit {
       "configSyncToDb.configList",
     )!;
 
-    for (const configId of configList) await this.syncConfigDiff(configId);
+    for (const configId of configList) await this.syncConfig(configId);
   }
 
-  async getConfig(id: string): Promise<OutputRuntimeConfigDto | null> {
-    const data = await this.runtimeConfigModel.findOne({ _id: id }).lean();
+  async getConfig(cid: string): Promise<OutputRuntimeConfigDto | null> {
+    const data = await this.runtimeConfigModel.findOne({ cid: cid }).lean();
 
     if (!data) {
-      throw new NotFoundException(`Config '${id}' not found`);
+      throw new NotFoundException(`Config '${cid}' not found`);
     }
     return data;
   }
 
   async updateConfig(
-    id: string,
+    cid: string,
     config: Record<string, unknown>,
     updatedBy: string,
   ): Promise<OutputRuntimeConfigDto | null> {
-    const updatedDoc = await this.runtimeConfigModel.findByIdAndUpdate(
-      { _id: id },
+    const updatedDoc = await this.runtimeConfigModel.findOneAndUpdate(
+      { cid: cid },
       { $set: { data: config, updatedBy } },
       { new: true },
     );
 
     if (!updatedDoc) {
-      throw new NotFoundException(`Config '${id}' not found`);
+      throw new NotFoundException(`Config '${cid}' not found`);
     }
     Logger.log(
-      `Updated app config entry '${id}' by user '${updatedBy}'`,
+      `Updated app config entry '${cid}' by user '${updatedBy}'`,
       "RuntimeConfigService",
     );
 
     return updatedDoc;
   }
 
-  async syncConfigDiff(configId: string): Promise<void> {
-    const autoSyncEnabled = this.configService.get<boolean>(
-      `configSyncToDb.enabled`,
-    );
-
+  async syncConfig(configId: string): Promise<void> {
     const sourceConfig =
       this.configService.get<Record<string, unknown>>(configId) || {};
 
-    const existing = await this.runtimeConfigModel
-      .findOne({ _id: configId })
-      .lean();
-
-    // If no existing config, create one with default values
-    if (!existing) {
-      await this.runtimeConfigModel.create({
-        _id: configId,
-        data: sourceConfig,
-      });
-      Logger.log(
-        `Created runtime config entry '${configId}' with default values from json file`,
+    if (!sourceConfig || Object.keys(sourceConfig).length === 0) {
+      Logger.warn(
+        `Config file: ${configId} is empty or missing, skipping sync`,
         "RuntimeConfigService",
       );
       return;
     }
 
-    if (!autoSyncEnabled) return;
+    const existing = await this.runtimeConfigModel
+      .findOne({ cid: configId })
+      .lean();
 
-    const dbValue = existing.data || {};
-
-    const updatedConfig = reconcileData(dbValue, sourceConfig) as Record<
-      string,
-      unknown
-    >;
-
-    if (isEqual(updatedConfig, dbValue)) {
+    // If no existing config, create one from config file
+    if (!existing) {
+      await this.runtimeConfigModel.create({
+        cid: configId,
+        data: sourceConfig,
+      });
+      Logger.log(
+        `Created runtime config entry: '${configId}' with config file`,
+        "RuntimeConfigService",
+      );
       return;
     }
 
-    const { delta, originals } = computeDeltaWithOriginals(
-      dbValue,
-      updatedConfig,
-    );
-
+    // overwrite existing config with config file
     await this.runtimeConfigModel.updateOne(
-      { _id: configId },
-      { data: updatedConfig },
+      { cid: configId },
+      { data: sourceConfig },
     );
 
     Logger.log(
-      {
-        before: JSON.stringify(originals),
-        after: JSON.stringify(delta),
-      },
-      `RuntimeConfigService - [${configId}] synchronized changes`,
+      `RuntimeConfigService - [${configId}] synchronized with config file`,
+      "RuntimeConfigService",
     );
   }
 }
