2b44f79ca5e0c1e261981db5cf506f0c9aff098e
minottic <carlo.minotti1@gmail.com>
2026-01-23 11:01:50 +0000

    chore: refactor FilterPipe to allow where and fields

--- src/common/pipes/filter.pipe.ts
+++ src/common/pipes/filter.pipe.ts
@@ -1,68 +1,13 @@
 import { Injectable, PipeTransform } from "@nestjs/common";
-import {
-  transformDeep,
-  TransformObjValuesPipe,
-} from "src/jobs/pipes/v3-filter.pipe";
+import { transformDeep } from "src/jobs/pipes/v3-filter.pipe";
 import { IFilters } from "../interfaces/common.interface";
-import _ from "lodash";
+import { isPlainObject, keys, pickBy } from "lodash";
 
-/**
- * @class FilterPipe
- * @description
- * A NestJS pipe that converts filter objects or JSON strings into a MongoDB-compatible format.
- *
- * - Recursively transforms all `where` keys at any depth.
- * - For all `where` relatives, replaces operators (`inq`, `nin`, `and`, `or`, `like`, `ilike`) with MongoDB equivalents.
- * - Adds `$options: "i"` for `ilike` to enable case-insensitive regex.
- * - Parses JSON strings if needed and returns the result in the same format (string or object).
- */
-@Injectable()
-export class FilterPipe<T = unknown> implements PipeTransform<
+abstract class FilterPipeAbstract<T = unknown> implements PipeTransform<
   { filter?: string } | string,
   { filter?: IFilters<T> } | IFilters<T>
 > {
-  private static readonly replaceOperatorsMap = {
-    inq: "$in",
-    nin: "$nin",
-    and: "$and",
-    or: "$or",
-    like: "$regex",
-    ilike: "$regex",
-    gte: "$gte",
-    lte: "$lte",
-    gt: "$gt",
-    lt: "$lt",
-  };
-  private readonly replaceOperatorsPipe: TransformObjValuesPipe;
-
-  constructor(options = { allowObjectFields: true }) {
-    const fields: { fields?: (value: unknown) => unknown } = {};
-    if (options.allowObjectFields)
-      fields.fields = (val: unknown) =>
-        _.isPlainObject(val)
-          ? _.keys(_.pickBy(val as Record<string, boolean>, Boolean))
-          : val;
-    this.replaceOperatorsPipe = new TransformObjValuesPipe({
-      where: (value: unknown) => {
-        return transformDeep(value, {
-          funcMap: {
-            ilike: (val: unknown, par: unknown) => {
-              const p = par as Record<string, unknown>;
-              p["$options"] = "i";
-              return val;
-            },
-          },
-          valueFn: (val: unknown) => {
-            if (typeof val !== "string") return val;
-            const dateFromString = new Date(val);
-            return isNaN(dateFromString.getTime()) ? val : dateFromString;
-          },
-          keyMap: FilterPipe.replaceOperatorsMap,
-        });
-      },
-      ...fields,
-    });
-  }
+  abstract applyTransform(value: unknown): unknown;
 
   transform(inValue: { filter?: string } | string):
     | {
@@ -76,11 +21,87 @@ export class FilterPipe<T = unknown> implements PipeTransform<
         ? JSON.parse(inValue)
         : JSON.parse(inValue.filter!);
 
-    const transformedFilter = this.replaceOperatorsPipe.transform(
-      parsedFilter,
-    ) as IFilters<T>;
+    const transformedFilter = this.applyTransform(parsedFilter) as IFilters<T>;
 
     if (typeof inValue === "string") return transformedFilter;
     return { ...inValue, filter: transformedFilter };
+  }
+}
+
+@Injectable()
+export class WherePipe<T = unknown> extends FilterPipeAbstract<T> {
+  private readonly replaceOperatorsMap = {
+    inq: "$in",
+    nin: "$nin",
+    and: "$and",
+    or: "$or",
+    like: "$regex",
+    ilike: "$regex",
+    gte: "$gte",
+    lte: "$lte",
+    gt: "$gt",
+    lt: "$lt",
+  };
+
+  applyTransform(value: unknown): unknown {
+    return transformDeep(value, {
+      funcMap: {
+        ilike: (val: unknown, par: unknown) => {
+          const p = par as Record<string, unknown>;
+          p["$options"] = "i";
+          return val;
+        },
+      },
+      valueFn: (val: unknown) => {
+        if (typeof val !== "string") return val;
+        const dateFromString = new Date(val);
+        return isNaN(dateFromString.getTime()) ? val : dateFromString;
+      },
+      keyMap: this.replaceOperatorsMap,
+    });
+  }
+}
+
+@Injectable()
+export class FieldsPipe<T = unknown> extends FilterPipeAbstract<T> {
+  applyTransform(value: unknown) {
+    if (isPlainObject(value)) {
+      return keys(pickBy(value as Record<string, boolean>, Boolean));
+    }
+    return value;
+  }
+}
+
+/**
+ * @class FilterPipe
+ * @description
+ * A NestJS pipe that converts filter objects or JSON strings into a MongoDB-compatible format.
+ *
+ * - Recursively transforms all `where` keys at any depth.
+ * - For all `where` relatives, replaces operators (`inq`, `nin`, `and`, `or`, `like`, `ilike`) with MongoDB equivalents.
+ * - Adds `$options: "i"` for `ilike` to enable case-insensitive regex.
+ * - Parses JSON strings if needed and returns the result in the same format (string or object).
+ */
+@Injectable()
+export class FilterPipe<T = unknown> extends FilterPipeAbstract<T> {
+  private wherePipe = new WherePipe();
+  private fieldsPipe = new FieldsPipe();
+  private options: { allowObjectFields: boolean };
+
+  constructor(options?: { allowObjectFields: boolean }) {
+    super();
+    this.options = options ?? { allowObjectFields: true };
+  }
+
+  applyTransform(value: unknown): unknown {
+    const fields: { fields?: (value: unknown) => unknown } = {};
+    if (this.options.allowObjectFields)
+      fields.fields = (val) => this.fieldsPipe.applyTransform(val);
+    return transformDeep(value, {
+      funcMap: {
+        where: (val) => this.wherePipe.applyTransform(val),
+        ...fields,
+      },
+    });
   }
 }
