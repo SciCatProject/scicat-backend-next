e4d57398620a673fd96c17666f6183b217dbe1fa
junjiequan <a331998513@gmail.com>
2026-02-13 11:05:50 +0100

    revert path change for access-grou-provider

--- src/auth/providers/access-group-provider/access-group.service.ts
+++
@@ -1,8 +0,0 @@
-import { UserPayload } from "src/auth/interfaces/userPayload.interface";
-
-export abstract class AccessGroupService {
-  abstract getAccessGroups(
-    //idpPayload: Record<string, unknown>,
-    userPayload: UserPayload,
-  ): Promise<string[]>;
-}
